bf588e86a25c003f04e10802a1725352eb2b7857
Jono Lewis <hello@jonolewis.com>
2017-05-01 10:56:43 -0700

    Created model for timelog, and added a basic auto-insert for dummy MongoDB data, but having issues with relationship.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var {Schema} = mongoose;
 
 var app = express();
 
@@ -16,7 +17,7 @@ app.use(function(req, res, next) {
 mongoose.connect('mongodb://localhost/hgnData');
 mongoose.set('debug', true);
 
-var profile = new mongoose.Schema({
+var profileSchema = new Schema({
 	name: String,
 	phone: String,
 	email: String,
@@ -29,36 +30,34 @@ var profile = new mongoose.Schema({
 	created: {type : Date, default : Date.now},
 });
 
-/*
-Sample Mongo DB entry 
-	{
-        "_id": "58f91d30b4c403d02cddd23d",
-        "about": "A moment of Silence!",
-        "comitted_hours": "22",
-        "created": "2017-04-20T23:03:36.778Z",
-        "email": "anil.amf237@gmail.com",
-        "estimated_tenure": "2017-04-20T23:03:36.778Z",
-        "avatar":"",
-        "facebook": "https://www.linkedin.com/in/ak-malla",
-        "linkedin": "https://www.linkedin.com/in/ak-malla",
-        "name": "Anil Kumar, Malla",
-        "phone": "9999999999"
-    }
-*/
-
-var noteSchema = new mongoose.Schema({
+var noteSchema = new Schema({
 	title: 'string',
 	content: 'string',
 	author: 'string'
 });
 
-var NoteModel = mongoose.model('profile',profile);
+var timelogSchema = new Schema({
+	//profile: { type: Schema.Types.ObjectID, ref: 'profiles' },
+  date: {type : Date, default : Date.now},
+  startTime: Date,
+  endTime: Date,
+  totalSeconds: Number,
+  tangible: Boolean,
+  workCompleted: String,
+  project: String,
+  task: String
+})
+
+var Profile = mongoose.model('profile', profileSchema);
+var Timelog = mongoose.model('timelog', timelogSchema);
+insertDummyData();
+
 app.get('/api/',function(req,res) {
 	res.send('Working');
 });
 
 app.get('/api/profiles', function(req,res) {
-	NoteModel.find({},function(err,docs) {
+	Profile.find({},function(err,docs) {
 		if(err) {
 			res.send(err);
 		}
@@ -67,5 +66,74 @@ app.get('/api/profiles', function(req,res) {
 		}
 	});
 });
+
+app.get('/api/timelogs', function(req,res) {
+	Timelog.find({},function(err,docs) {
+		if(err) {
+			res.send(err);
+		}
+		else {
+			res.send(docs);
+		}
+	});
+});
+
+
+app.get('api/timelog', function(req, res){
+	console.log('timelog get: ', res.body)
+})
+
+
+function insertDummyData() {
+	const errFunc = (collectionName) => { return (err) => {if (err) throw err; else console.log(`${collectionName} inserted...`)} }
+
+	Timelog.find({}, function(err, docs) {
+
+		if (err) {
+			console.log(err)
+		}
+		else {
+			if (docs && docs.length == 0) {
+				let t = new Timelog({
+					date: '2017-04-06',
+					startTime: '2017-04-06 12:00:00',
+					endTime: '2017-04-06 13:00:00',
+					totalSeconds: 60 * 60,
+					tangile: true,
+					workCompleted: 'Working on really great things.',
+					project: 'hgn',
+					task: 'Models for Mongo.'
+				})
+				t.save(errFunc('timelog'))
+			}
+		}
+	})
+
+	Profile.find({}, function(err, docs) {
+		if (err) {
+			console.log(err)
+		}
+		else {
+			if (docs && docs.length == 0) {
+				let p = new Profile({
+			        "_id": "58f91d30b4c403d02cddd23d",
+			        "about": "A moment of Silence!",
+			        "comitted_hours": "22",
+			        "created": "2017-04-20T23:03:36.778Z",
+			        "email": "anil.amf237@gmail.com",
+			        "estimated_tenure": "2017-04-20T23:03:36.778Z",
+			        "avatar":"",
+			        "facebook": "https://www.linkedin.com/in/ak-malla",
+			        "linkedin": "https://www.linkedin.com/in/ak-malla",
+			        "name": "Anil Kumar, Malla",
+			        "phone": "9999999999"
+			    })
+
+				p.save(errFunc('profile'))
+			}
+		}
+	})
+}
+
 
 app.listen('4500');
