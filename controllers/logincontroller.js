998bf4101fd6732028947cb5c528b0037297b49d
Shubhra Mittal <shubhra.goel@gmail.com>
2017-12-23 18:01:36 -0800

    Added login to return token on successfull login

--- controllers/logincontroller.js
+++ controllers/logincontroller.js
@@ -1,43 +1,58 @@
 let express = require('express');
 let userprofile = require('../models/userProfile');
 let bcrypt = require('bcryptjs');
+let jwt = require('jsonwebtoken');
 
-let logincontroller = function(){
+let logincontroller = function () {
 
-    let login = async function _login(req, res){
+  const JWT_HEADER = {
+    "alg": "RS256",
+    "typ": "JWT"
+  };
 
-        console.log(`inside controller ${req.body}`);
+  const JWT_SECRET = "hgndata";
 
-        let _userName = req.body.userName;
-        let _password = req.body.password;
-        
-        let user =  await userprofile.findOne({userName: _userName});
+  let login = async function _login(req, res) {
 
-        if(!user)
-        {
-            res.send({message: "Invalid username and/ or password." }).status(401);
-        }
+    let _userName = req.body.userName;
+    let _password = req.body.password;
 
-       let isPasswordMatch =  await bcrypt.compare(_password, user.password);
+    let user = await userprofile.findOne({
+      userName: _userName
+    });
 
-       if(isPasswordMatch)
-       {
-           res.send(user).status(200);
-       }
-       else
-       {
-           res.send({message: "Invalid username and/ or password." }).status(401);
-       }
-     
-        
+    if (!user) {
+      res.send({
+        message: "Invalid username and/ or password."
+      }).status(401);
+    }
 
-        //res.send(user).status(200);
-        };
-    
-        return{
+    let isPasswordMatch = false;
+    isPasswordMatch = await bcrypt.compare(_password, user.password);
 
-            login: login
-        };
+    if (isPasswordMatch) {
+
+      let jwt_payload = {
+        "userid": user._id,
+        "role": user.role,
+        "loggedinDate": Date.now()
+      };
+
+      let token = jwt.sign(jwt_payload, JWT_SECRET);
+
+      res.send(token).status(200);
+    } else {
+      res.send({
+        message: "Invalid username and/ or password."
+      }).status(401);
+    }
+
+  }
+
+  return {
+
+    login: login
+  };
 
 };
 
