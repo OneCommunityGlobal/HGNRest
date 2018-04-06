54f9ea5f1da9c60881cdaf7fdfaf6b127958cc4a
Priyanka Singh <singhpriyanka1@gmail.com>
2018-04-06 11:58:40 -0700

    resolving merge conflict

--- helpers/userhelper.js
+++ helpers/userhelper.js
@@ -11,23 +11,23 @@ var userhelper = function(){
     } ;
     var getTeamMembers = function (user) {
 
-      var userid =mongoose.Types.ObjectId(user._id );        
+      var userid =mongoose.Types.ObjectId(user._id );
      // var teamid = userdetails.teamId;
     return myteam.findById(userid).select ({"myteam._id":1,"myteam.role":1, "myteam.fullName":1, _id:0 });
-   
+
 
 
     }
 
-    
+
     var getUserName =  async function(userId)
     {
         let userid = mongoose.Types.ObjectId(userId);
       return userProfile.findById(userid, 'firstName lastName');
-        
-       
-       
-        
+
+
+
+
     }
 
     return{
