8a274613f144c67c494133e646a68ca85ff78ccb
Shubhra Mittal <shubhra.goel@gmail.com>
2018-04-18 12:55:33 -0700

    project name uniqueness

--- models/project.js
+++ models/project.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var projectschema = new Schema({
 
-    projectName: { type: String, required: true },
+    projectName: { type: String, required: true, unique: true },
     isActive: { type: Boolean, default: true },
     createdDatetime: { type: Date },
     modifiedDatetime: { type: Date, default: Date.now() }
