310216145faa0f84585018608e29a2f1812e3f2a
Sowmya Manohar <sowmi.sarojini@gmail.com>
2018-05-03 16:01:09 -0400

    Added isActive field to be returned from db for filtering in UserManagement

--- controllers/userProfileController.js
+++ controllers/userProfileController.js
@@ -18,7 +18,7 @@ var userProfileController = function (userProfile) {
       return;
     }
 
-    userProfile.find({}, '_id firstName lastName role weeklyComittedHours email', function (err, profiles) {
+    userProfile.find({}, '_id firstName lastName role weeklyComittedHours email isActive', function (err, profiles) {
       if (err) {
         res.status(404).send("Error finding user profiles");
         return;
