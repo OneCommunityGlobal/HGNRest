cf40c2ff2b9825e86a8ec5f7b31b0fa6512bbafe
Sowmya Manohar <sowmi.sarojini@gmail.com>
2018-05-07 18:28:41 -0400

    Generated Random no for a password

--- controllers/forgotPwdcontroller.js
+++ controllers/forgotPwdcontroller.js
@@ -14,7 +14,7 @@ var forgotPwdController = function (userProfile) {
             
             if(user.firstName === _firstName && user.lastName === _lastName)
             {
-                var ranPwd ="Vol123456";
+              var ranPwd =  create_UUID().concat("TEMP");
                 user.set({ password: ranPwd });
                 user.save()
                 .then(results => {
@@ -34,3 +34,13 @@ var forgotPwdController = function (userProfile) {
 }
 
 module.exports = forgotPwdController;
+
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+};
