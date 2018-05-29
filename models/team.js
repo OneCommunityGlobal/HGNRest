d4daf4fc000e7c9be2776aa86e6e55a788217803
Shubhra Mittal <shubhra.goel@gmail.com>
2018-05-29 14:41:58 -0700

    adding active attribute to teams

--- models/team.js
+++ models/team.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 var team = new Schema({
 
     teamName: { type: "String", required: true },
+    isActive: { type: "Boolean", required: true, default: true },
     createdDatetime: { type: Date },
     modifiedDatetime: { type: Date, default: Date.now() }
 
