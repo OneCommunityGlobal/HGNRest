bae31749a21a008008f673754e011e6e4205f1f8
Shubhra Mittal <shubhra.goel@gmail.com>
2018-09-24 13:19:08 -0700

    replaced node scheuler with cron for better support

--- helpers/userhelper.js
+++ helpers/userhelper.js
@@ -1,6 +1,8 @@
 var userProfile = require('../models/userProfile');
 var myteam = require('../helpers/helperModels/myTeam');
+var dashboardhelper = require("../helpers/dashboardhelper")()
 var mongoose = require('mongoose');
+var moment = require('moment-timezone');
 
 var userhelper = function () {
 
@@ -48,11 +50,42 @@ var userhelper = function () {
        
     }
 
+    var assignBlueBadgeforTimeNotMet = function(){
+        console.log("trigger job")
+        var pdtStartOfLastWeek = moment().tz("America/Los_Angeles").startOf("isoWeek").subtract(1, "week");
+        var pdtEndOfLastWeek = moment().tz("America/Los_Angeles").endOf("isoWeek").subtract(1, "week");
+        userProfile.find({ isActive: true}, '_id')
+        .then(users => {
+            users.forEach(user => {
+                const personId = mongoose.Types.ObjectId(user._id)
+                dashboardhelper.laborthisweek(personId, pdtStartOfLastWeek, pdtEndOfLastWeek)
+        .then(results => {
+            const weeklyComittedHours = results[0].weeklyComittedHours;
+            const timeSpent = results[0].timeSpent_hrs;
+            console.log(`Checking for user ${user._id} committed : ${weeklyComittedHours} logged : ${timeSpent}`)
+            
+            if (timeSpent < weeklyComittedHours)
+            {
+                const description = `System auto-assigned infringement for not meeting weekly volunteer time commitment. You logged ${timeSpent} hours against committed effort of ${weeklyComittedHours} hours in the week starting ${pdtStartOfLastWeek.format("dddd YYYY-MM-DD")} and ending ${pdtEndOfLastWeek.format("dddd YYYY-MM-DD")}`
+                const infringment = {date: moment().utc().format("YYYY-MM-DD"), description :description }
+                userProfile.findByIdAndUpdate(personId,{$push: {infringments: infringment}} )
+                .then(status => console.log(`Assigned infringment to ${status._id} ${status.firstName} ${status.lastName}`))
+                .catch(error  => console.log(error))
+            }
+        })
+        .catch(error => console.log(error))
+            });    
+
+        })
+        .catch(error => console.log(error))
+    }
+
     return {
 
         getUserName: getUserName,
         getTeamMembers: getTeamMembers,
-        validateprofilepic : validateprofilepic
+        validateprofilepic : validateprofilepic,
+        assignBlueBadgeforTimeNotMet: assignBlueBadgeforTimeNotMet
 
     }
 
