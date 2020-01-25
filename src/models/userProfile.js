1630d351039a5cc588d9f59fef497bdbab3c2512
Siddharth Gore <siddharth.gore@live.com>
2020-01-25 17:48:04 -0500

    added new fields to userProfile

--- src/models/userProfile.js
+++ src/models/userProfile.js
@@ -1,14 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
-const validate = require('mongoose-validator');
-const bcrypt = require('bcryptjs');
-
+const validate = require("mongoose-validator");
+const bcrypt = require("bcryptjs");
 
 const SALT_ROUNDS = 10;
 
 const userProfileSchema = new Schema({
-
   password: {
     type: String,
     required: true,
@@ -17,45 +15,76 @@ const userProfileSchema = new Schema({
         const passwordregex = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
         return passwordregex.test(v);
       },
-      message: '{VALUE} is not a valid password!password should be at least 8 charcaters long with uppercase, lowercase and number/special char.',
-    },
+      message:
+        "{VALUE} is not a valid password!password should be at least 8 charcaters long with uppercase, lowercase and number/special char."
+    }
   },
   isActive: { type: Boolean, required: true, default: true },
-  role: { type: String, required: true, enum: ['Volunteer', 'Manager', 'Administrator', 'Core Team'] },
+  jobTitle: { type: String },
+  emailPubliclyAccessible: { type: Boolean, default: true },
+  phoneNumberPubliclyAccessible: {
+    type: Boolean,
+    default: true
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["Volunteer", "Manager", "Administrator", "Core Team"]
+  },
   firstName: {
-    type: String, required: true, trim: true, minlength: 2,
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 2
   },
   lastName: { type: String, required: true, minlength: 2 },
   phoneNumber: [{ type: String, phoneNumber: String }],
   bio: { type: String },
   email: {
-    type: String, required: true, unique: true, validate: [validate({ validator: 'isEmail', message: 'Email address is invalid' })],
+    type: String,
+    required: true,
+    unique: true,
+    validate: [
+      validate({ validator: "isEmail", message: "Email address is invalid" })
+    ]
   },
   weeklyComittedHours: { type: Number, default: 10 },
   createdDate: { type: Date, required: true, default: Date.now() },
   lastModifiedDate: { type: Date, required: true, default: Date.now() },
-  personalLinks: [{ _id: Schema.Types.ObjectId, Name: String, Link: { type: String } }],
+  personalLinks: [
+    { _id: Schema.Types.ObjectId, Name: String, Link: { type: String } }
+  ],
   adminLinks: [{ _id: Schema.Types.ObjectId, Name: String, Link: String }],
-  teams: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'team' }],
-  projects: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'project' }],
-  badgeCollection: [{ badgeName: String, quantity: Number, lastModifiedDate: Date }],
+  teams: [{ type: mongoose.SchemaTypes.ObjectId, ref: "team" }],
+  projects: [{ type: mongoose.SchemaTypes.ObjectId, ref: "project" }],
+  badgeCollection: [
+    { badgeName: String, quantity: Number, lastModifiedDate: Date }
+  ],
   profilePic: { type: String },
-  infringments: [{ date: { type: String, required: true }, description: { type: String, required: true } }],
-
+  infringments: [
+    {
+      date: { type: String, required: true },
+      description: { type: String, required: true }
+    }
+  ]
 });
 
-userProfileSchema.pre('save', function (next) {
+userProfileSchema.pre("save", function(next) {
   const user = this;
-  if (!user.isModified('password')) return next();
+  if (!user.isModified("password")) return next();
 
-  return bcrypt.genSalt(SALT_ROUNDS)
+  return bcrypt
+    .genSalt(SALT_ROUNDS)
     .then(result => bcrypt.hash(user.password, result))
-    .then((hash) => {
+    .then(hash => {
       user.password = hash;
       return next();
     })
     .catch(error => next(error));
 });
 
-
-module.exports = mongoose.model('userProfile', userProfileSchema, 'userProfiles');
+module.exports = mongoose.model(
+  "userProfile",
+  userProfileSchema,
+  "userProfiles"
+);
