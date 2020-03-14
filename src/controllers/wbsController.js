f4da231b1538e7fb76f653c78557cd58f48a6301
Henry Nguyen <vnseattleapp@gmail.com>
2020-03-14 16:01:52 -0600

    Completed WBS

--- src/controllers/wbsController.js
+++ src/controllers/wbsController.js
@@ -1,57 +1,54 @@
 
 const wbsController = function (WBS) {
-    
-    const postWBS = function (req, res) {
-      if (req.body.requestor.role !== 'Administrator') {
-        res.status(403).send({ error: 'You are not authorized to create new projects.' });
-        return;
-      }
-  
-      if (!req.body.wbsName || !req.body.isActive) {
-        res.status(400).send({ error: 'WBS Name and active status are mandatory fields' });
-        return;
-      }
-  
-      const _wbs= new WBS();
-      _wbs.projectId = req.params.projectId;
-      _wbs.wbsName = req.body.wbsName;
-      _wbs.isActive = req.body.isActive;
-      _wbs.createdDatetime = Date.now();
-      _wbs.modifiedDatetime = Date.now();
-  
-      _wbs.save()
+  const postWBS = function (req, res) {
+    if (req.body.requestor.role !== 'Administrator') {
+      res.status(403).send({ error: 'You are not authorized to create new projects.' });
+      return;
+    }
+
+    if (!req.body.wbsName || !req.body.isActive) {
+      res.status(400).send({ error: 'WBS Name and active status are mandatory fields' });
+      return;
+    }
+
+    const _wbs = new WBS();
+    _wbs.projectId = req.params.projectId;
+    _wbs.wbsName = req.body.wbsName;
+    _wbs.isActive = req.body.isActive;
+    _wbs.createdDatetime = Date.now();
+    _wbs.modifiedDatetime = Date.now();
+
+    _wbs.save()
       .then(results => res.status(201).send(results))
       .catch(error => res.status(500).send({ error }));
-       
-    };
+  };
 
-    const deleteWBS = function (req, res) {
-      if (req.body.requestor.role !== 'Administrator') {
-        res.status(403).send({ error: 'You are  not authorized to delete projects.' });
+  const deleteWBS = function (req, res) {
+    if (req.body.requestor.role !== 'Administrator') {
+      res.status(403).send({ error: 'You are  not authorized to delete projects.' });
+      return;
+    }
+    const { wbsID } = req.params;
+    WBS.findById(wbsID, (error, record) => {
+      if (error || !record || (record === null) || (record.length === 0)) {
+        res.status(400).send({ error: 'No valid records found' });
         return;
       }
-      const { wbsID } = req.params;
-      WBS.findById(wbsID, (error, record) => {
-        if (error || !record || (record === null) || (record.length === 0)) {
-          res.status(400).send({ error: 'No valid records found' });
-          return;
-        }
-  
-        const removeWBS = record.remove();
 
-        Promise.all([removeWBS])
-          .then(res.status(200).send({ message: ' WBS successfully deleted' }))
-          .catch((errors) => { res.status(400).send(errors); });
+      const removeWBS = record.remove();
 
-      }).catch((errors) => { res.status(400).send(errors); });
-    };
-  
-  
-    return {
-        postWBS,
-        deleteWBS
-    };
+      Promise.all([removeWBS])
+        .then(res.status(200).send({ message: ' WBS successfully deleted' }))
+        .catch((errors) => { res.status(400).send(errors); });
+    }).catch((errors) => { res.status(400).send(errors); });
   };
-  
-  
-  module.exports = wbsController;
+
+
+  return {
+    postWBS,
+    deleteWBS,
+  };
+};
+
+
+module.exports = wbsController;
