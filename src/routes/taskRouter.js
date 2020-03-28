4678291126342ed0524ebdbd5a7d1c6719213149
Henry Nguyen <vnseattleapp@gmail.com>
2020-03-28 15:42:23 -0600

    Added get all tasks

--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -5,9 +5,13 @@ const routes = function (task) {
   const controller = require('../controllers/taskController')(task);
   const wbsRouter = express.Router();
 
+  wbsRouter.route('/tasks/:wbsId')
+    .get(controller.getTasks);
 
   wbsRouter.route('/task/:wbsId')
     .post(controller.postTask);
+
+  
 
   return wbsRouter;
 };
