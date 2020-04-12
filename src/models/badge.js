c58208856d2e363c7123dc07f161ae8217c2b37a
Wen Zhang <zhangwenfd@gmail.com>
2020-04-12 00:27:56 -0400

    feature: add badge colletion

---
+++ src/models/badge.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+// add categoryId after Category colletion is created
+const Badge = new Schema({
+  badgeName: { type: String, required: true },
+  projectId: { type: Schema.Types.ObjectId, required: [true, 'Project is a required field'], ref: 'project' },
+  imageUrl: { type: String },
+  ranking: { type: Number },
+  description: { type: String },
+});
+
+module.exports = mongoose.model('badge', Badge, 'badges');
