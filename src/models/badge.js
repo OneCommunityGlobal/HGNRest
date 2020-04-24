9eaf13bc2715add2dfcde3882d57ea8184a6fa4b
Wen Zhang <zhangwenfd@gmail.com>
2020-04-24 19:46:34 -0400

    feature: add fields for badges

--- src/models/badge.js
+++ src/models/badge.js
@@ -5,6 +5,7 @@ const { Schema } = mongoose;
 // add categoryId after Category colletion is created
 const Badge = new Schema({
   badgeName: { type: String, required: true },
+  category: { type: String, required: true, enum: ['Education', 'Infrastructure', 'Marketing & Promotion', 'Interviews & Hospitality', 'Funding & Partnership Building', 'Other'] },
   projectId: { type: Schema.Types.ObjectId, required: [true, 'Project is a required field'], ref: 'project' },
   imageUrl: { type: String },
   ranking: { type: Number },
