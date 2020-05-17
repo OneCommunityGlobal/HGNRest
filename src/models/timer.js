e2236ff89c1df794cbdd0b4b4fb6c564f50bf681
Tengxiao Wang <fredcatwtx@gmail.com>
2020-05-17 05:19:21 -0700

    Style fix

--- src/models/timer.js
+++ src/models/timer.js
@@ -4,8 +4,8 @@ const { Schema } = mongoose;
 
 const timerSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, required: true, ref: 'userProfile' },
-  pausedAt: {type: Number, default: 0},
-  isWorking: {type: Boolean, default: false},
+  pausedAt: { type: Number, default: 0 },
+  isWorking: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('timer', timerSchema, 'timers');
