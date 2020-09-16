9b2f8d1ebf85945967e49b9731bba7022f412ae1
Jerry Zhang <jerryzhang.dev@gmail.com>
2020-09-16 02:59:24 -0400

    tasks contain why,intent, and endstate info

--- src/models/task.js
+++ src/models/task.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
 const taskschema = new Schema({
-
   taskName: { type: String, required: true },
-  wbsId: { type: mongoose.SchemaTypes.ObjectId, ref: 'wbs', required: true },
+  wbsId: { type: mongoose.SchemaTypes.ObjectId, ref: "wbs", required: true },
   num: { type: String, required: true },
   level: { type: Number, required: true },
-  priority: { type: String, default: 'Primary' },
+  priority: { type: String, default: "Primary" },
   resources: [
     {
       name: { type: String, required: true },
-      userID: { type: mongoose.SchemaTypes.ObjectId, ref: 'userProfiles' },
+      userID: { type: mongoose.SchemaTypes.ObjectId, ref: "userProfiles" },
       profilePic: { type: String },
-    }],
+    },
+  ],
   isAssigned: { type: Boolean, default: true },
-  status: { type: String, default: 'Not Started' },
+  status: { type: String, default: "Not Started" },
   hoursBest: { type: Number, default: 0.0 },
   hoursWorst: { type: Number, default: 0.0 },
   hoursMost: { type: Number, default: 0.0 },
@@ -24,15 +24,31 @@ const taskschema = new Schema({
   startedDatetime: { type: Date },
   dueDatetime: { type: Date },
   links: [String],
-  parentId1: { type: mongoose.SchemaTypes.ObjectId, ref: 'task', default: null },
-  parentId2: { type: mongoose.SchemaTypes.ObjectId, ref: 'task', default: null },
-  parentId3: { type: mongoose.SchemaTypes.ObjectId, ref: 'task', default: null },
-  mother: { type: mongoose.SchemaTypes.ObjectId, ref: 'task', default: null },
+  parentId1: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "task",
+    default: null,
+  },
+  parentId2: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "task",
+    default: null,
+  },
+  parentId3: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "task",
+    default: null,
+  },
+  mother: { type: mongoose.SchemaTypes.ObjectId, ref: "task", default: null },
   position: { type: Number, required: true },
   isActive: { type: Boolean, default: true },
   createdDatetime: { type: Date },
   modifiedDatetime: { type: Date, default: Date.now() },
-
+  taskDetails: {
+    whyInfo: { type: String },
+    intentInfo: { type: String },
+    endstateInfo: { type: String },
+  },
 });
 
-module.exports = mongoose.model('task', taskschema, 'tasks');
+module.exports = mongoose.model("task", taskschema, "tasks");
