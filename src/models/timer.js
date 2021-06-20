c7a03edba2e542db2220c8459853705747335d34
Chris Weilacker <chrisweilacker@gmail.com>
2021-06-20 12:58:43 -0700

    Adding Timer Changes

--- src/models/timer.js
+++ src/models/timer.js
@@ -6,6 +6,8 @@ const timerSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, required: true, ref: 'userProfile' },
   pausedAt: { type: Number, default: 0 },
   isWorking: { type: Boolean, default: false },
+  started: { type: Date },
+  lastAccess: { type: Date },
 });
 
 module.exports = mongoose.model('timer', timerSchema, 'timers');
