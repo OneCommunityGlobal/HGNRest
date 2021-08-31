548d3c65f8616857f836a788035e5b3aa2075715
Cameron White <19396072+CameronWhiteCS@users.noreply.github.com>
2021-08-31 01:52:19 -0400

    Added the ability to edit the number of tangible hours by category (#85)
    Co-authored-by: Cameron White <cameron.white@ufl.edu>

--- src/controllers/userProfileController.js
+++ src/controllers/userProfileController.js
@@ -222,6 +222,7 @@ const userProfileController = function (UserProfile) {
         record.categoryTangibleHrs = req.body.categoryTangibleHrs ? req.body.categoryTangibleHrs : record.categoryTangibleHrs;
         record.totalTangibleHrs = req.body.totalTangibleHrs;
         record.timeEntryEditHistory = req.body.timeEntryEditHistory;
+        record.hoursByCategory = req.body.hoursByCategory;
       }
 
       if (infringmentAuthorizers.includes(req.body.requestor.role)) {
