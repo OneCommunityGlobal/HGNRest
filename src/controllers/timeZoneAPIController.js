e7bcaaa0386f568c40dbe8787c6d6d710530d1ab
Miguel Fernandes <migamado01@gmail.com>
2022-03-23 13:28:00 -0400

    Formatted file

--- src/controllers/timeZoneAPIController.js
+++ src/controllers/timeZoneAPIController.js
@@ -1,12 +1,12 @@
 const timeZoneAPIController = function () {
   const getTimeZoneAPIKey = (req, res) => {
     const requestorRole = req.body.requestor.role;
-    const PremiumRoles = ["Manager", "Administrator", "Core Team"];
+    const PremiumRoles = ['Manager', 'Administrator', 'Core Team'];
     const isPremiumRequestor = !!PremiumRoles.includes(requestorRole);
     const premiumKey = process.env.TIMEZONE_PREMIUM_KEY;
     const commonKey = process.env.TIMEZONE_COMMON_KEY;
     if (!req.body.requestor.role) {
-      res.status(403).send("Unauthorized Request");
+      res.status(403).send('Unauthorized Request');
       return;
     }
     if (isPremiumRequestor) {
