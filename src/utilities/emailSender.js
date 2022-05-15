06d21918a55a6558aa354d8b5d912f9cb0e538a2
Chris Weilacker <chrisweilacker@gmail.com>
2022-05-14 19:02:59 -0700

    Updated Email Sender

--- src/utilities/emailSender.js
+++ src/utilities/emailSender.js
@@ -5,6 +5,31 @@ const logger = require('../startup/logger');
 
 const closure = () => {
   const queue = [];
+
+  const CLIENT_EMAIL = process.env.REACT_APP_EMAIL;
+  const CLIENT_ID = process.env.REACT_APP_EMAIL_CLIENT_ID;
+  const CLIENT_SECRET = process.env.REACT_APP_EMAIL_CLIENT_SECRET;
+  const REDIRECT_URI = process.env.REACT_APP_EMAIL_CLIENT_REDIRECT_URI;
+  const REFRESH_TOKEN = process.env.REACT_APP_EMAIL_REFRESH_TOKEN;
+  // Create the email envelope (transport)
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: CLIENT_EMAIL,
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+    },
+  });
+
+  const OAuth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI,
+  );
+  
+  OAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
   setInterval(async () => {
     const nextItem = queue.shift();
 
@@ -14,48 +39,26 @@ const closure = () => {
       recipient, subject, message, cc, bcc,
     } = nextItem;
 
-    const CLIENT_EMAIL = process.env.REACT_APP_EMAIL;
-    const CLIENT_ID = process.env.REACT_APP_EMAIL_CLIENT_ID;
-    const CLIENT_SECRET = process.env.REACT_APP_EMAIL_CLIENT_SECRET;
-    const REDIRECT_URI = process.env.REACT_APP_EMAIL_CLIENT_REDIRECT_URI;
-    const REFRESH_TOKEN = process.env.REACT_APP_EMAIL_REFRESH_TOKEN;
-    const OAuth2Client = new google.auth.OAuth2(
-      CLIENT_ID,
-      CLIENT_SECRET,
-      REDIRECT_URI,
-    );
-    logger.logInfo('test refresh token : ', REFRESH_TOKEN);
-    OAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
     try {
       // Generate the accessToken on the fly
-      const ACCESSTOKEN = await OAuth2Client.getAccessToken();
-      logger.logInfo(ACCESSTOKEN);
-
-      // Create the email envelope (transport)
-      const transport = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          type: 'OAuth2',
-          user: CLIENT_EMAIL,
-          clientId: CLIENT_ID,
-          clientSecret: CLIENT_SECRET,
-          refreshToken: REFRESH_TOKEN,
-          accessToken: ACCESSTOKEN,
-        },
-      });
-
-      logger.logInfo(transport);
+      const res = await OAuth2Client.getAccessToken();
+      const ACCESSTOKEN = res.token;
 
       const mailOptions = {
-        from: process.env.SMTPUser,
+        from: CLIENT_EMAIL,
         to: recipient,
         cc,
         bcc,
         subject,
         html: message,
+        auth: {
+          user: CLIENT_EMAIL,
+          refreshToken: REFRESH_TOKEN,
+          accessToken: ACCESSTOKEN,
+        }
       };
 
-      const result = await transport.sendMail(mailOptions);
+      const result = await transporter.sendMail(mailOptions);
       logger.logInfo(result);
     } catch (error) {
       logger.logException(error);
