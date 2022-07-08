da8f6ee18679f3232344ba160ec2e34f15fa7ffd
Yiyun Tan <yiyuntan.cs@gmail.com>
2022-07-08 17:45:05 -0600

    make the hoursLogged field work for task

---
+++ src/helpers/timeEntryHelper.js
@@ -0,0 +1,26 @@
+const moment = require('moment-timezone');
+const timeEntry = require('../models/timeentry');
+
+const timeEntryHelper = function () {
+  const getAllHoursLoggedForSpecifiedProject = function (projectId) {
+    const fromDate = moment('1900-01-01 00:00:00').format('YYYY-MM-DD');
+    const toDate = moment().tz('America/Los_Angeles').endOf('week').format('YYYY-MM-DD');
+    return timeEntry.find(
+      {
+        projectId: projectId,
+        dateOfWork: { $gte: fromDate, $lte: toDate },
+      },
+      '-createdDateTime -lastModifiedDateTime',
+    )
+      .then((results) => {
+        const totalSecondsLogged = results.reduce((acc, { totalSeconds }) => acc + totalSeconds, 0);
+        return (totalSecondsLogged / 3600).toFixed(2);
+      });
+  };
+
+  return {
+    getAllHoursLoggedForSpecifiedProject,
+  };
+};
+
+module.exports = timeEntryHelper;
