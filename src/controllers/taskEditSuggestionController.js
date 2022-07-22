80fe85ce57f32bab6c833c4958ed5c1b07882c8e
Eiki Kan <eikikan89@gmail.com>
2022-07-22 14:54:12 -0400

    Add controller methods

--- src/controllers/taskEditSuggestionController.js
+++ src/controllers/taskEditSuggestionController.js
@@ -1,61 +1,38 @@
 /* eslint-disable linebreak-style */
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 const taskEditSuggestionController = function (TaskEditSuggestion) {
-  const getUnreadTaskNotificationsByUser = function (req, res) {
-    const { userId } = req.params;
-
-    TaskEditSuggestion.find({ recipient: userId, isRead: false })
-      .then((results) => {
-        res.status(200).send(results);
-      })
-      .catch((errors) => {
-        res.status(400).send(errors);
-      });
-  };
-
-  const createOrUpdateTaskNotification = async (req, res) => {
+  const createTaskEditSuggestion = async function (req, res) {
     try {
-      const taskId = mongoose.Types.ObjectId(req.params.taskId);
-      const { oldTask, userIds } = req.body;
-      await Promise.all(
-        userIds.map(async userId => (
-          TaskEditSuggestion.updateOne({
-            $and: [
-              { taskId },
-              { userId: mongoose.Types.ObjectId(userId) },
-            ],
-          },
-          {
-            $setOnInsert: {
-              oldTask,
-            },
-          },
-          {
-            upsert: true,
-            setDefaultsOnInsert: true,
-          })
-        )),
-      );
-
-      res.status(200).send({ message: 'Create or updated task notification' });
+      const taskEditSuggestion = new TaskEditSuggestion();
+      taskEditSuggestion.userId = req.body.userId;
+      taskEditSuggestion.user = req.body.user;
+      taskEditSuggestion.dateSuggested = Date.now();
+      taskEditSuggestion.taskId = req.body.taskId;
+      taskEditSuggestion.oldTask = req.body.oldTask;
+      taskEditSuggestion.newTask = req.body.newTask;
+      const result = await taskEditSuggestion.save();
+      res.status(201).send(result);
     } catch (error) {
-      console.log(error);
       res.status(400).send(error);
     }
   };
 
-  const deleteTaskNotification = function (req, res) {
+  const findAllTaskEditSuggestions = async function (req, res) {
+    try {
+      const result = await TaskEditSuggestion.find();
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  };
+
+  const deleteTaskEditSuggestion = function (req, res) {
     TaskEditSuggestion.findById(req.params.taskNotificationId)
       .then((result) => {
-        // // verify is requestor same as assignee
-        // if (req.body.requestor.requestorId !== result.recipient) {
-        //   res.status(403).send({ error: "Unauthroized request" });
-        //   return;
-        // }
         result
           .remove()
-          .then(res.status(200).send({ message: 'Deleted task notification' }))
+          .then(res.status(200).send({ message: 'Deleted task edit suggestion' }))
           .catch((error) => {
             res.status(400).send(error);
           });
@@ -65,29 +42,10 @@ const taskEditSuggestionController = function (TaskEditSuggestion) {
       });
   };
 
-  const markTaskNotificationAsRead = function (req, res) {
-    const { notificationId } = req.params;
-    TaskEditSuggestion.findById(notificationId)
-      .then((result) => {
-        if (result) {
-          result.isRead = true;
-          result.dateRead = Date.now();
-          result
-            .save()
-            .then(notification => res.status(200).send(notification))
-            .catch(error => res.status(400).send(error));
-        } else {
-          res.status(404).send('TaskNotification not found.');
-        }
-      })
-      .catch(error => res.status(400).send(error));
-  };
-
   return {
-    getUnreadTaskNotificationsByUser,
-    deleteTaskNotification,
-    createOrUpdateTaskNotification,
-    markTaskNotificationAsRead,
+    createTaskEditSuggestion,
+    findAllTaskEditSuggestions,
+    deleteTaskEditSuggestion,
   };
 };
 
