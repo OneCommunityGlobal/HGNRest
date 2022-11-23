a3a15df828660ebf9b9c8acf576f3dcd29218533
esk47 <66339186+esk47@users.noreply.github.com>
2022-11-22 22:33:49 -0500

    Ek task edit suggest (#186)
    * Define task edit suggestion schema
    
    * Init route, router, controller
    
    * Add create, read, delete to router
    
    * Add controller methods
    
    * Fix task edit suggestion post
    
    * Implement reject task edit suggestion
    
    * Implement count task edit suggestions
    
    * Store projectId and wbsId in oldTask
    
    * Allow only one task edit suggestion per task
    
    * Add create and update logic for task edit suggestions
    
    * Remove old comments
    
    * Fix edit resources undefined bug
    
    * remove eslint comments
    
    Co-authored-by: Yiyun Tan <yiyuntan.cs@gmail.com>
    Co-authored-by: Guilherme Wustro <gui.wustro@gmail.com>

---
+++ src/routes/taskEditSuggestionRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const routes = function (TaskEditSuggestion) {
+  const controller = require('../controllers/taskEditSuggestionController')(
+    TaskEditSuggestion,
+  );
+  const TaskEditSuggestionRouter = express.Router();
+
+  TaskEditSuggestionRouter.route('/taskeditsuggestion')
+    .post(controller.createOrUpdateTaskEditSuggestion);
+
+  TaskEditSuggestionRouter.route('/taskeditsuggestion')
+    .get(controller.findAllTaskEditSuggestions);
+
+  TaskEditSuggestionRouter.route('/taskeditsuggestion/:taskEditSuggestionId')
+    .delete(controller.deleteTaskEditSuggestion);
+
+  return TaskEditSuggestionRouter;
+};
+
+module.exports = routes;
