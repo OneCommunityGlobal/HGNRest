2cd9275af52e25595ba0f5c32ea1d11a51168415
Alan Chan <67836976+LeeSingChanYau@users.noreply.github.com>
2022-12-02 14:09:19 -0800

    Fixed the misspelling of infringement. (#220)
    * Fixed the misspelling of infringement.
    
    * Changed some variables to camel case.
    
    Co-authored-by: lee sing chan <leesingchan@Lees-MacBookPro14.local>

--- src/cronjobs/userProfileJobs.js
+++ src/cronjobs/userProfileJobs.js
@@ -1,7 +1,7 @@
 const { CronJob } = require('cron');
 const moment = require('moment-timezone');
 
-const userhelper = require('../helpers/userhelper')();
+const userhelper = require('../helpers/userHelper')();
 
 
 const userProfileJobs = () => {
