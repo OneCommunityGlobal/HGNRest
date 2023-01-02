fb887b9e1af624255497ef90b1975ddc6212f4d9
taldoflemis <coderflemis@gmail.com>
2023-01-02 18:47:12 -0300

    Feat: Add New Timer System
    add timer service and timer api
    
    add documentation to websockets server
    
    change the default values of timer model
    
    set max hours of countdown goal as 10 hours
    
    set time to 0 if elapsed timer is negative

--- src/websockets/index.js
+++ src/websockets/index.js
@@ -1,42 +1,110 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable operator-linebreak */
 /* eslint-disable consistent-return */
-/* eslint-disable no-var */
 /* eslint-disable quotes */
 /* eslint-disable linebreak-style */
 const WebSocket = require("ws");
-const { v4: uuidv4 } = require("uuid");
-const exitHook = require('async-exit-hook');
-const setupTimerService = require("./TimerService/index").default;
-const {
-  redisClients,
-  authenticate,
-  listener,
-  handleMessage,
-  TIMER_UPDATES_CHANNEL,
-  updateClientsList,
-  getUserConnectionKey,
-  handleClose,
-  syncRedisDatabaseOnShutDown,
-} = require("./api");
-const logger = require('../startup/logger');
+const moment = require("moment");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+const { getTimer, handleMessage, action } = require("./TimerService/");
 
-export default async (expressServer) => {
-  var timerService = await setupTimerService();
-  var clients = {};
+/*
+Here we authenticate the user.
+We get the token from the headers and try to verify it.
+If it fails, we throw an error.
+Else we check if the token is valid and if it is, we return the user id.
+*/
+export const authenticate = (req, res) => {
+  const authToken = req.headers?.["sec-websocket-protocol"];
+  let payload = "";
+  try {
+    payload = jwt.verify(authToken, config.JWT_SECRET);
+  } catch (error) {
+    res("401 Unauthorized", null);
+  }
 
-  await Promise.all([
-    redisClients.publisher.connect(),
-    redisClients.subscriber.connect(),
-    redisClients.main.connect(),
-  ]);
+  if (
+    !payload ||
+    !payload.expiryTimestamp ||
+    !payload.userid ||
+    !payload.role ||
+    moment().isAfter(payload.expiryTimestamp)
+  ) {
+    res("401 Unauthorized", null);
+  }
 
-  // Initialize websocket
-  const websocketServer = new WebSocket.Server({
+  res(null, payload.userid);
+};
+
+/*
+ * Here we insert the new connection to the connections map.
+ * If the user is not in the map, we create a new entry with the user id as key and the connection as value.
+ * Else we just push the connection to the array of connections.
+ */
+const insertNewUser = (connections, userId, wsConn) => {
+  const userConnetions = connections.get(userId);
+  if (!userConnetions) connections.set(userId, [wsConn]);
+  else userConnetions.push(wsConn);
+};
+
+/*
+ *Here we remove the connection from the connections map.
+ *If the user is not in the map, we do nothing.
+ *Else we remove the connection from the array of connections.
+ *If the array is empty, we delete the user from the map.
+ */
+const removeConnection = (connections, userId, connToRemove) => {
+  const userConnetions = connections.get(userId);
+  if (!userConnetions) return;
+
+  const newConns = userConnetions.filter((conn) => conn !== connToRemove);
+  if (newConns.length === 0) connections.delete(userId);
+  else connections.set(userId, newConns);
+};
+
+/*
+ * Here we broadcast the message to all the connections that are connected to the same user.
+ * We check if the connection is open before sending the message.
+ */
+const broadcastToSameUser = (connections, userId, data) => {
+  const userConnetions = connections.get(userId);
+  if (!userConnetions) return;
+  userConnetions.forEach((conn) => {
+    if (conn.readyState === WebSocket.OPEN) conn.send(data);
+  });
+};
+
+/*
+ * Here we check if there is another connection to the same user.
+ * If there is, we return true.
+ * Else we return false.
+ */
+const checkOtherConn = (connections, anotherConn, userId) => {
+  const userConnetions = connections.get(userId);
+  if (!userConnetions) return false;
+  for (const con of userConnetions) {
+    if (con !== anotherConn && con.readyState === WebSocket.OPEN) return true;
+  }
+  return false;
+};
+
+/*
+Here we start the timer service.
+First we create a map to store the clients and start the Websockets Server.
+Then we set the upgrade event listener to the Express Server, authenticate the user and
+if it is valid, we add the user id to the request and handle the upgrade and emit the connection event.
+*/
+export default async (expServer) => {
+  const clients = new Map();
+  const connections = new Map();
+
+  const wss = new WebSocket.Server({
     noServer: true,
     path: "/timer-service",
-    clientTracking: true,
   });
 
-  expressServer.on("upgrade", (request, socket, head) => {
+  expServer.on("upgrade", (request, socket, head) => {
     authenticate(request, (err, client) => {
       if (err || !client) {
         socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
@@ -44,84 +112,72 @@ export default async (expressServer) => {
         return;
       }
       request.userId = client;
-      websocketServer.handleUpgrade(request, socket, head, (websocket) => {
-        websocketServer.emit("connection", websocket, request);
+      wss.handleUpgrade(request, socket, head, (websocket) => {
+        wss.emit("connection", websocket, request);
       });
     });
   });
 
+  /*
+  For each new connection we start a timer of 5min to check if the connection is alive.
+  If it is, we then repeat the process. If it is not, we terminate the connection.
+  */
+  wss.on("connection", async (ws, req) => {
+    ws.isAlive = true;
+    ws.on("pong", () => {
+      ws.isAlive = true;
+    });
 
-  await redisClients.subscriber.subscribe(TIMER_UPDATES_CHANNEL, message => listener(message, { clients }));
+    const { userId } = req;
 
-  redisClients.main.on("error", err => logger.logException(err));
+    insertNewUser(connections, userId, ws);
 
-  // This is an clean up to help make sure the data is sync'd with Redis.
-  exitHook((callback) => {
-    syncRedisDatabaseOnShutDown(callback, { clients, redisPassedInClients: redisClients, timerService });
+    /*
+     * Here we get the timer from memory or from the database and send it to the client.
+     * We don't broadcast it
+     */
+    await getTimer(clients, userId);
+    ws.send(await handleMessage(action.GET_TIMER, clients, userId));
+
+    /*
+    Here we handle the messages from the client.
+    And we broadcast the response to all the clients that are connected to the same user.
+    */
+    ws.on("message", async (data) => {
+      const resp = await handleMessage(data, clients, userId);
+      broadcastToSameUser(connections, userId, resp);
+    });
+
+    /*
+    Here we handle the close event.
+    If there is another connection to the same user, we don't do anything.
+    Else he is the last connection and we do a forced pause if need be.
+    This may happen if the user closes all the tabs or the browser or he lost connection with
+    the service
+    We then remove the connection from the connections map.
+    */
+    ws.on("close", async () => {
+      if (!checkOtherConn(connections, ws, userId)) {
+        await handleMessage(action.FORCED_PAUSE, clients, userId);
+      }
+      removeConnection(connections, userId, ws);
+    });
   });
 
-  websocketServer.on(
-    "connection",
-    async (websocketConnection, connectionRequest) => {
-      let interval;
-       // Grab userId from connection
-      const userId = `${connectionRequest.userId}`;
+  // The function to check if the connection is alive
+  const interval = setInterval(async () => {
+    wss.clients.forEach(async (ws) => {
+      if (ws.isAlive === false) return ws.terminate();
 
-      websocketConnection.on("message", message => handleMessage(message, { timerService, userId, websocketConnection }));
-      websocketConnection.on("close", () => handleClose({
-        timerService,
-        userId,
-        websocketConnection,
-        interval,
-        clients,
-      }));
-      // Ping and ping method to keep websocket / client persistent
-      function keepConnectionAlive() {
-        function heartbeat() {
-          this.isAlive = true;
-        }
+      ws.isAlive = false;
+      ws.ping();
+    });
+  }, 3000000);
 
-        // Set websocket connection to isAlive true
-        websocketConnection.isAlive = true;
+  // Here we just clear the interval when the server closes
+  wss.on("close", () => {
+    clearInterval(interval);
+  });
 
-        // set handler
-        websocketConnection.on("pong", heartbeat);
-
-        interval = setInterval(() => {
-          websocketServer.clients.forEach((ws) => {
-            if (ws.isAlive === false) return ws.terminate();
-            ws.isAlive = false;
-            ws.ping();
-          });
-        }, 30000);
-      }
-
-
-      // Generate UUID to map between client / server
-      const connectionKey = uuidv4();
-
-      // Assign each connection the UUID
-      websocketConnection.id = connectionKey;
-
-      // Get current active connection from redis database
-      const currentConnections = (await redisClients.main.get(getUserConnectionKey(userId))) ?? 0;
-
-      // Set in redis database number of users connected
-      await redisClients.main.set(getUserConnectionKey(userId), `${+currentConnections + 1}`);
-
-      // Add to websocket and update in-memory client mapping
-      clients = updateClientsList({
-        clients,
-        userId,
-        connectionKey,
-        websocketConnection,
-      });
-
-
-      // keep connection alive between client / server
-      keepConnectionAlive();
-    },
-  );
-
-  return websocketServer;
+  return wss;
 };
