a2aeac63a18197d85118e3780885c6ef26c9aed3
mr.suigun <94319381+suigunfjc@users.noreply.github.com>
2023-04-24 14:03:54 -0700

    update postUserProfile to update cache correctly after new user creation (#331)

--- src/controllers/userProfileController.js
+++ src/controllers/userProfileController.js
@@ -193,16 +193,19 @@ const userProfileController = function (UserProfile) {
 
         // update backend cache
 
-        const userCache = `{"isActive":${true},"weeklyComittedHours":${up.weeklyComittedHours},
-                            "createdDate":"${up.createdDate.toISOString()}","_id":"${
-          up._id
-        }","role":"${up.role}",
-                            "firstName":"${up.firstName}","lastName":"${up.lastName}","email":"${
-          up.email
-        }"}`;
-        const userCacheJson = JSON.parse(userCache);
+        const userCache = {
+          permissions: up.permissions,
+          isActive: true,
+          weeklycommittedHours: up.weeklycommittedHours,
+          createdDate: up.createdDate.toISOString(),
+          _id: up._id,
+          role: up.role,
+          firstName: up.firstName,
+          lastName: up.lastName,
+          email: up.email,
+        };
         const allUserCache = JSON.parse(cache.getCache('allusers'));
-        allUserCache.push(userCacheJson);
+        allUserCache.push(userCache);
         cache.setCache('allusers', JSON.stringify(allUserCache));
       })
       .catch(error => res.status(501).send(error));
