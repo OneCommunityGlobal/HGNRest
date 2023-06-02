867e2064b7a9b22358f415ff1dfbf1e08ae4d2bd
sav <savcostabile@gmail.com>
2023-06-02 09:32:03 -0400

    commit 3

--- src/cronjobs/userProfileJobs.js
+++ src/cronjobs/userProfileJobs.js
@@ -1,14 +1,14 @@
-const { CronJob } = require("cron");
-const moment = require("moment-timezone");
+const { CronJob } = require('cron');
+const moment = require('moment-timezone');
 
-const userhelper = require("../helpers/userHelper")();
+const userhelper = require('../helpers/userHelper')();
 
 const userProfileJobs = () => {
   const allUserProfileJobs = new CronJob(
-    "1 * * * *", // Every day, 1 minute past midnight (PST).
+    '1 * * * *', // Every day, 1 minute past midnight (PST).
     async () => {
       const SUNDAY = 0;
-      if (moment().tz("America/Los_Angeles").day() === SUNDAY) {
+      if (moment().tz('America/Los_Angeles').day() === SUNDAY) {
         await userhelper.assignBlueSquareForTimeNotMet();
         await userhelper.applyMissedHourForCoreTeam();
         await userhelper.emailWeeklySummariesForAllUsers();
@@ -20,7 +20,7 @@ const userProfileJobs = () => {
     },
     null,
     false,
-    "America/Los_Angeles"
+    'America/Los_Angeles',
   );
 
   allUserProfileJobs.start();
