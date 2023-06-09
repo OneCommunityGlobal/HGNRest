59bab1d9cbf3d79a38369b790d91a2fea3edec78
EdwinLauMack <elaumack@ucsd.edu>
2023-06-09 15:24:08 -0700

    Update

--- src/models/userProfile.js
+++ src/models/userProfile.js
@@ -59,7 +59,7 @@ const userProfileSchema = new Schema({
     {
       badge: { type: mongoose.SchemaTypes.ObjectId, ref: 'badge' },
       count: { type: Number, default: 0 },
-      earnedDate:{type: Array, default:[]},
+      earnedDate: { type: Array, default: [] },
       lastModified: { type: Date, required: true, default: Date.now() },
       featured: {
         type: Boolean,
@@ -142,7 +142,7 @@ const userProfileSchema = new Schema({
   timeZone: { type: String, required: true, default: 'America/Los_Angeles' },
   isVisible: { type: Boolean, default: false },
   weeklySummaryOption: { type: String },
-  bioPosted: { type: String, default: false },
+  bioPosted: { type: String, default: 'default' },
 });
 
 userProfileSchema.pre('save', function (next) {
