e6bd59c2ae580324e24da3422a98de770c7acf3a
leonzh2k <leonzh2k@gmail.com>
2023-07-06 17:55:26 -0400

    add new fields to send in UserProfileController
    Add jobTitle and location fields to the userProfile objects
    that are sent back to the client when requesting all userProfiles.
    These two bits of info are displayed on the interactive map.

--- src/controllers/userProfileController.js
+++ src/controllers/userProfileController.js
@@ -65,7 +65,7 @@ const userProfileController = function (UserProfile) {
     
     UserProfile.find(
       {},
-      '_id firstName lastName role weeklycommittedHours email permissions isActive reactivationDate createdDate endDate location jobTitle totalTangibleHrs',
+      '_id firstName lastName role weeklycommittedHours email permissions isActive reactivationDate createdDate endDate location jobTitle',
     )
       .sort({
         lastName: 1,
