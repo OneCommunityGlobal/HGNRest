d312effebbd5cd28132c3ac9bc3bcb4712646a0f
lchua2314 <lchua2314@gmail.com>
2023-07-17 18:40:44 -0700

    Hide sensitive data

--- src/startup/db.js
+++ src/startup/db.js
@@ -3,26 +3,33 @@ const mongoose = require('mongoose');
 const logger = require('./logger');
 const userProfile = require('../models/userProfile');
 const initialPermissions = require('../utilities/createInitialPermissions');
+const config = require('../config');
 
+const {
+  DEF_PWD,
+  TIME_ARCHIVE_FIRST_NAME,
+  TIME_ARCHIVE_LAST_NAME,
+  TIME_ARCHIVE_EMAIL,
+  } = config;
 mongoose.Promise = Promise;
 
 const afterConnect = async () => {
   try {
     const user = await userProfile.findOne(
       {
-        firstName: { $regex: 'TimeArchiveAccount', $options: 'i' },
-        lastName: { $regex: 'TimeArchiveAccount', $options: 'i' },
+        firstName: { $regex: TIME_ARCHIVE_FIRST_NAME, $options: 'i' },
+        lastName: { $regex: TIME_ARCHIVE_LAST_NAME, $options: 'i' },
       },
     );
 
     await initialPermissions();
     if (!user) {
       userProfile.create({
-        firstName: 'TimeArchiveAccount',
-        lastName: 'TimeArchiveAccount',
-        email: 'TimeArchiveAccount@yopmail.com',
+        firstName: TIME_ARCHIVE_FIRST_NAME,
+        lastName: TIME_ARCHIVE_LAST_NAME,
+        email: TIME_ARCHIVE_EMAIL,
         role: 'Volunteer',
-        password: '123Welcome!',
+        password: DEF_PWD,
       })
         .then(result => logger.logInfo(`TimeArchive account was created with id of ${result._id}`))
         .catch(error => logger.logException(error));
