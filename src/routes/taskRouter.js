1f6f7ca6f2b173973ca59188ff6e030f57c91284
AriaYu927 <yl8651@nyu.edu>
2023-07-24 14:43:43 +0800

    revert send email

--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -46,9 +46,6 @@ const routes = function (task, userProfile) {
   wbsRouter.route('/user/:userId/teams/tasks')
     .get(controller.getTasksForTeamsByUser);
 
-  wbsRouter.route('/dashboard/reviewreq/:userId')
-    .post(controller.sendReviewReq);
-
   return wbsRouter;
 };
 
