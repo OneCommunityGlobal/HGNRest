6057cd308dcf598d7623fcff7e4ea1d35e77a0a1
Nathan Hoffman <nathanahoffman@gmail.com>
2023-08-02 15:38:45 -0700

    added back other accounts

--- src/utilities/permissions.js
+++ src/utilities/permissions.js
@@ -15,11 +15,13 @@ const hasPermission = async (role, action) => {
   return isAllowed;
 };
 
-const canRequestorUpdateUser = (requestorId, userId) => !(
-  userId === '64c17eb8c737b05dd4ac4e28' // 'devadmin@hgn.net'
-    && (
-      requestorId !== '63feae337186de1898fa8f51' // 'jae@onecommunityglobal.org'
-    )
-);
+const canRequestorUpdateUser = (requestorId, userId) => {
+  const allowedIds = ['63feae337186de1898fa8f51', // jae@onecommunityglobal.org
+                    '5baac381e16814009017678c', // one.community@me.com
+                    '63fe855b7186de1898fa8ab7', // jsabol@me.com
+                  ];
+  return !(userId === '64c17eb8c737b05dd4ac4e28' // 'devadmin@hgn.net'
+    && allowedIds.contains(requestorId));
+};
 
 module.exports = { hasPermission, canRequestorUpdateUser };
