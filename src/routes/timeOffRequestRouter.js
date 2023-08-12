dfbe73313344a9b193a88a9281d1da153ff95ec2
abdel <abdelmounaim.lallouache@gmail.com>
2023-08-12 15:05:41 -0500

    add setTimeOffRequest,add  getTimeOffRequests,add getTimeOffRequestbyId

--- src/routes/timeOffRequestRouter.js
+++ src/routes/timeOffRequestRouter.js
@@ -5,8 +5,17 @@ const routes = function (timeOffRequest) {
     const timeOffRequestRouter = express.Router();
     const controller = require('../controllers/timeOffRequestController')(timeOffRequest);
 
+    timeOffRequestRouter.route('/setTimeOffRequest')
+        .post(controller.setTimeOffRequest);
+
     timeOffRequestRouter.route('/getTimeOffRequests')
         .get(controller.getTimeOffRequests);
+
+    timeOffRequestRouter.route('/getTimeOffRequest/:id')
+        .get(controller.getTimeOffRequestbyId);
+
+    timeOffRequestRouter.route('/updateTimeOffRequest')
+        .post(controller.updateTimeOffRequest);
 
     return timeOffRequestRouter;
 };
