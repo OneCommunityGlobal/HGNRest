68e9033e60b989e1e580214e990abda081f69dc5
ShihaoXiong <shihao.xiong@icloud.com>
2023-08-18 15:11:24 -0400

    Update syntax

--- src/utilities/addMembersToTeams.js
+++ src/utilities/addMembersToTeams.js
@@ -3,43 +3,37 @@
  * It will create a new field called "members"
  * and it will populate it with users' created Date ny default
  */
-require('dotenv').load();
+require("dotenv").load();
 
-const mongoose = require('mongoose');
-const logger = require('../startup/logger');
-const UserProfile = require('../models/userProfile');
-const Teams = require('../models/team');
+const mongoose = require("mongoose");
+const logger = require("../startup/logger");
+const UserProfile = require("../models/userProfile");
+const Teams = require("../models/team");
 
 const addMembersField = async () => {
-  await Teams.updateMany({}, { $set: { members: [] } }).catch(error => logger.logException('Error adding field:', error));
+  await Teams.updateMany({}, { $set: { members: [] } }).catch(error =>
+    logger.logException("Error adding field:", error)
+  );
 
   const allUsers = await UserProfile.find({});
   const updateOperations = allUsers
-    .map((user) => {
+    .map(user => {
       const { _id, teams, createdDate } = user;
-      return teams.map(team => Teams.updateOne({ _id: team }, { $addToSet: { members: { userId: _id, addDateTime: createdDate } } }));
+      return teams.map(team =>
+        Teams.updateOne({ _id: team }, { $addToSet: { members: { userId: _id, addDateTime: createdDate } } })
+      );
     })
     .flat();
 
-  Promise.all(updateOperations)
-    .catch(error => logger.logException(error))
-    .finally(() => {
-      mongoose.connection.close();
-      console.log('Done! ✅');
-    });
+  await Promise.all(updateOperations).catch(error => logger.logException(error));
 };
 
-const deleteMemberField = () => {
-  Teams.updateMany({}, { $unset: { members: '' } })
-    .catch(err => console.error(err))
-    .finally(() => {
-      mongoose.connection.close();
-      console.log('Done! ✅');
-    });
+const deleteMembersField = async () => {
+  await Teams.updateMany({}, { $unset: { members: "" } }).catch(err => console.error(err));
 };
 
 const run = () => {
-  console.log('Loading... This may take a few minutes!');
+  console.log("Loading... This may take a few minutes!");
   const uri = `mongodb://${process.env.user}:${encodeURIComponent(process.env.password)}@${process.env.cluster}/${
     process.env.dbName
   }?ssl=true&replicaSet=${process.env.replicaSetName}&authSource=admin`;
@@ -48,11 +42,15 @@ const run = () => {
     .connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
+      useFindAndModify: false
     })
-    // .then(deleteMemberField)
+    // .then(deleteMembersField)
     .then(addMembersField)
-    .catch(err => logger.logException(err));
+    .catch(err => logger.logException(err))
+    .finally(() => {
+      mongoose.connection.close();
+      console.log("Done! ✅");
+    });
 };
 
 run();
