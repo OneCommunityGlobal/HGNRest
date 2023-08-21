74707325c3bbe18fb9672d5e776cef9b6034d9e5
Nathan Hoffman <nathanahoffman@gmail.com>
2023-08-20 20:03:04 -0700

    Protect Jae's dev accounts as well

--- src/utilities/permissions.js
+++ src/utilities/permissions.js
@@ -16,12 +16,16 @@ const hasPermission = async (role, action) => {
 };
 
 const canRequestorUpdateUser = (requestorId, userId) => {
-  const allowedIds = ['63feae337186de1898fa8f51', // jae@onecommunityglobal.org
-                    '5baac381e16814009017678c', // one.community@me.com
-                    '63fe855b7186de1898fa8ab7', // jsabol@me.com
-                  ];
-  return !(userId === '64c17eb8c737b05dd4ac4e28' // 'devadmin@hgn.net'
-    && !allowedIds.includes(requestorId));
+  const allowedIds = ['63feae337186de1898fa8f51', // dev jae@onecommunityglobal.org
+                      '5baac381e16814009017678c', // dev one.community@me.com
+                      '63fe855b7186de1898fa8ab7', // dev jsabol@me.com
+                    ];
+  const protectedIds = ['63feae337186de1898fa8f51', // dev jae@onecommunityglobal.org
+                        '5baac381e16814009017678c', // dev one.community@me.com
+                        '63fe855b7186de1898fa8ab7', // dev jsabol@me.com
+                        '64c17eb8c737b05dd4ac4e28', // dev devadmin@hgn.net
+                      ];
+  return !(protectedIds.includes(userId) && !allowedIds.includes(requestorId));
 };
 
 module.exports = { hasPermission, canRequestorUpdateUser };
