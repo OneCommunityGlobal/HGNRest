8deb1b9c0616c9c52dfaf41255a80d83a4668554
Aaron Persaud <aaronpersaud@hotmail.com>
2023-09-03 04:46:52 -0400

    add time

--- src/cronjobs/tempJob.js
+++ src/cronjobs/tempJob.js
@@ -4,7 +4,7 @@ const userhelper = require('../helpers/userHelper')();
 
 const tempJob = () => {
     const shiftSummary = new CronJob(
-        '45 * * * *', // Every hh:52.
+        '0 * * * *', // Every hh:52.
         async () => {
             await userhelper.assignBlueSquareForTimeNotMet();
         },
