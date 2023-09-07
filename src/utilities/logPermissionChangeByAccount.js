004f5ca52b49fdd9f3f429135e92ac5c70acdbc0
Jerry Ren <jerryren.5279@gmail.com>
2023-09-07 11:00:06 -0400

    Edit logging function to accomodate new req.body

--- src/utilities/logPermissionChangeByAccount.js
+++ src/utilities/logPermissionChangeByAccount.js
@@ -9,33 +9,24 @@ const changedPermissionsLogger = async (req, res, next) => {
 
 // Function saves logs to hgnData_dev.permissionChangeLogs collection
 const logPermissionChangeByAccount = async (requestBody) => {
-  const { updatedRole, userProfile, requestor } = requestBody
+  const { roleId, roleName, permissions, requestor, role, email } = requestBody
   const dateTime = moment().tz("America/Los_Angeles").format();
   
   try {
-    // Extract relevant data from the request
-    const { roleId, roleName, permissions } = updatedRole; // Adjust this as per your request body structure
-    const { role, email } = userProfile;
-    const { requestorId } = requestor;
-
-    // Create a new PermissionChangeLog object
     const logEntry = new PermissionChangeLog({
       logDateTime: dateTime,
       roleId: roleId,
       roleName: roleName,
       permissions: permissions,
-      requestorId: requestorId,
+      requestorId: requestor.requestorId,
       requestorRole: role,
       requestorEmail: email,
     });
 
-    // Save the log entry to the database
     await logEntry.save();
 
   } catch (error) {
-    // Handle any errors that occur during logging
     console.error('Error logging permission change:', error);
-    // You can choose to send an error response to the client or handle it differently
     res.status(500).json({ error: 'Failed to log permission change' });
   }
 }
