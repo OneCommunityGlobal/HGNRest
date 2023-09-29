0364c42f3f1ef9019fe2829d344ff3e34b439418
Tim Kent <timkentdev@gmail.com>
2023-09-29 13:35:30 -0700

    add material schema, update item type schema

--- src/models/inventoryItemType.js
+++ src/models/inventoryItemType.js
@@ -2,11 +2,24 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const InventoryItemType = new Schema({
+// const InventoryItemType = new Schema({
+//   name: { type: String, required: true },
+//   description: { type: String },
+//   imageUrl: { type: String },
+//   quantifier: { type: String, default: 'each' },
+// });
+
+const InventoryItemType = new Schema({ // creates an item, tracks total amount in organization's stock
+  type: { type: String, required: true }, // ie Material, Equipment, Tool
   name: { type: String, required: true },
-  description: { type: String },
-  imageUrl: { type: String },
-  quantifier: { type: String, default: 'each' },
+  description: { type: String, required: true, maxLength: 150 },
+  uom: { type: String, required: true }, // unit of measurement
+  totalStock: { type: Number, required: true }, // total amount of all stock acquired
+  totalAvailable: { type: Number, required: true }, // amount of item available to be checked out
+  projectsUsing: [String], // ids of projects using the item
+  imageUrl: { type: String }
 });
 
 module.exports = mongoose.model('inventoryItemType', InventoryItemType, 'inventoryItemType');
+
+
