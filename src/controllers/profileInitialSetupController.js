beb19d7f2ffe42733ed1b743f11589786712362e
abdelmounaim lallouache <abdelmounaim.lallouache@gmail.com>
2023-09-30 14:49:09 -0500

    update email body for new user setup

--- src/controllers/profileInitialSetupController.js
+++ src/controllers/profileInitialSetupController.js
@@ -22,7 +22,16 @@ function sendLinkMessage(Link) {
 function informManagerMessage(user) {
   const message = `
     <p>Hello,</p>
-    <p>New User <b>${user.firstName} ${user.lastName}</b> has completed their part of setup.<br> These areas need to now be completed by an Admin: Weekly Committed Hours, Admin Document, Link to Media Files, Assign Projects, and (if applicable) Assign Team.</p>
+  <p>New User <b>${user.firstName} ${user.lastName}</b> has completed their part of setup.</p>
+  <p>These areas need to now be completed by an Admin:</p>
+  <ul>
+    <li>Weekly Committed Hours</li>
+    <li>Admin Document</li>
+    <li>Link to Media Files</li>
+    <li>Assign Projects</li>
+    <li>4-digit Admin Code</li>
+    <li>And (if applicable) Assign Team</li>
+  </ul>
     <table border="1" cellpadding="10">
         <tr>
             <td><strong>First Name:</strong></td>
