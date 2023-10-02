be7bce6990713dbcfc191eb4979c67f2d50c9390
Oleksandr Riazantsev <riazantsev.aleks@gmail.com>
2023-10-02 16:13:42 -0400

    fix: jobTitle model key, feat: adding isActive status, getting all locations, put new location to a collection

--- src/controllers/mapLocationsController.js
+++ src/controllers/mapLocationsController.js
@@ -1,62 +1,44 @@
-const mongoose = require('mongoose');
-const mapLocation = require('../models/mapLocation');
-const { hasPermission } = require('../utilities/permissions');
-
-const mapLocationsController = function () {
+const mapLocationsController = function (mapLocation) {
   const getAllLocations = function (req, res) {
-    console.log('controller:')
-    console.log(req.body)
 
     mapLocation.find({})
-      .then(results => res.send(results).status(200))
-      .catch(error => res.send(error).status(404));
+      .then(results =>
+        res.send(results).status(200)
+      )
+      .catch(error => 
+        res.send(error).status(404));
   };
   const deleteLocation = async function (req, res) {
-    if (!await hasPermission(req.body.requestor.role, 'deleteTeam')) {
-      res.status(403).send({ error: 'You are not authorized to delete teams.' });
-      return;
-    }
-    const { teamId } = req.params;
-    Team.findById(teamId, (error, record) => {
-      if (error || record === null) {
-        res.status(400).send({ error: 'No valid records found' });
-        return;
-      }
-      const removeteamfromprofile = userProfile.updateMany({}, { $pull: { teams: record._id } }).exec();
-      const deleteteam = record.remove();
 
-      Promise.all([removeteamfromprofile, deleteteam])
-        .then(res.status(200).send({ message: ' Team successfully deleted and user profiles updated' }))
-        .catch((errors) => {
-          res.status(400).send(errors);
-        });
-    }).catch((error) => {
-      res.status(400).send(error);
-    });
-  };
-  const putUserLocation = async function (req, res) {
-    if (!await hasPermission(req.body.requestor.role, 'putTeam')) {
+    if (!req.body.requestor.role === 'Administrator' || !req.body.requestor.role === 'Owner') {
       res.status(403).send('You are not authorized to make changes in the teams.');
       return;
     }
+  };
+  const putUserLocation = async function (req, res) {
 
-    const { teamId } = req.params;
+    if (!req.body.requestor.role === 'Administrator' || !req.body.requestor.role === 'Owner') {
+      res.status(403).send('You are not authorized to make changes in the teams.');
+      return;
+    }
+    const locationData = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      jobTitle: req.body.jobTitle,
+      location: req.body.location,
+    }
+    const location = new mapLocation(locationData);
 
-    Team.findById(teamId, (error, record) => {
-      if (error || record === null) {
-        res.status(400).send('No valid records found');
-        return;
+    try {
+      const response = await location.save()
+      if(!response) {
+        throw new Error('Something went wrong during saving the location...')
       }
-      record.teamName = req.body.teamName;
-      record.isActive = req.body.isActive;
-      record.createdDatetime = Date.now();
-      record.modifiedDatetime = Date.now();
-
-      record
-        .save()
-        .then(results => res.status(201).send(results._id))
-        .catch(errors => res.status(400).send(errors));
-    });
+      res.status(200).send(response);
+    } catch (err) {
+      console.log(err.message)
+      res.status(500).json({message: err.message || 'Something went wrong...'});
+    }
   };
 
   return {
