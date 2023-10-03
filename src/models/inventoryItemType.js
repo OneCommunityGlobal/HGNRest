9da89884167f825caf399c09ae44d0bb6fe35079
Tim Kent <timkentdev@gmail.com>
2023-10-02 20:12:37 -0700

    update inventoryitemtype schema

--- src/models/inventoryItemType.js
+++ src/models/inventoryItemType.js
@@ -2,21 +2,14 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-// const InventoryItemType = new Schema({
-//   name: { type: String, required: true },
-//   description: { type: String },
-//   imageUrl: { type: String },
-//   quantifier: { type: String, default: 'each' },
-// });
-
 const InventoryItemType = new Schema({ // creates an item, tracks total amount in organization's stock
   type: { type: String, required: true }, // ie Material, Equipment, Tool
   name: { type: String, required: true },
   description: { type: String, required: true, maxLength: 150 },
   uom: { type: String, required: true }, // unit of measurement
   totalStock: { type: Number, required: true }, // total amount of all stock acquired
-  totalAvailable: { type: Number, required: true }, // amount of item available to be checked out
-  projectsUsing: [String], // ids of projects using the item
+  totalAvailable: { type: Number, required: true },
+  projectsUsing: [ {type: mongoose.SchemaTypes.ObjectId, ref: 'project'} ],
   imageUrl: { type: String }
 });
 
