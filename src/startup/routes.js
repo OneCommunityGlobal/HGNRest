9fdcdb8d4930421928d5deb4d825c7f5f9c6d213
Tim Kent <timkentdev@gmail.com>
2023-10-02 18:09:21 -0700

    update route, router, controller with updated material schema and new collection fetch

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -20,6 +20,7 @@ const ownerStandardMessage = require('../models/ownerStandardMessage');
 const profileInitialSetuptoken = require('../models/profileInitialSetupToken');
 const reason = require('../models/reason');
 const mouseoverText = require('../models/mouseoverText');
+const inventoryItemMaterial = require('../models/inventoryItemMaterial');
 
 const userProfileRouter = require('../routes/userProfileRouter')(userProfile);
 const badgeRouter = require('../routes/badgeRouter')(badge);
@@ -55,8 +56,7 @@ const mouseoverTextRouter = require('../routes/mouseoverTextRouter')(mouseoverTe
 
 // bm dashboard
 const bmLoginRouter = require('../routes/bmdashboard/bmLoginRouter')();
-const bmMaterialsRouter = require('../routes/bmdashboard/bmMaterialsRouter')();
-
+const bmMaterialsRouter = require('../routes/bmdashboard/bmMaterialsRouter')(inventoryItemMaterial, inventoryItemType);
 
 module.exports = function (app) {
   app.use('/api', forgotPwdRouter);
