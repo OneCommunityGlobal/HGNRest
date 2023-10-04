d4e04bd6c970ec6c27a4b21d48ba64b1b3c8fdfc
Tim Kent <timkentdev@gmail.com>
2023-10-04 16:31:43 -0700

    populate db fetch with project, inventoryItemType objects

--- src/controllers/bmdashboard/bmMaterialsController.js
+++ src/controllers/bmdashboard/bmMaterialsController.js
@@ -4,6 +4,14 @@ const bmMaterialsController = function (ItemMaterial, ItemType) {
   const bmMaterialsList = async function _matsList(req, res) {
     try {
       ItemMaterial.find()
+      .populate({
+        path: 'project',
+        select: '_id projectName'
+      })
+      .populate({
+        path: 'inventoryItemType',
+        select: '_id name uom totalStock'
+      })
       .then(results => res.status(200).send(results))
       .catch(error => res.status(500).send(error))
     } catch (err) {
