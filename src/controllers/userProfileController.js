109e22dd42b55160a56e1e3264ebd389b7b4443a
AriaYu927 <yl8651@nyu.edu>
2023-10-05 16:27:37 +0800

    allow five-letter code

--- src/controllers/userProfileController.js
+++ src/controllers/userProfileController.js
@@ -314,7 +314,7 @@ const userProfileController = function (UserProfile) {
         return;
       }
 
-      const teamcodeRegex = /^[a-zA-Z]-[a-zA-Z]{3}$/;
+      const teamcodeRegex = /^([a-zA-Z]-[a-zA-Z]{3}|[a-zA-Z]{5})$/;
       if (!teamcodeRegex.test(req.body.teamCode)) {
         res.status(400).send("The team code is invalid");
         return;
@@ -608,7 +608,7 @@ const userProfileController = function (UserProfile) {
     if (key === "teamCode") {
       const canEditTeamCode = req.body.requestor.role === "Owner" ||
         req.body.requestor.permissions?.frontPermissions.includes("editTeamCode");
-      const teamcodeRegex = /^[a-zA-Z]-[a-zA-Z]{3}$/;
+      const teamcodeRegex = /^([a-zA-Z]-[a-zA-Z]{3}|[a-zA-Z]{5})$/;
 
       if(!canEditTeamCode){
         res.status(403).send("You are not authorized to edit team code.");
