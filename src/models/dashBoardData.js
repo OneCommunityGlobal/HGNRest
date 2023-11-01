8036f37181e6f41b064d702357d542d0b90aac45
tsunami776 <43768723+tsunami776@users.noreply.github.com>
2023-10-31 20:07:56 -0500

    Tried to fix old approach

---
+++ src/models/dashBoardData.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const DashboardData = new Schema({
+    _id: { type: mongoose.Schema.Types.String },
+    aIPromptText: { type: String },
+});
+
+module.exports = mongoose.model('dashboardData', DashboardData, 'dashboard');
