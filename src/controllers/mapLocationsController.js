ac823a55717273c4675f55d21529c26f948093f5
Oleksandr Riazantsev <riazantsev.aleks@gmail.com>
2023-11-01 12:58:36 -0400

    fix: moving part of logic from the frontend, bug fixes feat: updating timezone

--- src/controllers/mapLocationsController.js
+++ src/controllers/mapLocationsController.js
@@ -1,27 +1,32 @@
-const userProfile = require('../models/userProfile');
+const UserProfile = require('../models/userProfile');
 const cache = require('../utilities/nodeCache')();
 
-const mapLocationsController = function (MapLocation) {
-  const getAllLocations = function (req, res) {
-    const priorText = 'Prior to HGN Data Collection';
-    MapLocation.find({})
-      .then(results => {
-        const users = results.map(item => {
-          return ({
-            title: priorText,
-            firstName: item.firstName !== priorText ? item.firstName : '',
-            lastName: item.lastName !== priorText ? item.lastName : '',
-            jobTitle: item.jobTitle !== priorText ? item.jobTitle : '',
-            location: item.location,
-            isActive: item.isActive,
-            _id: item._id
-          })
-        })
-        res.send(users).status(200);
 
-      })
-      .catch(error =>
-        res.send(error).status(404));
+const mapLocationsController = function (MapLocation) {
+  const getAllLocations = async function (req, res) {
+
+    try {
+      const users = [];
+      const results = await UserProfile.find({},
+        '_id firstName lastName isActive location jobTitle totalTangibleHrs hoursByCategory'
+      );
+
+      results.forEach((item) => {
+        if (
+          (item.location?.coords.lat && item.location?.coords.lng && item.totalTangibleHrs >= 10) ||
+          (item.location?.coords.lat && item.location?.coords.lng && calculateTotalHours(item.hoursByCategory) >= 10)
+        ) {
+          users.push(item);
+        }
+      });
+
+      const m_users = await MapLocation.find({});
+
+      res.status(200).send({ users, m_users });
+    } catch (err) {
+      res.status(404).send(err);
+    }
+
   };
   const deleteLocation = async function (req, res) {
 
@@ -37,7 +42,7 @@ const mapLocationsController = function (MapLocation) {
   };
   const putUserLocation = async function (req, res) {
 
-    if (!req.body.requestor.role === 'Administrator' || !req.body.requestor.role === 'Owner') {
+    if (!req.body.requestor.role === 'Owner') {
       res.status(403).send('You are not authorized to make changes in the teams.');
       return;
     }
@@ -61,32 +66,36 @@ const mapLocationsController = function (MapLocation) {
     }
   };
   const updateUserLocation = async function (req, res) {
-    console.log(req.body)
-    if (!req.body.requestor.role === 'Administrator' || !req.body.requestor.role === 'Owner') {
+    if (!req.body.requestor.role === 'Owner') {
       res.status(403).send('You are not authorized to make changes in the teams.');
       return;
     }
     const userType = req.body.type;
-    const userId= req.body._id;
+    const userId = req.body._id;
     const updateData = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        jobTitle: req.body.jobTitle,
-        location: req.body.location,
-        _id: req.body._id
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      jobTitle: req.body.jobTitle,
+      location: req.body.location,
+    }
+
+    if (req.body.timeZone) {
+      updateData.timeZone = req.body.timeZone
     }
 
     try {
       let response;
-      if(userType === 'user') {
-        response = await userProfile.findOneAndUpdate({ _id: userId }, {$set: {...updateData, jobTitle: [updateData.jobTitle]}}, { new: true });
+      if (userType === 'user') {
+        console.log('updateData----', updateData);
+        response = await UserProfile.findOneAndUpdate({ _id: userId }, { $set: { ...updateData, jobTitle: [updateData.jobTitle] } }, { new: true });
         cache.removeCache('allusers')
         cache.removeCache(`user-${userId}`);
+
         cache.setCache(`user-${userId}`, JSON.stringify(response));
       } else {
-        response = await MapLocation.findOneAndUpdate({ _id: userId }, {$set: updateData}, { new: true })
+        response = await MapLocation.findOneAndUpdate({ _id: userId }, { $set: updateData }, { new: true })
       }
-      
+
       if (!response) {
         throw new Error('Something went wrong during saving the location...')
       }
@@ -98,13 +107,22 @@ const mapLocationsController = function (MapLocation) {
         _id: response._id,
         type: userType
       }
-      
+
       res.status(200).send(newData);
     } catch (err) {
       console.log(err.message)
       res.status(500).json({ message: err.message || 'Something went wrong...' });
     }
   };
+
+  function calculateTotalHours(hoursByCategory) {
+    let hours = 0;
+    Object.keys(hoursByCategory).forEach((x) => {
+      hours += hoursByCategory[x];
+    });
+    return hours;
+  }
+
   return {
     getAllLocations,
     deleteLocation,
