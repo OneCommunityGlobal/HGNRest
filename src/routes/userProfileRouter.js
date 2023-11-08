d8e440e05869bd24fcc31c350f03dfab8a345c1c
Ruike Qiu <114443664+StrawberryCalpico@users.noreply.github.com>
2023-11-08 16:30:36 -0500

    Trim White space, sanitize url

--- src/routes/userProfileRouter.js
+++ src/routes/userProfileRouter.js
@@ -1,53 +1,85 @@
-const express = require('express');
+import { body, customSanitizer } from "express-validator";
 
+const express = require("express");
 
 const routes = function (userProfile) {
-  const controller = require('../controllers/userProfileController')(userProfile);
+  const controller = require("../controllers/userProfileController")(
+    userProfile
+  );
 
   const userProfileRouter = express.Router();
 
-  userProfileRouter.route('/userProfile')
+  userProfileRouter
+    .route("/userProfile")
     .get(controller.getUserProfiles)
-    .post(controller.postUserProfile);
+    .post(
+      body("firstName").customSanitizer((value) => value.replace(/\s/g, "")),
+      body("lastName").customSanitizer((value) => value.replace(/\s/g, "")),
+      controller.postUserProfile
+    );
 
-  userProfileRouter.route('/userProfile/:userId')
+  userProfileRouter
+    .route("/userProfile/:userId")
     .get(controller.getUserById)
-    .put(controller.putUserProfile)
+    .put(
+      body("firstName").customSanitizer((value) => value.replace(/\s/g, "")),
+      body("lastName").customSanitizer((value) => value.replace(/\s/g, "")),
+      body("personalLinks").customSanitizer((value) =>
+        value.map((link) => {
+          if (link.Name.replace(/\s/g, "") || link.Link.replace(/\s/g, "")) {
+            return {
+              ...link,
+              Name: link.Name.replace(/\s/g, ""),
+              Link: link.Link.replace(/\s/g, ""),
+            };
+          }
+          throw new Error("Url not valid");
+        })
+      ),
+      controller.putUserProfile
+    )
     .delete(controller.deleteUserProfile)
     .patch(controller.changeUserStatus);
 
-  userProfileRouter.route('/userProfile/name/:name')
+  userProfileRouter
+    .route("/userProfile/name/:name")
     .get(controller.getUserByName);
 
-  userProfileRouter.route('/refreshToken/:userId')
-    .get(controller.refreshToken);
+  userProfileRouter.route("/refreshToken/:userId").get(controller.refreshToken);
 
-  userProfileRouter.route('/userProfile/reportees/:userId')
+  userProfileRouter
+    .route("/userProfile/reportees/:userId")
     .get(controller.getreportees);
 
-  userProfileRouter.route('/userProfile/teammembers/:userId')
+  userProfileRouter
+    .route("/userProfile/teammembers/:userId")
     .get(controller.getTeamMembersofUser);
 
-  userProfileRouter.route('/userProfile/:userId/property')
+  userProfileRouter
+    .route("/userProfile/:userId/property")
     .patch(controller.updateOneProperty);
 
-  userProfileRouter.route('/userProfile/:userId/updatePassword')
+  userProfileRouter
+    .route("/userProfile/:userId/updatePassword")
     .patch(controller.updatepassword);
 
-  userProfileRouter.route('/userProfile/:userId/resetPassword')
+  userProfileRouter
+    .route("/userProfile/:userId/resetPassword")
     .patch(controller.resetPassword);
 
-  userProfileRouter.route('/userProfile/name/:userId')
+  userProfileRouter
+    .route("/userProfile/name/:userId")
     .get(controller.getUserName);
 
-  userProfileRouter.route('/userProfile/project/:projectId')
+  userProfileRouter
+    .route("/userProfile/project/:projectId")
     .get(controller.getProjectMembers);
 
-  userProfileRouter.route('/userProfile/socials/facebook')
+  userProfileRouter
+    .route("/userProfile/socials/facebook")
     .get(controller.getAllUsersWithFacebookLink);
 
   return userProfileRouter;
 };
-
 
 module.exports = routes;
