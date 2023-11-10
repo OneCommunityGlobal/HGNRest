8a634b4dfdd9339a89d8dae939925a5ac10abb74
Tim Kent <timkentdev@gmail.com>
2023-11-10 09:31:52 -0800

    add new inventory models, invtype router and controller

---
+++ src/models/bmdashboard/buildingInventoryType.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const buildingInventoryType = new Schema({
+  category: String, // Consumable, Material, Tool, Equipment
+  name: String,
+  description: String,
+  unit: String, // unit of measurement
+  imageUrl: String,
+});
+
+module.exports = mongoose.model('buildingInventoryType', buildingInventoryType, 'buildingInventoryTypes');
