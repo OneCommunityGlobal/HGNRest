e8ff01a3ddcd60cfa94271f93dba33186370948a
Rhea Wu <rheawu1212@gmail.com>
2023-11-15 16:00:42 -0500

    rhea create newLessom routes and controller

---
+++ src/controllers/bmdashboard/bmNewLessonController.js
@@ -0,0 +1,49 @@
+// potentially inteacting with monoose db
+// const mongoose = require('mongoose');
+
+// local dummy data just for testing:
+const newLessonList = [
+    {
+        id: '01',
+        title: 'lesson_1',
+        content: 'This is lesson_1 for manufacting.',
+        date: '2023-05-23',
+        author: 'james',
+        tag: 'manufacting',
+        project: ' project_1',
+    },
+
+    {
+        id: '02',
+        title: 'lesson_2',
+        content: 'This is lesson_2 for ui/ux design.',
+        date: '2022-06-12',
+        author: 'max',
+        tag: 'ui/ux',
+        project: ' project_2',
+    },
+
+];
+
+const bmNewLessonController = () => {
+    const bmPostLessonList = async (req, res) => {
+        try {
+            // get new lesson's title, content, tag, creating time, belongs-to-project-name, view-by-who
+            // creating new lesson profile, add it into schema lessonList (wait to be create...)
+            // using dummy data for now:
+            newLessonList.push(req.body);
+            res.status(200).send(newLessonList);
+        } catch (err) {
+            res.json(err);
+        }
+    };
+    const bmGetLessonList = async (req, res) => {
+        try {
+            res.status(200).send(newLessonList);
+        } catch (err) {
+            res.json(err);
+        }
+    };
+    return { bmPostLessonList, bmGetLessonList };
+};
+module.exports = bmNewLessonController;
