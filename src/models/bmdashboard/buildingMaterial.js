89e5cfa11a94aa7a75ed1d1232f0e4b40b665547
Vishala Ramasamy <vishala.ramasamy@gmail.com>
2023-11-30 13:35:10 -0800

    update material changes

--- src/models/bmdashboard/buildingMaterial.js
+++ src/models/bmdashboard/buildingMaterial.js
@@ -3,21 +3,25 @@ const { Schema } = mongoose;
 const buildingMaterial = new Schema({
   itemType: { type: mongoose.SchemaTypes.ObjectId, ref: 'buildingInventoryType' },
   project: { type: mongoose.SchemaTypes.ObjectId, ref: 'buildingProject' },
-  stockBought: Number, // total amount of item bought for use in the project
-  stockUsed: Number, // total amount of item used successfully in the project
-  stockWasted: Number, // total amount of item wasted/ruined/lost in the project
-  stockAvailable: Number, // bought - (used + wasted)
+  stockBought: { type: Number, default: 0 }, // total amount of item bought for use in the project
+  stockUsed: { type: Number, default: 0 }, // total amount of item used successfully in the project
+  stockWasted: { type: Number, default: 0 }, // total amount of item wasted/ruined/lost in the project
+  stockAvailable: { type: Number, default: 0 }, // bought - (used + wasted)
   purchaseRecord: [{
+    _id: false, // do not add _id field to subdocument
     date: { type: Date, default: Date.now() },
     requestedBy: { type: mongoose.SchemaTypes.ObjectId, ref: 'userProfile' },
-    quantity: Number,
-    status: { type: String, default: 'Pending' }, // Pending, Rejected, Approved
+    quantity: { type: Number, required: true },
+    priority: { type: String, enum: ['Low', 'Medium', 'High'], required: true },
+    brand: String,
+    status: { type: String, default: 'Pending', enum: ['Approved', 'Pending', 'Rejected'] },
   }],
   updateRecord: [{
-    date: Date,
+    _id: false,
+    date: { type: Date, required: true },
     createdBy: { type: mongoose.SchemaTypes.ObjectId, ref: 'userProfile' },
-    quantityUsed: Number, 
-    quantityWasted: Number,
+    quantityUsed: { type: Number, required: true },
+    quantityWasted: { type: Number, required: true },
   }],
 });
 module.exports = mongoose.model('buildingMaterial', buildingMaterial, 'buildingMaterials');
