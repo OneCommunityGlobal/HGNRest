6647cf2e13507a77cf842f92a43d9067cb3e44ee
lacnoskillz <kaikane3457@hotmail.com>
2023-12-01 12:14:46 -0600

    remake branch without package-lock.json modifications

--- src/controllers/bmdashboard/bmInventoryTypeController.js
+++ src/controllers/bmdashboard/bmInventoryTypeController.js
@@ -1,19 +1,54 @@
 const bmInventoryTypeController = function (InvType) {
   const fetchMaterialTypes = async (req, res) => {
     try {
-      InvType
-        .find()
-        .exec()
-        .then(result => res.status(200).send(result))
-        .catch(error => res.status(500).send(error));
-    } catch (err) {
-      res.json(err);
+      const result = await InvType.find().exec();
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  };
+  const fetchSingleInventoryType = async (req, res) => {
+    const { invtypeId } = req.params;
+    try {
+      const result = await InvType.findById(invtypeId).exec();
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(500).send(error);
     }
   };
 
-  return {
-    fetchMaterialTypes,
+  const updateNameAndUnit = async (req, res) => {
+    try {
+      const { invtypeId } = req.params;
+      const { name, unit } = req.body;
+
+      const updateData = {};
+
+      if (name) {
+        updateData.name = name;
+      }
+
+      if (unit) {
+        updateData.unit = unit;
+      }
+
+      const updatedInvType = await InvType.findByIdAndUpdate(
+        invtypeId,
+        updateData,
+        { new: true, runValidators: true },
+      );
+
+      if (!updatedInvType) {
+        return res.status(404).json({ error: 'invType Material not found check Id' });
+      }
+
+      res.status(200).json(updatedInvType);
+    } catch (error) {
+      res.status(500).send(error);
+    }
   };
+
+  return { fetchMaterialTypes, fetchSingleInventoryType, updateNameAndUnit };
 };
 
 module.exports = bmInventoryTypeController;
