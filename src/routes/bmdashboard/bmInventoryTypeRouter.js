c1294d44413f8b67abbadbb0ac8ab1e953ab967d
lacnoskillz <kaikane3457@hotmail.com>
2023-12-01 12:14:46 -0600

    remake branch without package-lock.json modifications

--- src/routes/bmdashboard/bmInventoryTypeRouter.js
+++ src/routes/bmdashboard/bmInventoryTypeRouter.js
@@ -4,9 +4,14 @@ const routes = function (invType) {
   const inventoryTypeRouter = express.Router();
   const controller = require('../../controllers/bmdashboard/bmInventoryTypeController')(invType);
 
+  // Route for fetching all material types
   inventoryTypeRouter.route('/invtypes/materials')
     .get(controller.fetchMaterialTypes);
 
+  // Combined routes for getting a single inventory type and updating its name and unit of measurement
+  inventoryTypeRouter.route('/invtypes/material/:invtypeId')
+    .get(controller.fetchSingleInventoryType)
+    .put(controller.updateNameAndUnit);
   return inventoryTypeRouter;
 };
 
