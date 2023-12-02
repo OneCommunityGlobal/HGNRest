fff5f072bf915dfa54cd60406f9daae00e8a0782
Rhea Wu <rheawu1212@gmail.com>
2023-12-02 10:58:50 -0500

    Rhea adding newLesson schema and set up db

---
+++ src/models/bmdashboard/buildingNewLesson.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const LessonList = new Schema({
+    title: { type: String, required: true, maxLength: 20 },
+    content: { type: String, required: true, maxLength: 500 },
+    date: { type: Date, required: true, default: Date.now() },
+    author: { type: mongoose.SchemaTypes.ObjectId, ref: 'userProfile', required: true },
+    tag: [{ type: String, required: true, maxLength: 10 }],
+    relatedProject: { type: mongoose.SchemaTypes.ObjectId, ref: 'project', required: true },
+});
+
+module.exports = mongoose.model('lessonList', LessonList);
