b81b0a15340148a6378ce6aa920e39bfe01a76b2
Shiwani Rajagopalan <rajagopalanshiwani@gmail.com>
2023-12-06 17:16:44 -0500

    added timeOffFrom and timeOffTill fields to mongoDB

--- src/controllers/reasonSchedulingController.js
+++ src/controllers/reasonSchedulingController.js
@@ -1,23 +1,19 @@
-
-const moment = require('moment-timezone');
-const UserModel = require('../models/userProfile');
-const ReasonModel = require('../models/reason');
-const emailSender = require('../utilities/emailSender');
-
+const moment = require("moment-timezone");
+const UserModel = require("../models/userProfile");
+const ReasonModel = require("../models/reason");
+const emailSender = require("../utilities/emailSender");
 
 const postReason = async (req, res) => {
   try {
     const { userId, requestor, reasonData } = req.body;
 
     const newDate = moment
-      .tz(reasonData.date, 'America/Los_Angeles')
-      .startOf('day');
-    const currentDate = moment
-      .tz('America/Los_Angeles')
-      .startOf('day');
+      .tz(reasonData.date, "America/Los_Angeles")
+      .startOf("day");
+    const currentDate = moment.tz("America/Los_Angeles").startOf("day");
 
     // error case 0
-    if (moment.tz(reasonData.date, 'America/Los_Angeles').day() !== 0) {
+    if (moment.tz(reasonData.date, "America/Los_Angeles").day() !== 0) {
       return res.status(400).json({
         message:
           "You must choose the Sunday YOU'LL RETURN as your date. This is so your reason ends up as a note on that blue square.",
@@ -27,19 +23,19 @@ const postReason = async (req, res) => {
 
     if (newDate.isBefore(currentDate)) {
       return res.status(400).json({
-        message: 'You should select a date that is yet to come',
+        message: "You should select a date that is yet to come",
         errorCode: 7,
       });
     }
 
     if (!reasonData.message) {
       return res.status(400).json({
-        message: 'You must provide a reason.',
+        message: "You must provide a reason.",
         errorCode: 6,
       });
     }
 
-     // Commented this condition to make reason scheduler available to all the users.
+    // Commented this condition to make reason scheduler available to all the users.
     // error case 1
     // if (requestor.role !== 'Owner' && requestor.role !== 'Administrator') {
     //   return res.status(403).json({
@@ -54,15 +50,61 @@ const postReason = async (req, res) => {
     // error case 2
     if (!foundUser) {
       return res.status(404).json({
-        message: 'User not found',
+        message: "User not found",
         errorCode: 2,
       });
     }
 
+    // new changes
+
+    if (
+      foundUser.hasOwnProperty("timeOffFrom") &&
+      foundUser.hasOwnProperty("timeOffTill")
+    ) {
+      if (currentDate >= foundUser.timeOffTill) {
+        await UserModel.findOneAndUpdate(
+          {
+            _id: userId,
+          },
+          {
+            $set: {
+              timeOffFrom: currentDate,
+              timeOffTill: newDate,
+            },
+          }
+        );
+      } else {
+        await UserModel.findOneAndUpdate(
+          {
+            _id: userId,
+          },
+          {
+            $set: {
+              timeOffTill: newDate,
+            },
+          }
+        );
+      }
+    } else {
+      await UserModel.findOneAndUpdate(
+        {
+          _id: userId,
+        },
+        {
+          $set: {
+            timeOffFrom: currentDate,
+            timeOffTill: newDate,
+          },
+        }
+      );
+    }
+
+    //
+
     const foundReason = await ReasonModel.findOne({
       date: moment
-        .tz(reasonData.date, 'America/Los_Angeles')
-        .startOf('day')
+        .tz(reasonData.date, "America/Los_Angeles")
+        .startOf("day")
         .toISOString(),
       userId,
     });
@@ -70,14 +112,14 @@ const postReason = async (req, res) => {
     // error case 3
     if (foundReason) {
       return res.status(403).json({
-        message: 'The reason must be unique to the date',
+        message: "The reason must be unique to the date",
         errorCode: 3,
       });
     }
 
     const savingDate = moment
-      .tz(reasonData.date, 'America/Los_Angeles')
-      .startOf('day')
+      .tz(reasonData.date, "America/Los_Angeles")
+      .startOf("day")
       .toISOString();
 
     const newReason = new ReasonModel({
@@ -86,14 +128,13 @@ const postReason = async (req, res) => {
       userId,
     });
 
-
     // await newReason.save();
-   const savedData = await newReason.save();
+    const savedData = await newReason.save();
     if (savedData) {
       // Upon clicking the "Save" button in the Blue Square Reason Scheduler, an email will be automatically sent to the user and Jae.
-     const subject = `Blue Square Reason for ${foundUser.firstName} ${foundUser.lastName} has been set`;
+      const subject = `Blue Square Reason for ${foundUser.firstName} ${foundUser.lastName} has been set`;
 
-          const emailBody = `<p> Hi ! </p>
+      const emailBody = `<p> Hi ! </p>
 
           <p>This email is to let you know that ${foundUser.firstName} ${foundUser.lastName} has set their Blue Square Reason.</p>
           
@@ -103,21 +144,20 @@ const postReason = async (req, res) => {
           <p>Thank you,<br />
           One Community</p>`;
 
+      // 1 hardcoded email- emailSender('@gmail.com', subject, emailBody, null, null);
 
-          // 1 hardcoded email- emailSender('@gmail.com', subject, emailBody, null, null);
+      // 2 user email -
+      emailSender(`${foundUser.email}`, subject, emailBody, null, null);
 
-          // 2 user email -
-          emailSender(`${foundUser.email}`, subject, emailBody, null, null);
-
-          // 3 - user email and hardcoded email ( After PR approval hardcode Jae's email)
-          //  emailSender(`${foundUser.email},@gmail.com`, subject, emailBody, null, null);
-     }
+      // 3 - user email and hardcoded email ( After PR approval hardcode Jae's email)
+      //  emailSender(`${foundUser.email},@gmail.com`, subject, emailBody, null, null);
+    }
 
     return res.sendStatus(200);
   } catch (error) {
     console.log(error);
     return res.status(400).json({
-      errMessage: 'Something went wrong',
+      errMessage: "Something went wrong",
     });
   }
 };
@@ -140,7 +180,7 @@ const getAllReasons = async (req, res) => {
     // error case 2
     if (!foundUser) {
       return res.status(404).json({
-        message: 'User not found',
+        message: "User not found",
       });
     }
 
@@ -154,7 +194,7 @@ const getAllReasons = async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(400).json({
-      errMessage: 'Something went wrong while fetching the user',
+      errMessage: "Something went wrong while fetching the user",
     });
   }
 };
@@ -178,24 +218,24 @@ const getSingleReason = async (req, res) => {
     // error case 2
     if (!foundUser) {
       return res.status(404).json({
-        message: 'User not found',
+        message: "User not found",
         errorCode: 2,
       });
     }
 
     const foundReason = await ReasonModel.findOne({
       date: moment
-        .tz(queryDate, 'America/Los_Angeles')
-        .startOf('day')
+        .tz(queryDate, "America/Los_Angeles")
+        .startOf("day")
         .toISOString(),
       userId,
     });
 
     if (!foundReason) {
       return res.status(200).json({
-        reason: '',
-        date: '',
-        userId: '',
+        reason: "",
+        date: "",
+        userId: "",
         isSet: false,
       });
     }
@@ -204,7 +244,7 @@ const getSingleReason = async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(400).json({
-      message: 'Something went wrong while fetching single reason',
+      message: "Something went wrong while fetching single reason",
     });
   }
 };
@@ -225,7 +265,7 @@ const patchReason = async (req, res) => {
 
     if (!reasonData.message) {
       return res.status(400).json({
-        message: 'You must provide a reason.',
+        message: "You must provide a reason.",
         errorCode: 6,
       });
     }
@@ -235,22 +275,22 @@ const patchReason = async (req, res) => {
     // error case 2
     if (!foundUser) {
       return res.status(404).json({
-        message: 'User not found',
+        message: "User not found",
         errorCode: 2,
       });
     }
 
     const foundReason = await ReasonModel.findOne({
       date: moment
-        .tz(reasonData.date, 'America/Los_Angeles')
-        .startOf('day')
+        .tz(reasonData.date, "America/Los_Angeles")
+        .startOf("day")
         .toISOString(),
       userId,
     });
     // error case 4
     if (!foundReason) {
       return res.status(404).json({
-        message: 'Reason not found',
+        message: "Reason not found",
         errorCode: 4,
       });
     }
@@ -260,9 +300,9 @@ const patchReason = async (req, res) => {
     const savedData = await foundReason.save();
     if (savedData) {
       // Upon clicking the "Save" button in the Blue Square Reason Scheduler, an email will be automatically sent to the user and Jae.
-     const subject = `Blue Square Reason for ${foundUser.firstName} ${foundUser.lastName} has been updated`;
+      const subject = `Blue Square Reason for ${foundUser.firstName} ${foundUser.lastName} has been updated`;
 
-          const emailBody = `<p> Hi ! </p>
+      const emailBody = `<p> Hi ! </p>
 
           <p>This email is to let you know that ${foundUser.firstName} ${foundUser.lastName} has updated their Blue Square Reason.</p>
           
@@ -272,22 +312,21 @@ const patchReason = async (req, res) => {
           <p>Thank you,<br />
           One Community</p>`;
 
+      // 1 hardcoded email- emailSender('@gmail.com', subject, emailBody, null, null);
 
-          // 1 hardcoded email- emailSender('@gmail.com', subject, emailBody, null, null);
+      // 2 user email -
+      emailSender(`${foundUser.email}`, subject, emailBody, null, null);
 
-          // 2 user email -
-          emailSender(`${foundUser.email}`, subject, emailBody, null, null);
-
-          // 3 - user email and hardcoded email ( After PR approval hardcode Jae's email)
-          //  emailSender(`${foundUser.email},@gmail.com`, subject, emailBody, null, null);
-     }
+      // 3 - user email and hardcoded email ( After PR approval hardcode Jae's email)
+      //  emailSender(`${foundUser.email},@gmail.com`, subject, emailBody, null, null);
+    }
 
     return res.status(200).json({
-      message: 'Reason Updated!',
+      message: "Reason Updated!",
     });
   } catch (error) {
     return res.status(400).json({
-      message: 'something went wrong while patching the reason',
+      message: "something went wrong while patching the reason",
     });
   }
 };
@@ -298,10 +337,10 @@ const deleteReason = async (req, res) => {
     const { userId } = req.params;
 
     // error case 1
-    if (requestor.role !== 'Owner' && requestor.role !== 'Administrator') {
+    if (requestor.role !== "Owner" && requestor.role !== "Administrator") {
       return res.status(403).json({
         message:
-          'You must be an Owner or Administrator to schedule a reason for a Blue Square',
+          "You must be an Owner or Administrator to schedule a reason for a Blue Square",
         errorCode: 1,
       });
     }
@@ -311,21 +350,21 @@ const deleteReason = async (req, res) => {
     // error case 2
     if (!foundUser) {
       return res.status(404).json({
-        message: 'User not found',
+        message: "User not found",
         errorCode: 2,
       });
     }
 
     const foundReason = await ReasonModel.findOne({
       date: moment
-        .tz(reasonData.date, 'America/Los_Angeles')
-        .startOf('day')
+        .tz(reasonData.date, "America/Los_Angeles")
+        .startOf("day")
         .toISOString(),
     });
 
     if (!foundReason) {
       return res.status(404).json({
-        message: 'Reason not found',
+        message: "Reason not found",
         errorCode: 4,
       });
     }
@@ -333,13 +372,13 @@ const deleteReason = async (req, res) => {
     foundReason.remove((err) => {
       if (err) {
         return res.status(500).json({
-          message: 'Error while deleting document',
+          message: "Error while deleting document",
           errorCode: 5,
         });
       }
 
       return res.status(200).json({
-        message: 'Document deleted',
+        message: "Document deleted",
       });
     });
   } catch (error) {}
