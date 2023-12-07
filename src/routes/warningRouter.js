34055bd2fc0d4c527c32268ce11117965ff6bad1
luisarevalo21 <arevaloluis89@yahoo.com>
2023-12-06 16:52:50 -0800

    updated backend

--- src/routes/warningRouter.js
+++ src/routes/warningRouter.js
@@ -1,12 +1,17 @@
 const express = require("express");
 
-const routes = function () {
-  const controller = require("../controllers/warningsController");
+const routes = function (userProfile) {
+  const controller = require("../controllers/warningsController")(userProfile);
 
   const warningRouter = express.Router();
 
-  warningRouter.route("/warnings/:id").get(controller.getWarningsByUserId);
+  warningRouter
+    .route("/warnings/:id")
+    .get(controller.getWarningsByUserId)
+    .post(controller.postWarningsToUserProfile);
 
+  // .post(controller.postWarningsToUserProfile);
+  //
   return warningRouter;
 };
 module.exports = routes;
