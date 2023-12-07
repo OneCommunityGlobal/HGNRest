545dc8d9579e9c9841345a905b590e7af97650f7
luisarevalo21 <arevaloluis89@yahoo.com>
2023-12-06 16:52:50 -0800

    updated backend

--- src/controllers/warningsController.js
+++ src/controllers/warningsController.js
@@ -1,15 +1,51 @@
 const mongoose = require("mongoose");
 const userProfile = require("../models/userProfile");
 
-const warningsController = function () {
+const warningsController = function (UserProfile) {
+  // check to see how to add an array caleld warnigns to the abckend
+  // anc check if its being saved when posting
+  // change post to a put instead as I am saving data that exisits.
+
   const getWarningsByUserId = async function (req, res) {
     console.log("get warning called", req.body);
+
+    const { userId } = req.body;
+
+    // const { roleId } = req.params;
+    UserProfile.findById(userId)
+      .then((results) => res.status(200).send(results))
+      .catch((error) => res.status(404).send({ error }));
+    res.status(200).json({ message: "test message" });
   };
 
-  const postUserProfile = async function (req, res) {};
+  const postWarningsToUserProfile = async function (req, res) {
+    console.log("body", req.body);
+    const { userId, iconId, color, dateAssigned } = req.body;
+    console.log("user id", userId, iconId, color, dateAssigned);
+
+    UserProfile.findById(userId, (error, record) => {
+      if (error || record === null) {
+        res.status(400).send("No valid records found");
+        return;
+      }
+
+      record.warnings = { userId: userId, color: color, iconId: iconId };
+      record
+        .save()
+        .then((results) => res.status(201).send({ message: "succes", results }))
+        .catch((errors) => res.status(400).send(errors));
+    });
+
+    // const foundUser = await UserProfile.findById({ _id: userId }, "");
+    // foundUser
+    // console.log("posting warnigns to user profile was called ", foundUser);
+    // console.log("posting warnigns to user profile was called ", req.body);
+    // res.status(200).json({ message: "testing post warning to user profile" });
+  };
 
   return {
     getWarningsByUserId,
+    postWarningsToUserProfile,
   };
 };
 
