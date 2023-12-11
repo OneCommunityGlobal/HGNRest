efc53ff6b4aeac1d24314310a911323a11fff9e1
Shengwei Peng <ptrpengdev@gmail.com>
2023-12-11 14:16:23 -0500

    Update assign badge feature

---
+++ src/utilities/exceptionHandler.js
@@ -0,0 +1,17 @@
+const logger = require('../startup/logger');
+
+const errorHandler = (err, req, res, next) => {
+    logger.logException(err);
+
+    const errStatus = err.statusCode || 500;
+    const errMsg = err.message || 'Internal Server Error. Please try again later. If the problem persists, please contact support ID.';
+    res.status(errStatus).json({
+        success: false,
+        status: errStatus,
+        message: errMsg,
+        stack: process.env.NODE_ENV === 'local' ? err.stack : {},
+    });
+    next();
+};
+
+export default errorHandler;
