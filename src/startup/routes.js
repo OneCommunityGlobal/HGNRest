475b90692443226f0e77e5485cb3444b0c0a1e1e
Hsin An Anny Wang <98345990+astrjc0326@users.noreply.github.com>
2023-12-11 11:09:36 -0800

    add: title startup routes

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -25,6 +25,7 @@ const mapLocations = require('../models/mapLocation');
 const buildingProject = require('../models/bmdashboard/buildingProject');
 const buildingInventoryType = require('../models/bmdashboard/buildingInventoryType');
 const buildingMaterial = require('../models/bmdashboard/buildingMaterial');
+const title = require('../models/title');
 
 const userProfileRouter = require('../routes/userProfileRouter')(userProfile);
 const badgeRouter = require('../routes/badgeRouter')(badge);
@@ -48,6 +49,7 @@ const inventoryRouter = require('../routes/inventoryRouter')(inventoryItem, inve
 const timeZoneAPIRouter = require('../routes/timeZoneAPIRoutes')();
 const profileInitialSetupRouter = require('../routes/profileInitialSetupRouter')(profileInitialSetuptoken, userProfile, project);
 const isEmailExistsRouter = require('../routes/isEmailExistsRouter')();
+const titleRouter = require('../routes/titleRouter')(title);
 
 
 const taskEditSuggestion = require('../models/taskEditSuggestion');
@@ -61,6 +63,7 @@ const reasonRouter = require('../routes/reasonRouter')(reason, userProfile);
 const mouseoverTextRouter = require('../routes/mouseoverTextRouter')(mouseoverText);
 
 const mapLocationRouter = require('../routes/mapLocationsRouter')(mapLocations);
+
 
 // bm dashboard
 const bmLoginRouter = require('../routes/bmdashboard/bmLoginRouter')();
@@ -99,6 +102,8 @@ module.exports = function (app) {
   app.use('/api', mouseoverTextRouter);
   app.use('/api', isEmailExistsRouter);
   app.use('/api', mapLocationRouter);
+  app.use('/api', titleRouter);
+
   // bm dashboard
   app.use('/api/bm', bmLoginRouter);
   app.use('/api/bm', bmMaterialsRouter);
