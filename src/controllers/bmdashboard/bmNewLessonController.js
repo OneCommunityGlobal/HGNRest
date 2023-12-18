c5787bb4ff72b124a841eaafc0a2f7a11977af0b
Rhea Wu <rheawu1212@gmail.com>
2023-12-18 11:09:59 -0500

    get function using find() return empty array

--- src/controllers/bmdashboard/bmNewLessonController.js
+++ src/controllers/bmdashboard/bmNewLessonController.js
@@ -1,31 +1,19 @@
 const mongoose = require('mongoose');
-// const buildingNewLesson = require('../../models/bmdashboard/buildingNewLesson');
 
-// local dummy data just for testing:
-// const newLessonList = [
-//     {
-//         id: '01',
-//         title: 'lesson_1',
-//         content: 'This is lesson_1 for manufacting.',
-//         date: '2023-05-23',
-//         author: 'james',
-//         tag: 'manufacting',
-//         project: ' project_1',
-//     },
-
-//     {
-//         id: '02',
-//         title: 'lesson_2',
-//         content: 'This is lesson_2 for ui/ux design.',
-//         date: '2022-06-12',
-//         author: 'max',
-//         tag: 'ui/ux',
-//         project: ' project_2',
-//     },
-
-// ];
-
-const bmNewLessonController = (buildingNewLesson) => {
+const bmNewLessonController = function (BuildingNewLesson) {
+    const bmGetLessonList = async (req, res) => {
+        try {
+            BuildingNewLesson
+            .find()
+            // .populate()
+            // .exec()
+            .then(result => res.status(200).send(result))
+            .catch(error => res.status(500).send(error));
+            // res.status(200).send(newLessonList);
+        } catch (err) {
+            res.json(err);
+        }
+    };
     const bmPostLessonList = async (req, res) => {
         try {
             // get new lesson's title, content, tag, creating time, belongs-to-project-name, view-by-who
@@ -33,27 +21,12 @@ const bmNewLessonController = (buildingNewLesson) => {
             // using dummy data for now:
             // newLessonList.push(req.body);
             // res.status(200).send(newLessonList);
-        } catch (err) {
-            res.json(err);
-        }
-    };
-    const bmGetLessonList = async (req, res) => {
-        try {
-            buildingNewLesson.find()
-                .populate([
-                    {
-                        path: 'title',
-                        select: '_id title'
-                    }
-                ])
-                .exec()
-                .then(result => res.status(200).send(result))
-                .catch(error => res.status(500).send(error));
-            // res.status(200).send(newLessonList);
+            console.log('this is post lesson controller');
         } catch (err) {
             res.json(err);
         }
     };
     return { bmPostLessonList, bmGetLessonList };
 };
+
 module.exports = bmNewLessonController;
