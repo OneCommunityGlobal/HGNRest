b77e4357e7cfd00149c1d18b571da99f53049f5f
Ruike Qiu <114443664+StrawberryCalpico@users.noreply.github.com>
2023-12-20 17:16:31 -0800

    Update for adminLinks

--- src/routes/userProfileRouter.js
+++ src/routes/userProfileRouter.js
@@ -1,81 +1,91 @@
-const express = require("express");
-import { body } from "express-validator";
+import { body } from 'express-validator';
+
+const express = require('express');
+
 
 const routes = function (userProfile) {
-  const controller = require("../controllers/userProfileController")(
-    userProfile
+  const controller = require('../controllers/userProfileController')(
+    userProfile,
   );
 
   const userProfileRouter = express.Router();
 
   userProfileRouter
-    .route("/userProfile")
+    .route('/userProfile')
     .get(controller.getUserProfiles)
     .post(
-      body("firstName").customSanitizer((value) => value.trim()),
-      body("lastName").customSanitizer((value) => value.trim()),
-      controller.postUserProfile
+      body('firstName').customSanitizer(value => value.trim()),
+      body('lastName').customSanitizer(value => value.trim()),
+      controller.postUserProfile,
     );
 
   userProfileRouter
-    .route("/userProfile/:userId")
+    .route('/userProfile/:userId')
     .get(controller.getUserById)
     .put(
-      body("firstName").customSanitizer((value) => value.trim()),
-      body("lastName").customSanitizer((value) => value.trim()),
-      body("personalLinks").customSanitizer((value) =>
-        value.map((link) => {
-          if (link.Name.replace(/\s/g, "") || link.Link.replace(/\s/g, "")) {
-            return {
-              ...link,
-              Name: link.Name.trim(),
-              Link: link.Link.replace(/\s/g, ""),
-            };
-          }
-          throw new Error("Url not valid");
-        })
-      ),
-      controller.putUserProfile
+      body('firstName').customSanitizer(value => value.trim()),
+      body('lastName').customSanitizer(value => value.trim()),
+      body('personalLinks').customSanitizer(value => value.map((link) => {
+        if (link.Name.replace(/\s/g, '') || link.Link.replace(/\s/g, '')) {
+          return {
+            ...link,
+            Name: link.Name.trim(),
+            Link: link.Link.replace(/\s/g, ''),
+          };
+        }
+        throw new Error('Url not valid');
+      })),
+      body('adminLinks').customSanitizer(value => value.map((link) => {
+        if (link.Name.replace(/\s/g, '') || link.Link.replace(/\s/g, '')) {
+          return {
+            ...link,
+            Name: link.Name.trim(),
+            Link: link.Link.replace(/\s/g, ''),
+          };
+        }
+        throw new Error('Url not valid');
+      })),
+      controller.putUserProfile,
     )
     .delete(controller.deleteUserProfile)
     .patch(controller.changeUserStatus);
 
   userProfileRouter
-    .route("/userProfile/name/:name")
+    .route('/userProfile/name/:name')
     .get(controller.getUserByName);
 
-  userProfileRouter.route("/refreshToken/:userId").get(controller.refreshToken);
+  userProfileRouter.route('/refreshToken/:userId').get(controller.refreshToken);
 
   userProfileRouter
-    .route("/userProfile/reportees/:userId")
+    .route('/userProfile/reportees/:userId')
     .get(controller.getreportees);
 
   userProfileRouter
-    .route("/userProfile/teammembers/:userId")
+    .route('/userProfile/teammembers/:userId')
     .get(controller.getTeamMembersofUser);
 
   userProfileRouter
-    .route("/userProfile/:userId/property")
+    .route('/userProfile/:userId/property')
     .patch(controller.updateOneProperty);
 
   userProfileRouter
-    .route("/userProfile/:userId/updatePassword")
+    .route('/userProfile/:userId/updatePassword')
     .patch(controller.updatepassword);
 
   userProfileRouter
-    .route("/userProfile/:userId/resetPassword")
+    .route('/userProfile/:userId/resetPassword')
     .patch(controller.resetPassword);
 
   userProfileRouter
-    .route("/userProfile/name/:userId")
+    .route('/userProfile/name/:userId')
     .get(controller.getUserName);
 
   userProfileRouter
-    .route("/userProfile/project/:projectId")
+    .route('/userProfile/project/:projectId')
     .get(controller.getProjectMembers);
 
   userProfileRouter
-    .route("/userProfile/socials/facebook")
+    .route('/userProfile/socials/facebook')
     .get(controller.getAllUsersWithFacebookLink);
 
   return userProfileRouter;
