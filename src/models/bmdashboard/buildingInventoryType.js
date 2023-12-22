4acbedb9a58e65adc36917c6ac5d2257ee18d206
Tim Kent <timkentdev@gmail.com>
2023-12-22 15:04:45 -0800

    add discriminators to inv type model

--- src/models/bmdashboard/buildingInventoryType.js
+++ src/models/bmdashboard/buildingInventoryType.js
@@ -2,12 +2,80 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const buildingInventoryType = new Schema({
-  category: { type: String, enum: ['Consumable', 'Material', 'Tool', 'Equipment'], required: true },
+//---------------------------
+// BASE INVENTORY TYPE SCHEMA
+//---------------------------
+
+// all schemas will inherit these properties
+// all documents will live in buildingInventoryTypes collection
+
+const invTypeBaseSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  unit: { type: String, required: true }, // unit of measurement
   imageUrl: String,
 });
 
-module.exports = mongoose.model('buildingInventoryType', buildingInventoryType, 'buildingInventoryTypes');
+const invTypeBase = mongoose.model('invTypeBase', invTypeBaseSchema, 'buildingInventoryTypes');
+
+//---------------------------
+// MATERIAL TYPE
+//---------------------------
+
+// ex: sand, stone, brick, lumber
+
+const materialType = invTypeBase.discriminator('material', new mongoose.Schema({
+  category: { type: String, enum: ['Material'] },
+  unit: { type: String, required: true }, // unit of measurement
+}));
+
+//---------------------------
+// CONSUMABLE TYPE
+//---------------------------
+
+// ex: screws, nails, staples
+
+const consumableType = invTypeBase.discriminator('consumable', new mongoose.Schema({
+  category: { type: String, enum: ['Consumable'] },
+  size: { type: String, required: true },
+}));
+
+//---------------------------
+// REUSABLE TYPE
+//---------------------------
+
+// ex: gloves, brushes, hammers, screwdrivers
+
+const reusableType = invTypeBase.discriminator('reusable', new mongoose.Schema({
+  category: { type: String, enum: ['Reusable'] },
+}));
+
+//---------------------------
+// TOOL TYPE
+//---------------------------
+
+// ex: shovels, wheelbarrows, power drills, jackhammers
+
+const toolType = invTypeBase.discriminator('tool', new mongoose.Schema({
+  category: { type: String, enum: ['Tool'] },
+  isPowered: { type: Boolean, required: true },
+  powerSource: { type: String, required: () => this.isPowered }, // required if isPowered = true (syntax?)
+}));
+
+//---------------------------
+// EQUIPMENT TYPE
+//---------------------------
+
+// ex: tractors, excavators
+
+const equipmentType = invTypeBase.discriminator('equipment', new mongoose.Schema({
+  category: { type: String, enum: ['Equipment'] },
+  fuelType: { type: String, required: true },
+}));
+
+module.exports = {
+  materialType,
+  consumableType,
+  reusableType,
+  toolType,
+  equipmentType,
+};
