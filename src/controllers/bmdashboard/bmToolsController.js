6f33bdb9160cdb0847ae576c60a43a0edc49aa33
Aishwarya Kalkundrikar <91091456+Aishwaryak01@users.noreply.github.com>
2024-01-12 13:29:32 -0500

    Update bmToolsController.js

--- src/controllers/bmdashboard/bmToolsController.js
+++ src/controllers/bmdashboard/bmToolsController.js
@@ -1,50 +1,99 @@
 const mongoose = require('mongoose');
 
-const bmToolsController = function (buildingTool) {
-  const bmPurchaseTools = async function (req, res) {
-    const {
-      projectId,
-      toolId,
-      quantity,
-      priority,
-      brand,
-      requestor: { requestorId },
-    } = req.body;
-    const newPurchaseRecord = {
-      quantity,
-      priority,
-      brand,
-      requestedBy: requestorId,
-    };
-    try {
-      const doc = await buildingTool.findOne({ project: projectId, itemType: toolId });
-      if (!doc) {
-        const newDoc = {
-          itemType: toolId,
-          project: projectId,
-          purchaseRecord: [newPurchaseRecord],
+const bmToolController = (BuildingTool) => {
+    const fetchSingleTool = async (req, res) => {
+        const { toolId } = req.params;
+        try {
+            BuildingTool
+                .findById(toolId)
+                .populate([
+                    {
+                        path: 'itemType',
+                        select: '_id name description unit imageUrl category',
+                    },
+                    {
+                        path: 'userResponsible',
+                        select: '_id firstName lastName',
+                    },
+                    {
+                        path: 'purchaseRecord',
+                        populate: {
+                            path: 'requestedBy',
+                            select: '_id firstName lastName',
+                        },
+                    },
+                    {
+                        path: 'updateRecord',
+                        populate: {
+                            path: 'createdBy',
+                            select: '_id firstName lastName',
+                        },
+                    },
+                    {
+                        path: 'logRecord',
+                        populate: [{
+                            path: 'createdBy',
+                            select: '_id firstName lastName',
+                        },
+                        {
+                            path: 'responsibleUser',
+                            select: '_id firstName lastName',
+                    }],
+                    },
+                ])
+                .exec()
+                .then(tool => res.status(200).send(tool))
+                .catch(error => res.status(500).send(error));
+        } catch (err) {
+            res.json(err);
+        }
+     };
+
+    const bmPurchaseTools = async function (req, res) {
+        const {
+          projectId,
+          toolId,
+          quantity,
+          priority,
+          brand,
+          requestor: { requestorId },
+        } = req.body;
+        const newPurchaseRecord = {
+          quantity,
+          priority,
+          brand,
+          requestedBy: requestorId,
         };
-        buildingTool
-        .create(newDoc)
-        .then(() => res.status(201).send())
-        .catch(error => res.status(500).send(error));
-        return;
-      }
-      buildingTool
-      .findOneAndUpdate(
-          { _id: mongoose.Types.ObjectId(doc._id) },
-          { $push: { purchaseRecord: newPurchaseRecord } },
-          )
-        .exec()
-        .then(() => res.status(201).send())
-        .catch(error => res.status(500).send(error));
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  };
-  return {
-    bmPurchaseTools
-    };
+        try {
+          const doc = await BuildingTool.findOne({ project: projectId, itemType: toolId });
+          if (!doc) {
+            const newDoc = {
+              itemType: toolId,
+              project: projectId,
+              purchaseRecord: [newPurchaseRecord],
+            };
+            BuildingTool
+            .create(newDoc)
+            .then(() => res.status(201).send())
+            .catch(error => res.status(500).send(error));
+            return;
+          }
+          BuildingTool
+          .findOneAndUpdate(
+              { _id: mongoose.Types.ObjectId(doc._id) },
+              { $push: { purchaseRecord: newPurchaseRecord } },
+              )
+            .exec()
+            .then(() => res.status(201).send())
+            .catch(error => res.status(500).send(error));
+        } catch (error) {
+          res.status(500).send(error);
+        }
+      };
+      return {
+        fetchSingleTool,
+        bmPurchaseTools
+        };
 };
 
-module.exports = bmToolsController;
+module.exports = bmToolController;
