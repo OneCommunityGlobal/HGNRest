ebb48b474d3ce99230ed86005e1ebc0c262211e4
Aishwarya Kalkundrikar <91091456+Aishwaryak01@users.noreply.github.com>
2024-01-12 13:28:45 -0500

    Update bmInventoryTypeController.js

--- src/controllers/bmdashboard/bmInventoryTypeController.js
+++ src/controllers/bmdashboard/bmInventoryTypeController.js
@@ -1,7 +1,7 @@
-const bmInventoryTypeController = function (InvType) {
-  const fetchMaterialTypes = async (req, res) => {
+function bmInventoryTypeController(InvType, MatType, ConsType, ReusType, ToolType, EquipType) {
+  async function fetchMaterialTypes(req, res) {
     try {
-      InvType
+      MatType
         .find()
         .exec()
         .then(result => res.status(200).send(result))
@@ -9,11 +9,11 @@ const bmInventoryTypeController = function (InvType) {
     } catch (err) {
       res.json(err);
     }
-  };
+  }
 
   const fetchToolTypes = async (req, res) => {
     try {
-      InvType
+      ToolType
         .find()
         .exec()
         .then(result => res.status(200).send(result))
@@ -21,12 +21,93 @@ const bmInventoryTypeController = function (InvType) {
     } catch (err) {
       res.json(err);
     }
-  };
+  }
 
+  async function addEquipmentType(req, res) {
+    const {
+      name,
+      desc: description,
+      fuel: fuelType,
+      requestor: { requestorId },
+    } = req.body;
+    try {
+      EquipType
+        .find({ name })
+        .then((result) => {
+          if (result.length) {
+            res.status(409).send();
+          } else {
+            const newDoc = {
+              category: 'Equipment',
+              name,
+              description,
+              fuelType,
+              createdBy: requestorId,
+            };
+            EquipType
+            .create(newDoc)
+            .then(() => res.status(201).send())
+            .catch((error) => {
+              if (error._message.includes('validation failed')) {
+                res.status(400).send(error);
+              } else {
+                res.status(500).send(error);
+              }
+            });
+          }
+        })
+        .catch(error => res.status(500).send(error));
+      } catch (error) {
+      res.status(500).send(error);
+      }
+    }
+    const fetchSingleInventoryType = async (req, res) => {
+      const { invtypeId } = req.params;
+      try {
+        const result = await InvType.findById(invtypeId).exec();
+        res.status(200).send(result);
+      } catch (error) {
+        res.status(500).send(error);
+      }
+    };
+
+    const updateNameAndUnit = async (req, res) => {
+      try {
+        const { invtypeId } = req.params;
+        const { name, unit } = req.body;
+
+        const updateData = {};
+
+        if (name) {
+          updateData.name = name;
+        }
+
+        if (unit) {
+          updateData.unit = unit;
+        }
+
+        const updatedInvType = await InvType.findByIdAndUpdate(
+          invtypeId,
+          updateData,
+          { new: true, runValidators: true },
+        );
+
+        if (!updatedInvType) {
+          return res.status(404).json({ error: 'invType Material not found check Id' });
+        }
+
+        res.status(200).json(updatedInvType);
+      } catch (error) {
+        res.status(500).send(error);
+      }
+    };
   return {
     fetchMaterialTypes,
     fetchToolTypes,
+    addEquipmentType,
+    fetchSingleInventoryType,
+    updateNameAndUnit,
   };
-};
+}
 
 module.exports = bmInventoryTypeController;
