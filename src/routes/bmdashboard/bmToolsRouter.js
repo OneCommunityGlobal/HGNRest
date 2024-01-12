20e5607845db5f57f5adb99ed75a84d9777d9b14
Aishwarya Kalkundrikar <91091456+Aishwaryak01@users.noreply.github.com>
2024-01-12 13:30:38 -0500

    Update bmToolsRouter.js

--- src/routes/bmdashboard/bmToolsRouter.js
+++ src/routes/bmdashboard/bmToolsRouter.js
@@ -1,12 +1,17 @@
 const express = require('express');
 
-const routes = function (buildingTool) {
-  const toolsRouter = express.Router();
-  const controller = require('../../controllers/bmdashboard/bmToolsController')(buildingTool);
-  toolsRouter.route('/tools/purchase')
-    .post(controller.bmPurchaseTools);
-  
-  return toolsRouter;
+const routes = function (BuildingTool) {
+    const toolRouter = express.Router();
+    const controller = require('../../controllers/bmdashboard/bmToolController')(BuildingTool);
+
+    toolRouter.route('/tools/:toolId')
+        .get(controller.fetchSingleTool);
+    
+    toolRouter.route('/tools/purchase')
+        .post(controller.bmPurchaseTools);
+
+    return toolRouter;
 };
 
 module.exports = routes;
+
