9ec3f786bb30a8f2af24bf4ea76e158f3aba1aea
Jay <jdragon2050@gmail.com>
2024-01-28 23:14:34 -0800

    add controller logic + default permission for owner

--- src/utilities/createInitialPermissions.js
+++ src/utilities/createInitialPermissions.js
@@ -179,6 +179,7 @@ const permissionsRoles = [
       'assignProjectToUsers',
       'importTask',
       'postTask',
+      'postTimeEntry',
       'updateNum',
       'updateTask',
       'swapTask',
