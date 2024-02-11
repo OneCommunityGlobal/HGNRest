abfbe936c4e1c5666ce35aee738eef67b345f3e7
One Community <one-community@users.noreply.github.com>
2024-02-11 12:20:38 -0800

    Revert "Backend Release to Main [1.42]"

--- src/models/weeklySummaryAIPrompt.js
+++ src/models/weeklySummaryAIPrompt.js
@@ -3,13 +3,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const WeeklySummaryAIPrompt = new Schema({
-  _id: { type: mongoose.Schema.Types.String },
-  aIPromptText: { type: String },
-  modifiedDatetime: { type: Date, default: Date.now() }, // The modifiedDateTime will be updated, each time there is change/ update in the AI prompt.
+    _id: { type: mongoose.Schema.Types.String },
+    aIPromptText: { type: String },
 });
 
-module.exports = mongoose.model(
-  'weeklySummaryAIPrompt',
-  WeeklySummaryAIPrompt,
-  'weeklySummaryAIPrompt',
-);
+module.exports = mongoose.model('weeklySummaryAIPrompt', WeeklySummaryAIPrompt, 'weeklySummaryAIPrompt');
