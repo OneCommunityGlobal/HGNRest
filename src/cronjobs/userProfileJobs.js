5f394dafb0e231a6669d730b6973528396c31480
Renanluizssx <renanluizssx@gmail.com>
2024-03-01 13:54:49 -0300

    commited

--- src/cronjobs/userProfileJobs.js
+++ src/cronjobs/userProfileJobs.js
@@ -6,7 +6,7 @@ const userhelper = require('../helpers/userHelper')();
 const userProfileJobs = () => {
   const allUserProfileJobs = new CronJob(
     // '* * * * *', // Comment out for testing. Run Every minute.
-    '1 0 * * 0', // Every Sunday, 1 minute past midnight.
+    '* * * * *', // Every Sunday, 1 minute past midnight.
     async () => {
       const SUNDAY = 0;
       if (moment().tz('America/Los_Angeles').day() === SUNDAY) {
