d0c320699ae304d51ac629abc6c27b37ee504ccc
Aishwarya Kalkundrikar <91091456+Aishwaryak01@users.noreply.github.com>
2024-03-05 18:08:46 -0500

    Update routes.js

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -116,14 +116,6 @@ const bmConsumablesRouter = require('../routes/bmdashboard/bmConsumablesRouter')
 const bmEquipmentsRouter = require('../routes/bmdashboard/bmEquipmentsRouter')(buildingEquipment);
 const bmInventoryTypeRouter = require('../routes/bmdashboard/bmInventoryTypeRouter')(invTypeBase, materialType, consumableType, reusableType, toolType, equipmentType);
 const bmToolRouter = require('../routes/bmdashboard/bmToolRouter')(buildingTool);
-const bmInventoryTypeRouter = require('../routes/bmdashboard/bmInventoryTypeRouter')(
-    invTypeBase,
-    materialType,
-    consumableType,
-    reusableType,
-    toolType,
-    equipmentType,
-  );
 const bmToolRouter = require('../routes/bmdashboard/bmToolRouter')(
   buildingTool,
 );
