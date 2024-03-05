042664af1c90df8670b9342d358774f7275a389b
One Community <one-community@users.noreply.github.com>
2024-03-05 15:28:35 -0800

    Revert "Revert "Revert "Abdel update bleu square scheduler"""

--- src/models/timeOffRequest.js
+++ src/models/timeOffRequest.js
@@ -8,6 +8,7 @@ const timeOffRequest = new Schema({
   startingDate: { type: Date, required: true },
   endingDate: { type: Date },
   duration: { type: Number, required: true }, // in weeks
-}, { timestamps: true });
+
+});
 
 module.exports = mongoose.model('timeOffRequest', timeOffRequest, 'timeOffRequests');
