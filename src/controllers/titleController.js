643908671c0139519adc794af7540560e246c8c2
Yaohong Xiang <xiang.yao@northeastern.edu>
2024-03-18 22:11:01 -0400

    add title model and routes

---
+++ src/controllers/titleController.js
@@ -0,0 +1,51 @@
+const titlecontroller = function (Title) {
+    const getAllTitles = function (req, res) {
+      Title.find({})
+        .then((results) => res.status(200).send(results))
+        .catch((error) => res.status(404).send(error));
+  };
+
+    const getTitleById = function (req, res) {
+      const { titleId } = req.params;
+
+      Title.findById(titleId)
+        .then((results) => res.send(results))
+        .catch((error) => res.send(error));
+    };
+
+    const postTitle = async function (req, res) {
+      const title = new Title();
+
+      title.titleName = req.body.titleName;
+      title.teamCode = req.body.teamCode;
+      title.projectAssigned = req.body.projectAssigned;
+      title.mediaFolder = req.body.mediaFolder;
+      title.teamAssiged = req.body.teamAssiged;
+      // get the shortname
+      const shortnames = title.titleName.split(' ');
+      const shortname = (shortnames[0][0] + shortnames[1][0]).toUpperCase();
+
+      title.shortName = shortname;
+
+      title
+        .save()
+        .then((results) => res.status(200).send(results))
+        .catch((error) => res.status(404).send(error));
+    };
+
+    const deleteTitleById = async function (req, res) {
+      const { titleId } = req.params;
+      Title.deleteOne({ _id: titleId })
+        .then((result) => res.send(result))
+        .catch((error) => res.send(error));
+    };
+
+    return {
+      getAllTitles,
+      getTitleById,
+      postTitle,
+      deleteTitleById,
+    };
+  };
+
+  module.exports = titlecontroller;
---
+++ src/routes/titleRouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = function (title) {
+  const controller = require('../controllers/titleController')(title);
+
+  const titleRouter = express.Router();
+
+  titleRouter.route('/title')
+    .get(controller.getAllTitles)
+    .post(controller.postTitle);
+
+  titleRouter.route('/title/:titleId')
+    .get(controller.getTitleById)
+    .put(controller.deleteTitleById);
+
+  return titleRouter;
+};
+
+module.exports = router;
