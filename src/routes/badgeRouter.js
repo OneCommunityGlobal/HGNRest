97f094703f9cbb72bdc16b43ba0818ff68b9d4ce
Xiao Zhang <zx694665334@gmail.com>
2024-03-20 15:32:49 -0700

    Update badgeRouter.js

--- src/routes/badgeRouter.js
+++ src/routes/badgeRouter.js
@@ -4,6 +4,7 @@ const routes = function (badge) {
   const controller = require('../controllers/badgeController')(badge);
 
   const badgeRouter = express.Router();
+  
   badgeRouter.route('/badge')
     .get(controller.getAllBadges)
     .post(controller.postBadge);
