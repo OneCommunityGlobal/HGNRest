58e840742aff6e3f7ee9085361adaed06c7637af
ilyaflaks <ilyaflaks@gmail.com>
2024-03-22 09:16:32 -0700

    fine tuned the nested loops

--- src/controllers/bmdashboard/bmToolController.js
+++ src/controllers/bmdashboard/bmToolController.js
@@ -102,47 +102,34 @@ const bmToolController = (BuildingTool, ToolType) => {
       };
 
       const bmLogTools = async function (req, res) {
-        //TODO: add loops to accept an array of toolItem id's
-        //logically this should accept a type id too, I await the currentToolType early on,
-        //the loop through array of tools added and perform the function below. 
-        //might have to declare a new available and used constants to keep track of shit outside of the loop and update the tool type after to loop exits
-        //this is going to be a pretty slow function...
-        console.log("*******************************************")
-        console.log("bmLogTools in bmToolController called");
+        // console.log("*******************************************")
+        // console.log("bmLogTools in bmToolController called");
         const requestor = req.body.requestor.requestorId;
-        // const typesArray = req.body.typesArray;
-        // const action = req.body.action; 
-        // const date = req.body.date;
         const {typesArray, action, date} = req.body
-        console.log("requestor: ",requestor, /*", typesArray: ", typesArray,*/ ", action: ",action, ", date: ", date);
+        // console.log("requestor: ",requestor, ", action: ",action, ", date: ", date);
 
          
         try{
-          console.log("try block")
           for (const type of typesArray) {
-            console.log("looping typesArray")
             const toolTypeDoc = await ToolType.findOne({ _id: mongoose.Types.ObjectId(type.toolType) });
             if (!toolTypeDoc) {
-             console.log("Tool type with this id was not found")
+            //  console.log("Tool type with this id was not found")
              return res.status(404).send('Tool type with this id was not found.');
             }
-            console.log("toolTypeDoc found, name: ", toolTypeDoc.name)
+            // console.log("toolTypeDoc found, name: ", toolTypeDoc.name)
             const availableItems = toolTypeDoc.available;
             const usingItems = toolTypeDoc.using;
-            console.log("availableItems: ", availableItems, ", usingItems: ", usingItems)
-            //is it more efficient to loop through type.toolItems first and then perform an action accord to check in or check out? Might reduce the amount of code! 
-        
+            // console.log("availableItems: ", availableItems, ", usingItems: ", usingItems)
             
             type.toolItems.forEach(async toolItem=>{
-              console.log("looping toolItems. toolItem code: ", toolItem.code);
+              // console.log("looping toolItems. toolItem id: ", toolItem);
               if(action === "Check Out" && availableItems.length > 0){
-                  console.log("check out && availableItems > 0")
+                  // console.log("check out && availableItems > 0")
                   const foundIndex = availableItems.indexOf(toolItem);
                   if(foundIndex >= 0){
-                    console.log("found toolItem in availableItems: ", foundIndex);
+                    // console.log("found toolItem in availableItems: ", foundIndex);
                     availableItems.splice(foundIndex, 1);
                     usingItems.push(toolItem);
-                    console.log("Check out spliced and pushed")
                   }else{
                     console.log("Didn't find this id in availableItems array")
                     //return 400
@@ -153,14 +140,11 @@ const bmToolController = (BuildingTool, ToolType) => {
               }
 
               if(action === "Check In" && usingItems.length > 0){
-                console.log("check in && usingItems > 0")
                 const foundIndex = usingItems.indexOf(toolItem);
-                  
                   if(foundIndex >= 0){
-                    console.log("found toolItem in usingItems: ", foundIndex);
+                    // console.log("found toolItem in usingItems: ", foundIndex);
                     usingItems.splice(foundIndex, 1);
                     availableItems.push(toolItem);
-                    console.log("Check in spliced and pushed")
                   }else{
                     console.log("Didn't find this id in availableItems array")
                     // return 400
@@ -175,20 +159,25 @@ const bmToolController = (BuildingTool, ToolType) => {
                 console.log("STATUS 404 didnt find item ");
                 // return res.status(404).send({message: 'Tool item with this id was not found.'});
               }
-              console.log("found item buildingToolDoc, ", buildingToolDoc._id);
+              // console.log("found item buildingToolDoc, ", buildingToolDoc._id);
               const newRecord = {
                 date: date,
                 createdBy: requestor,
                 responsibleUser: buildingToolDoc.userResponsible, 
                 type: action
               }
+              // console.log("newRecord: ", newRecord);
               buildingToolDoc.logRecord.push(newRecord);
               console.log("buildingToolDoc.logRecord: ", buildingToolDoc.logRecord)
               buildingToolDoc.save();
-              console.log("saved buildingToolDoc");
+              // console.log("saved buildingToolDoc (tool item)");
             })
+
             await toolTypeDoc.save();
+            console.log("saved toolTypeDoc");
+            console.log("######################## end of outer loop iteration");
           } //end loop through typesArray 
+          console.log("end loop through typesArray ");
           res.status(200).send({message: 'Log request processed successfully!'});
         }catch(error){
             console.log("183. error: ", error); //delete later
