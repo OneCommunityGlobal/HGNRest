11218cf6b355eab4524c6276b266d4561a023e6b
brandta-1 <116298512+brandta-1@users.noreply.github.com>
2024-03-22 11:58:27 -0400

    controller for ChatGPT summaries

--- src/config.js
+++ src/config.js
@@ -1,18 +1,17 @@
 const config = {};
 
 config.JWT_SECRET = process.env.JWT_SECRET;
-config.REQUEST_AUTHKEY = 'Authorization';
+config.REQUEST_AUTHKEY = "Authorization";
 config.TOKEN = {
   Lifetime: process.env.TOKEN_LIFETIME,
   Units: process.env.TOKEN_LIFETIME_UNITS,
 };
 config.JWT_HEADER = {
-  alg: 'RS256',
-  typ: 'JWT',
+  alg: "RS256",
+  typ: "JWT",
 };
 
 // ChatGPT configurations
-config.CHATGPT_API_KEY = process.env.CHATGPT_API_KEY;
-config.CHATGPT_API_URL = 'https://api.chatgpt.com';
+config.OPENAI_API_KEY_API_KEY = process.env.OPENAI_API_KEY;
 
 module.exports = config;
--- src/controllers/chatGPTController.js
+++ src/controllers/chatGPTController.js
@@ -1,49 +1,61 @@
 // Import the 'openai' package
-global.AbortController = require('abort-controller');
-const OpenAI = require('openai');
-const TimeEntry = require('../models/timeentry');
+global.AbortController = require("abort-controller");
+const OpenAI = require("openai");
+const TimeEntry = require("../models/timeentry");
+const AIPrompt = require("../models/weeklySummaryAIPrompt");
 
-const config = require('../config'); // Import config file
-const TimeEntryController = require('./timeEntryController');
+const config = require("../config"); // Import config file
+const TimeEntryController = require("./timeEntryController");
 // Initialize the OpenAI client with the ChatGPT API key from config.js
 const openai = new OpenAI({
   apiKey: config.CHATGPT_API_KEY, // Use the key from your config file
-  apiUrl: config.CHATGPT_API_URL,
 });
-
-// Remove URLs from the notes
-const processNotes = notes => notes.map(note => note.replace(/https?:\/\/[^\s]+/g, ''));
-
+/**
+ * takes in a request which contains the user's name and id
+ * @param {Object} req - Request object.
+ * @param {Object} res - Response object.
+ * @returns {Object} json containing a string from ChatGPT
+ */
 const interactWithChatGPT = async (req, res) => {
-    try {
-        const notes = await TimeEntryController(TimeEntry).getTimeEntriesForCurrentWeek(req);
-        if (!notes || (Array.isArray(notes) && notes.length === 0)) {
-            return res.status(200).json({ response: 'No notes found for the current week.' });
-        }
-        const processedNotes = processNotes(notes);
-        // console.log(processedNotes, 987654);
-        const instruction = 'translate following into Chinese.';
-        // `Summarize the following notes of my week's work, Make sure it is professionally written in 3rd person format.
-        // Write it as only one paragraph. Keep it less than 500 words. Start the paragraph with 'This week'.
-        // Make sure the paragraph contains no links or URLs and write it in a tone that is matter-of-fact and without embellishment.
-        // Do not add flowery language, keep it simple and factual. Do not add a final summary sentence.`;
-        try {
-            const gptResponse = await openai.completions.create({
-                model: 'davinci',
-                prompt: `${instruction}\n\n${processedNotes}`,
-                max_tokens: 150,
-            });
-            // console.log(`${instruction}\n\n${processedNotes}`, 111);
-            // console.log(gptResponse, 90);
-        return res.json({ response: gptResponse.choices[0].text.trim() });
-        } catch (e) {
-            // console.log(openai.completions.create);
-            // console.log(e, 999);
-            res.json({ data: e });
-        }
-    } catch (error) {
-        return res.status(500).json({ error: "123Failed to get ChatGPT response for the current week's notes." });
+  try {
+    // get the user's notes and the latest prompt
+    let notes = await TimeEntryController(
+      TimeEntry,
+    ).getTimeEntriesForCurrentWeek(req.body.requestor);
+    const prompt = await AIPrompt.findById({ _id: "ai-prompt" });
+
+    if (!notes || (Array.isArray(notes) && notes.length === 0)) {
+      return res
+        .status(200)
+        .json({ response: "No notes found for the current week." });
     }
+
+    notes = notes.map((note) => note.replace(/https?:\/\/[^\s]+/g, ""));
+    notes = notes.join(" ");
+
+    const instruction = prompt.aIPromptText.replace(
+      "This week",
+      `This week ${req.body.firstName}`,
+    );
+
+    try {
+      const gptResponse = await openai.chat.completions.create({
+        messages: [{ role: "system", content: `${instruction}\n\n${notes}` }],
+        model: "gpt-3.5-turbo",
+      });
+
+      res.status(200);
+      return res.json({ GPTSummary: gptResponse.choices[0].message.content });
+    } catch (e) {
+      res.status(500).json({ data: e.message });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      error: "123Failed to get ChatGPT response for the current week's notes.",
+    });
+  }
+
+  return null;
 };
 
 module.exports = interactWithChatGPT;
