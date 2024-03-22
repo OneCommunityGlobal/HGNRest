f6ed44aae5dfaa5d66c1b0360d37965877438cbc
Shengwei Peng <ptrpengdev@gmail.com>
2024-03-22 12:36:51 -0400

    Modify notification feature in the sysetm  - Remove unused legacy code for the notification feature, including the controller, helper, and contants file  - Refactor notification related file, including model schema, controller, and routes.

--- src/models/notification.js
+++ src/models/notification.js
@@ -2,13 +2,19 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+
 const notificationSchema = new Schema({
 
   message: { type: String, required: true },
-  recipient: { type: Schema.Types.ObjectId, ref: 'userProfile' },
+  sender: { type: Schema.Types.ObjectId, ref: 'userProfile', required: true },
+  recipient: { type: Schema.Types.ObjectId, ref: 'userProfile', required: true },
+  isSystenGenerated: { type: Boolean, default: false },
   isRead: { type: Boolean, default: false },
-  eventType: { type: String },
+  createdTimeStamps: { type: Date, default: Date.now },
 
 });
+
+notificationSchema.index({ recipient: 1, createdTimeStamps: 1, isRead: 1 });
+notificationSchema.index({ sender: 1, createdTimeStamps: 1 });
 
 module.exports = mongoose.model('notification', notificationSchema, 'notifications');
