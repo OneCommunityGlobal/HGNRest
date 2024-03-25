9640088eab0c41c01a7cdaeaf03973c7cca898f3
Abi Liu <abiliu018@gmail.com>
2024-03-25 16:42:28 -0700

    test: Ensures postactionItem returns 400 if any error occurs during save.

---
+++ src/controllers/actionItemController.spec.js
@@ -0,0 +1,61 @@
+const actionItemController = require('./actionItemController');
+const {
+  mockReq,
+  mockRes,
+  mongoHelper: { dbConnect, dbDisconnect },
+  assertResMock,
+} = require('../test');
+// const notificationhelper = require('../helpers/notificationhelper')();
+const ActionItem = require('../models/actionItem');
+
+// Sut = Systems Under Test, aka the functions inside the controllers we are testing.
+// this function creates the actionItemController then returns the individual functions inside the controller.
+const makeSut = () => {
+  const { postactionItem } = actionItemController(ActionItem);
+
+  return {
+    postactionItem,
+  };
+};
+
+describe('Action Item Controller tests', () => {
+  beforeAll(async () => {
+    await dbConnect();
+  });
+
+  beforeEach(() => {
+    mockReq.params.userid = '5a7e21f00317bc1538def4b7';
+    mockReq.body.requestor = {
+      requestorId: '5a7e21f00317bc1538def4b7',
+      assignedTo: '5a7ccd20fde60f1f1857ba16',
+    };
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(async () => {
+    await dbDisconnect();
+  });
+
+  describe('postactionItem function', () => {
+    test('Ensures postactionItem returns 400 if any error occurs during save.', async () => {
+      const { postactionItem } = makeSut();
+      const errorMsg = 'Error occured during save.';
+      jest
+        .spyOn(ActionItem.prototype, 'save')
+        .mockImplementationOnce(() => Promise.reject(new Error(errorMsg)));
+
+      const response = await postactionItem(mockReq, mockRes);
+
+      assertResMock(400, new Error(errorMsg), response, mockRes);
+    });
+  });
+
+  // describe("getActionItem function", () => {
+  //     test('Ensures getactionItem returns 400 if userid is not in req.params', async () => {
+  //         const { getactionItem } = makeSut()
+  //     })
+  // })
+});
