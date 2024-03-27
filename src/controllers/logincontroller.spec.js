e6ff0d9b8a5adc355dec7d8ab07cf88685a28336
Anirudh Dutt <anidutt27@gmail.com>
2024-03-27 13:08:10 -0700

    test: Returns error 400 if there is no email or password

--- src/controllers/logincontroller.spec.js
+++ src/controllers/logincontroller.spec.js
@@ -2,8 +2,9 @@ const logincontroller = require('./logincontroller');
 const { mockReq, mockRes, assertResMock } = require('../test');
 
 const makeSut = () => {
-  const { getUser } = logincontroller();
+  const { login, getUser } = logincontroller();
   return {
+    login,
     getUser,
   };
 };
@@ -13,9 +14,28 @@ describe('logincontroller module', () => {
     jest.clearAllMocks();
   });
 
+  describe.only('login', () => {
+    test.only('Ensure login returns error 400 if there is no email or password', async () => {
+      const { login } = makeSut();
+      const mockReqModified = {
+        ...mockReq,
+        ...{
+          body: {
+            email: 'hello@gmail.com',
+            password: '',
+          },
+        },
+      };
+      // console.log("REQ MOD ", mockReqModified)
+      const res = await login(mockReqModified, mockRes);
+      assertResMock(400, { error: 'Invalid request' }, res, mockRes);
+    });
+  });
+
   describe('getUser', () => {
     it('Ensure getUser returns 200, with the requestor body', () => {
       const { getUser } = makeSut();
+
       const res = getUser(mockReq, mockRes);
       assertResMock(200, mockReq.body.requestor, res, mockRes);
     });
