8f12e083ee957d78dc3e0ff1aa33edb2aeb46149
Abi Liu <abiliu018@gmail.com>
2024-03-27 11:36:13 -0700

    test: integration tests for getActionItem routes

--- src/routes/actionItemRouter.test.js
+++ src/routes/actionItemRouter.test.js
@@ -13,7 +13,7 @@ describe('actionItem routes', () => {
   let requestorUser;
   let assignedUser;
   let token;
-  const reqBody = {
+  let reqBody = {
     ...mockReq.body,
   };
 
@@ -22,6 +22,12 @@ describe('actionItem routes', () => {
     requestorUser = await createUser(); // requestor user
     assignedUser = await createUser(); // assignedTo user
     token = jwtPayload(requestorUser);
+    reqBody = {
+      ...reqBody,
+      requestor: { requestorId: requestorUser._id, assignedTo: assignedUser._id },
+      description: 'Any description',
+      assignedTo: assignedUser._id,
+    };
   });
 
   beforeEach(async () => {
@@ -35,28 +41,17 @@ describe('actionItem routes', () => {
 
   describe('postactionItem', () => {
     it('should return 401 if authorization header is not present', async () => {
-      await agent.post('/api/actionItem').send(reqBody.body).expect(401);
+      await agent.post('/api/actionItem').send(reqBody).expect(401);
     });
 
     it('Should return 404 if the route does not exist', async () => {
-      await agent
-        .post('/api/actionItems')
-        .send(reqBody.body)
-        .set('Authorization', token)
-        .expect(404);
+      await agent.post('/api/actionItems').send(reqBody).set('Authorization', token).expect(404);
     });
 
     it('Should create an actionItem on success', async () => {
-      const postReqBody = {
-        ...reqBody,
-        requestor: { requestorId: requestorUser._id, assignedTo: assignedUser._id },
-        description: 'Any description',
-        assignedTo: assignedUser._id,
-      };
-
       const response = await agent
         .post('/api/actionItem')
-        .send(postReqBody)
+        .send(reqBody)
         .set('Authorization', token)
         .expect(200);
 
@@ -64,8 +59,48 @@ describe('actionItem routes', () => {
         _id: expect.anything(),
         assignedTo: assignedUser._id.toString(),
         createdBy: 'You',
-        description: postReqBody.description,
+        description: reqBody.description,
       });
+    });
+  });
+
+  describe('getActionItem', () => {
+    it('should return 401 if authorization header is not present', async () => {
+      await agent.get(`/api/actionItem/user/${assignedUser._id}`).expect(401);
+    });
+
+    it('Should return 404 if the route does not exist', async () => {
+      await agent
+        .get(`/api/actionItems/user/${assignedUser._id}`)
+        .set('Authorization', token)
+        .expect(404);
+    });
+
+    it('Should return all actionItems that are assigned to the user on success', async () => {
+      // in the case that there are no actionItems, we expect a response of []
+      let response = await agent
+        .get(`/api/actionItem/user/${assignedUser._id}`)
+        .set('Authorization', token)
+        .expect(200);
+
+      expect(response.body).toEqual([]);
+
+      // now we add actionItems for assignedUser
+      await agent.post('/api/actionItem').send(reqBody).set('Authorization', token).expect(200);
+
+      response = await agent
+        .get(`/api/actionItem/user/${assignedUser._id}`)
+        .set('Authorization', token)
+        .expect(200);
+
+      expect(response.body).toEqual([
+        {
+          _id: expect.anything(),
+          assignedTo: assignedUser._id.toString(),
+          createdBy: `${requestorUser.firstName} ${requestorUser.lastName}`,
+          description: reqBody.description,
+        },
+      ]);
     });
   });
 });
