8945e95f752a270fd8cb103e80a9ee3837fceaaa
Anirudh Dutt <anidutt27@gmail.com>
2024-03-28 15:14:07 -0700

    test: Returns error 403 if the user exists but is not active

--- src/controllers/logincontroller.spec.js
+++ src/controllers/logincontroller.spec.js
@@ -1,5 +1,5 @@
 const logincontroller = require('./logincontroller');
-const { mockReq, mockRes, assertResMock } = require('../test');
+const { mockReq, mockRes, assertResMock, mockUser } = require('../test');
 const userProfile = require('../models/userProfile');
 
 const makeSut = () => {
@@ -15,7 +15,7 @@ describe('logincontroller module', () => {
     jest.clearAllMocks();
   });
 
-  describe('login', () => {
+  describe.only('login', () => {
     test('Ensure login returns error 400 if there is no email or password', async () => {
       const { login } = makeSut();
       const mockReqModified = {
@@ -49,6 +49,41 @@ describe('logincontroller module', () => {
       const res = await login(mockReqModified, mockRes);
       expect(findOneSpy).toHaveBeenCalledWith({ email: mockReqModified.body.email });
       assertResMock(403, { message: 'Username not found.' }, res, mockRes);
+    });
+
+    test.only('Ensure login returns error 403 if the user exists but is not active', async () => {
+      const { login } = makeSut();
+      const mockReqModified = {
+        ...mockReq,
+        ...{
+          body: {
+            email: 'example@test.com',
+            password: 'exampletest',
+          },
+        },
+      };
+      const mockUserModified = {
+        ...mockUser,
+        ...{
+          isActive: false,
+        },
+      };
+
+      const findOneSpy = jest
+        .spyOn(userProfile, 'findOne')
+        .mockImplementation(() => Promise.resolve(mockUserModified));
+
+      const res = await login(mockReqModified, mockRes);
+      expect(findOneSpy).toHaveBeenCalledWith({ email: mockReqModified.body.email });
+      assertResMock(
+        403,
+        {
+          message:
+            'Sorry, this account is no longer active. If you feel this is in error, please contact your Manager and/or Administrator.',
+        },
+        res,
+        mockRes,
+      );
     });
   });
 
