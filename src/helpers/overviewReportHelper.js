8543db45f2667d8a15be1123e8cfa32ebd59fb99
Abi Liu <abiliu018@gmail.com>
2024-04-03 19:54:28 -0700

    fix: fixed small bug in the getVolunteerHourStats method

--- src/helpers/overviewReportHelper.js
+++ src/helpers/overviewReportHelper.js
@@ -1,26 +1,25 @@
 /* eslint-disable quotes */
 const Team = require('../models/team');
 const UserProfile = require('../models/userProfile');
-const timeEntries = require('../models/timeentry');
+// const timeEntries = require('../models/timeentry');
 
 const overviewReportHelper = function () {
   /**
-   * 
+   *
    * @returns The number of teams with 4 or more members.
    */
-  async function getFourPlusMembersTeamCount () {
+  async function getFourPlusMembersTeamCount() {
     // check if members array has 4 or more members
     return Team.countDocuments({ 'members.4': { $exists: true } });
   }
 
-
   /**
    * Get the total number of badges awarded between the two input dates.
-   * @param {*} startDate 
-   * @param {*} endDate 
+   * @param {*} startDate
+   * @param {*} endDate
    * @returns The total number of badges awarded between the two input dates.
    */
-  async function getTotalBadgesAwardedCount (startDate, endDate) {
+  async function getTotalBadgesAwardedCount(startDate, endDate) {
     return UserProfile.aggregate([
       {
         $unwind: '$badgeCollection',
@@ -41,16 +40,16 @@ const overviewReportHelper = function () {
 
   /**
    *  Get the number of users celebrating their anniversary between the two input dates.
-   * @param {*} startDate 
-   * @param {*} endDate 
+   * @param {*} startDate
+   * @param {*} endDate
    * @returns  The number of users celebrating their anniversary between the two input dates.
    */
-  async function getAnniversaryCount (startDate, endDate) {
+  async function getAnniversaryCount(startDate, endDate) {
     return UserProfile.aggregate([
       {
         $addFields: {
-          createdMonthDay: { $dateToString: { format: "%m-%d", date: "$createdDate" } }
-        }
+          createdMonthDay: { $dateToString: { format: '%m-%d', date: '$createdDate' } },
+        },
       },
       {
         $match: {
@@ -70,7 +69,7 @@ const overviewReportHelper = function () {
    * Get the role and count of users.
    * @returns The role and count of users.
    */
-  async function getRoleCount () {
+  async function getRoleCount() {
     return UserProfile.aggregate([
       {
         $group: {
@@ -83,11 +82,11 @@ const overviewReportHelper = function () {
 
   /**
    * Get the number of Blue Square infringements between the two input dates.
-   * @param {*} startDate 
-   * @param {*} endDate 
-   * @returns 
+   * @param {*} startDate
+   * @param {*} endDate
+   * @returns
    */
-  async function getBlueSquareStats (startDate, endDate) {
+  async function getBlueSquareStats(startDate, endDate) {
     return UserProfile.aggregate([
       {
         $unwind: '$infringements',
@@ -155,54 +154,57 @@ const overviewReportHelper = function () {
     const hoursStats = await UserProfile.aggregate([
       {
         $lookup: {
-          from: "timeEntries", // The collection to join
-          localField: "_id", // Field from the userProfile collection
-          foreignField: "personId", // Field from the timeEntries collection
-          as: "timeEntries" // The array field that will contain the joined documents
-        }
+          from: 'timeEntries', // The collection to join
+          localField: '_id', // Field from the userProfile collection
+          foreignField: 'personId', // Field from the timeEntries collection
+          as: 'timeEntries', // The array field that will contain the joined documents
+        },
       },
       {
         $unwind: {
-          path: "$timeEntries",
-          preserveNullAndEmptyArrays: true // Preserve users with no time entries
-        }
+          path: '$timeEntries',
+          preserveNullAndEmptyArrays: true, // Preserve users with no time entries
+        },
       },
       {
         $match: {
           // Adjust this condition to include all users, filtering timeEntries by date
           $or: [
-            { "timeEntries.dateOfWork": { $gte: startDate, $lte: endDate } },
-            { "timeEntries": { $exists: false } }
-          ]
-        }
+            { 'timeEntries.dateOfWork': { $gte: startDate, $lte: endDate } },
+            { timeEntries: { $exists: false } },
+          ],
+        },
       },
       {
         $group: {
-          _id: "$_id",
-          personId: { $first: "$_id" },
-          totalSeconds: { $sum: "$timeEntries.totalSeconds" }, // Sum seconds from timeEntries
-        }
+          _id: '$_id',
+          personId: { $first: '$_id' },
+          totalSeconds: { $sum: '$timeEntries.totalSeconds' }, // Sum seconds from timeEntries
+        },
       },
       {
         $project: {
-          totalHours: { $divide: ["$totalSeconds", 3600] }, // Convert seconds to hours
-        }
+          totalHours: { $divide: ['$totalSeconds', 3600] }, // Convert seconds to hours
+        },
       },
     ]);
 
     const volunteerHoursStats = {
       numberOfUsers: hoursStats.length,
-    }
+    };
 
-    for (let i = 0; i < 6; i++) {
+    // we cannot start the loop at 0 or else the parameters inside the volunteerHourStats will be off by a multiple of 10
+    for (let i = 1; i < 7; i + 1) {
       const group = i * 10;
       volunteerHoursStats[`${group}-${group + 9}`] = 0;
     }
 
     // Group users by the number of hours logged
-    hoursStats.forEach(user => {
+    hoursStats.forEach((user) => {
       if (user.totalHours >= 60) {
-        volunteerHoursStats['60+'] = volunteerHoursStats['60+'] ? volunteerHoursStats['60+'] + 1 : 1;
+        volunteerHoursStats['60+'] = volunteerHoursStats['60+']
+          ? volunteerHoursStats['60+'] + 1
+          : 1;
       } else {
         const group = Math.floor(user.totalHours / 10) * 10;
         volunteerHoursStats[`${group}-${group + 9}`] += 1;
