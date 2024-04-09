9051625b32daffd3cc87168a09f664b5360e4617
Yaohong Xiang <xiang.yao@northeastern.edu>
2024-04-09 18:02:00 -0400

    Add validation for set up model fields

--- src/controllers/titleController.js
+++ src/controllers/titleController.js
@@ -1,3 +1,6 @@
+const Team = require('../models/team');
+const Project = require('../models/project');
+
 const titlecontroller = function (Title) {
     const getAllTitles = function (req, res) {
       Title.find({})
@@ -21,12 +24,38 @@ const titlecontroller = function (Title) {
       title.projectAssigned = req.body.projectAssigned;
       title.mediaFolder = req.body.mediaFolder;
       title.teamAssiged = req.body.teamAssiged;
-      // get the shortname
-      const shortnames = title.titleName.split(' ');
-      const shortname = (shortnames[0][0] + shortnames[1][0]).toUpperCase();
 
+      
+      // valid title name
+      if (!title.titleName.trim()) {
+        res.status(400).send({ message: 'Title cannot be empty.' });
+        return;
+      }
+
+      const shortnames = title.titleName.split(' ');
+      let shortname;
+      if (shortnames.length > 1) {
+          shortname = (shortnames[0][0] + shortnames[1][0]).toUpperCase();
+      } else if (shortnames.length === 1) {
+          shortname = shortnames[0][0].toUpperCase();
+      }
       title.shortName = shortname;
 
+
+      // Validate team code by checking if it exists in the database
+      const teamCodeExists = await checkTeamCodeExists(title.teamCode);
+      if (!teamCodeExists) {
+        return res.status(400).send({ message: 'Invalid team code. Please provide a valid team code.' });
+      }
+
+      // validate if project exist
+      const projectExist = await checkProjectExists(title.projectAssigned._id);
+      if (!projectExist) {
+        return res.status(400).send({ message: 'Project does not exist.' });
+      }
+
+
+      
       title
         .save()
         .then((results) => res.status(200).send(results))
@@ -40,12 +69,47 @@ const titlecontroller = function (Title) {
         .catch((error) => res.send(error));
     };
 
-    return {
-      getAllTitles,
-      getTitleById,
-      postTitle,
-      deleteTitleById,
+    const deleteAllTitles = async function (req, res) {
+      Title.deleteMany({})
+        .then((result) => {
+            if (result.deletedCount === 0) {
+                res.send({ message: 'No titles found to delete.' });
+            } else {
+                res.send({ message: `${result.deletedCount} titles were deleted successfully.` });
+            }
+        })
+        .catch((error) => {
+            res.status(500).send(error);
+        });
     };
+
+    async function checkTeamCodeExists(teamCode) {
+      try {
+        const team = await Team.findOne({ teamCode }).exec();
+        return !!team;
+      } catch (error) {
+        console.error('Error checking if team code exists:', error);
+        throw error;
+      }
+    }
+
+    async function checkProjectExists(projectID) {
+      try {
+        const project = await Project.findOne({ _id: projectID }).exec();
+        return !!project;
+      } catch (error) {
+        console.error('Error checking if project exists:', error);
+        throw error;
+      }
+    }
+
+  return {
+    getAllTitles,
+    getTitleById,
+    postTitle,
+    deleteTitleById,
+    deleteAllTitles,
   };
+};
 
   module.exports = titlecontroller;
