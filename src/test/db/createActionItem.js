2a0cc1ab6e637447c31c01ff3cb97263f074a15a
DiegoSalas27 <dominicsc2hs@gmail.com>
2024-04-10 20:47:56 -0400

    chore: putting back unit and integration tests

---
+++ src/test/db/createActionItem.js
@@ -0,0 +1,15 @@
+const ActionItem = require('../../models/actionItem');
+
+const createActionItem = async (assignedToId, createdById) => {
+    const _actionItem = new ActionItem();
+
+    _actionItem.description = 'Any description';
+    _actionItem.assignedTo = assignedToId;
+    _actionItem.createdBy = createdById;
+    _actionItem.createdDateTime = new Date().toISOString();
+
+    const actionItem = await _actionItem.save();
+    return actionItem;
+};
+
+module.exports = createActionItem;
