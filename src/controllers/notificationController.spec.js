0b0579e5404787e03d25989df892b0d5633cc3a2
Jordy <jordycorporan95@gmail.com>
2024-04-22 16:15:18 -0400

    test(notificationController): add test for invalid userId in getUserNotifications

---
+++ src/controllers/notificationController.spec.js
@@ -0,0 +1,30 @@
+const notificationController = require('./notificationController');
+const Notification = require('../models/notification');
+const { mockReq, mockRes, assertResMock } = require('../test');
+
+const makeSut = () => {
+  const { getUserNotifications } = notificationController(Notification);
+
+  return {
+    getUserNotifications,
+  };
+};
+
+describe('Notification controller tests', () => {
+  beforeEach(() => {
+    mockReq.params.userId = '65cf6c3706d8ac105827bb2e';
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('getUserNotifications', () => {
+    test('Ensures getUserNotifications returns 400 if userId is not valid', () => {
+      const { getUserNotifications } = makeSut();
+      mockReq.params.userId = 'invalidId';
+      const response = getUserNotifications(mockReq, mockRes);
+      assertResMock(400, { error: 'Bad Request' }, response, mockRes);
+    });
+  });
+});
