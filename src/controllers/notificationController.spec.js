03d057897736cba19f778f527770aee3f09a9298
Jordy <jordycorporan95@gmail.com>
2024-04-22 16:20:20 -0400

    test(notificationController): expand error handling tests for getUserNotifications

--- src/controllers/notificationController.spec.js
+++ src/controllers/notificationController.spec.js
@@ -10,6 +10,8 @@ const makeSut = () => {
   };
 };
 
+const flushPromises = () => new Promise(setImmediate);
+
 describe('Notification controller tests', () => {
   beforeEach(() => {
     mockReq.params.userId = '65cf6c3706d8ac105827bb2e';
@@ -25,6 +27,16 @@ describe('Notification controller tests', () => {
       mockReq.params.userId = 'invalidId';
       const response = getUserNotifications(mockReq, mockRes);
       assertResMock(400, { error: 'Bad Request' }, response, mockRes);
+    });
+
+    test('Ensures getUserNotifications returns 400 if userId is valid but finding user fails', async () => {
+      const { getUserNotifications } = makeSut();
+      jest
+        .spyOn(Notification, 'find')
+        .mockImplementationOnce(() => Promise.reject(new Error('error')));
+      const response = getUserNotifications(mockReq, mockRes);
+      await flushPromises();
+      assertResMock(400, new Error('error'), response, mockRes);
     });
   });
 });
