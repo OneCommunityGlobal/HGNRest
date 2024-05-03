3794fb881ef34f68c49ddc20b84a92bf4ef0d17f
Jordy <jordycorporan95@gmail.com>
2024-05-03 02:39:50 -0400

    test(notificationController): add postive case to test suite
    Added Positive case 200 for getUserNotifications. Furthermore, I updated the docs for the negative and postive cases. Still need to add one last negative to complete the suite.

--- src/controllers/notificationController.spec.js
+++ src/controllers/notificationController.spec.js
@@ -1,5 +1,6 @@
 const notificationController = require('./notificationController');
 const Notification = require('../models/notification');
+const notificationService = require('../services/notificationService');
 const { mockReq, mockRes } = require('../test');
 
 const makeSut = () => {
@@ -30,12 +31,33 @@ describe('Notification controller tests', () => {
     });
     test('Ensures getUserNotifications returns error 403 if userId does not match requestorId', async () => {
       const { getUserNotifications } = makeSut();
-      mockReq.params = { userId: '65cf6c3706d8ac105827bb2e' };
       mockReq.body = { requestor: { requestorId: 'differentUserId', role: 'Administrator' } };
       await getUserNotifications(mockReq, mockRes);
 
       expect(mockRes.status).toHaveBeenCalledWith(403);
       expect(mockRes.send).toHaveBeenCalledWith({ error: 'Unauthorized request' });
+    });
+    test('Ensures getUserNotifications returns 200 and notifications data when notifications are fetched successfully', async () => {
+      const { getUserNotifications } = makeSut();
+      const mockNotifications = [
+        { id: '123', message: 'Notification Test 1' },
+        { id: '123', message: 'Notification Test 2' },
+        { id: '123', message: 'Notification Test 3' },
+      ];
+      mockReq.body.requestor = {
+        requestorId: '65cf6c3706d8ac105827bb2e',
+        role: 'Administrator',
+      };
+
+      const mockService = jest.fn().mockResolvedValue(mockNotifications);
+
+      notificationService.getNotifications = mockService;
+
+      await getUserNotifications(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.send).toHaveBeenCalledWith(mockNotifications);
+      expect(mockService).toHaveBeenCalledWith(mockReq.params.userId);
     });
   });
 });
