a8399d97269a673a6916a9364d0c78e48d0205d6
Jordy <jordycorporan95@gmail.com>
2024-05-03 20:34:03 -0400

    test(notificationController): add delete and mark notification test suites
    Added the remaining tests for marking notification as read and deleting notification. Also, fix more bugs in the controller with the operator not evaluating the function correctly. Next steps are to refactor my tests and trim repeated code

--- src/controllers/notificationController.spec.js
+++ src/controllers/notificationController.spec.js
@@ -9,6 +9,8 @@ const makeSut = () => {
     getUnreadUserNotifications,
     getSentNotifications,
     createUserNotification,
+    deleteUserNotification,
+    markNotificationAsRead,
   } = notificationController(Notification);
 
   return {
@@ -16,6 +18,8 @@ const makeSut = () => {
     getUnreadUserNotifications,
     getSentNotifications,
     createUserNotification,
+    deleteUserNotification,
+    markNotificationAsRead,
   };
 };
 
@@ -211,7 +215,7 @@ describe('Notification controller Unit Tests', () => {
       const mockNotification = {
         message: 'Notification Test',
         recipient: '65cf6c3706d8ac105827bb2e',
-        sender: '5a7e21f00317bc1538def4b7'
+        sender: '5a7e21f00317bc1538def4b7',
       };
       mockReq.body = {
         requestor: {
@@ -221,7 +225,7 @@ describe('Notification controller Unit Tests', () => {
         recipient: '65cf6c3706d8ac105827bb2e',
       };
       mockReq.requestor = {
-        requestorId: '5a7e21f00317bc1538def4b7'
+        requestorId: '5a7e21f00317bc1538def4b7',
       };
 
       const mockService = jest.fn().mockResolvedValue(mockNotification);
@@ -231,7 +235,11 @@ describe('Notification controller Unit Tests', () => {
 
       expect(mockRes.status).toHaveBeenCalledWith(200);
       expect(mockRes.send).toHaveBeenCalledWith(mockNotification);
-      expect(mockService).toHaveBeenCalledWith(mockReq.requestor.requestorId, mockReq.body.recipient, mockReq.body.message);
+      expect(mockService).toHaveBeenCalledWith(
+        mockReq.requestor.requestorId,
+        mockReq.body.recipient,
+        mockReq.body.message,
+      );
     });
     test('Ensures createUserNotification returns error 500 if there is an internal error while creating a notification.', async () => {
       const { createUserNotification } = makeSut();
@@ -247,4 +255,107 @@ describe('Notification controller Unit Tests', () => {
       expect(mockRes.send).toHaveBeenCalledWith({ error: 'Internal Error' });
     });
   });
+  describe('deleteUserNotification', () => {
+    test('Ensures deleteUserNotification returns error 403 when requestor role is not Admin or Owner', async () => {
+      const { deleteUserNotification } = makeSut();
+      mockReq.body.requestor.role = 'randomRole'
+      await deleteUserNotification(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(403);
+      expect(mockRes.send).toHaveBeenCalledWith({ error: 'Unauthorized request' });
+    });
+    test('Ensures deleteUserNotification returns 200 and deletes notification', async () => {
+      const { deleteUserNotification } = makeSut();
+      const mockNotification = {
+        message: 'Notification Test',
+        recipient: '65cf6c3706d8ac105827bb2e',
+        sender: '5a7e21f00317bc1538def4b7',
+      };
+      mockReq.body = {
+        requestor: {
+          role: 'Administrator',
+        },
+        message: 'Notification Test',
+        recipient: '65cf6c3706d8ac105827bb2e',
+      };
+      mockReq.requestor = {
+        requestorId: '5a7e21f00317bc1538def4b7',
+      };
+
+      const mockService = jest.fn().mockResolvedValue(mockNotification);
+      notificationService.deleteNotification = mockService;
+
+      await deleteUserNotification(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.send).toHaveBeenCalledWith(mockNotification);
+      expect(mockService).toHaveBeenCalledWith(mockReq.params.notificationId);
+    });
+    test('Ensures deleteUserNotification returns error 500 if there is an internal error while deleting a notification.', async () => {
+      const { deleteUserNotification } = makeSut();
+      mockReq.body.requestor = {
+        requestorId: '65cf6c3706d8ac105827bb2e',
+        role: 'Administrator',
+      };
+      notificationService.deleteNotification = jest
+        .fn()
+        .mockRejectedValue({ error: 'Internal Error' });
+      await deleteUserNotification(mockReq, mockRes);
+      expect(mockRes.status).toHaveBeenCalledWith(500);
+      expect(mockRes.send).toHaveBeenCalledWith({ error: 'Internal Error' });
+    });
+  });
+  describe('markNotificationsAsRead', () => {
+    test('Ensures markNotificationAsRead returns 400 if recipientId is missing',() => {
+      const { markNotificationAsRead } = makeSut();
+      mockReq.body.requestor.requestorId = '';
+      markNotificationAsRead(mockReq, mockRes);
+      expect(mockRes.status).toHaveBeenCalledWith(400);
+      expect(mockRes.send).toHaveBeenCalledWith({ error: 'Recipient ID is required' });
+    })
+    test('Ensures markNotificationAsRead returns 200 and marks notification as read', async () => {
+      const { markNotificationAsRead } = makeSut();
+      const mockNotification = {
+        message: 'Notification Test',
+        recipient: '65cf6c3706d8ac105827bb2e',
+        sender: '5a7e21f00317bc1538def4b7',
+      };
+      mockReq.body = {
+        requestor: {
+          role: 'Administrator',
+        },
+        message: 'Notification Test',
+        recipient: '65cf6c3706d8ac105827bb2e',
+      };
+      mockReq.body.requestor = {
+        requestorId: '5a7e21f00317bc1538def4b7',
+      };
+      mockReq.params = {
+        notificationId: '12345'
+      };
+
+      const mockService = jest.fn().mockResolvedValue(mockNotification);
+      notificationService.markNotificationAsRead = mockService;
+
+      await markNotificationAsRead(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.send).toHaveBeenCalledWith(mockNotification);
+      expect(mockService).toHaveBeenCalledWith(mockReq.params.notificationId,mockReq.body.requestor.requestorId);
+    });
+    test('Ensures markNotificationAsRead returns 500 if there is an internal error while marking notification as read.', async () => {
+      const { markNotificationAsRead } = makeSut();
+      mockReq.body.requestor = {
+        requestorId: '65cf6c3706d8ac105827bb2e',
+        role: 'Administrator',
+      };
+      notificationService.markNotificationAsRead = jest
+        .fn()
+        .mockRejectedValue({ error: 'Internal Error' });
+      await markNotificationAsRead(mockReq, mockRes);
+      expect(mockRes.status).toHaveBeenCalledWith(500);
+      expect(mockRes.send).toHaveBeenCalledWith({ error: 'Internal Error' });
+    });
+
+  })
 });
