d12ad42813c22f5a3b5dda7e373ce931574602fb
Parth-tech <pjangid@usc.edu>
2024-05-25 19:10:17 -0700

    test: fix assertions.js and forgotPwdController.spec.js test file

--- src/test/assertions.js
+++ src/test/assertions.js
@@ -1,7 +1,7 @@
 const assertResMock = (statusCode, message, response, mockRes) => {
   console.log(mockRes);
-  // expect(mockRes.status).toHaveBeenCalledWith(statusCode);
-  // expect(mockRes.send).toHaveBeenCalledWith(message);
+  expect(mockRes.status).toHaveBeenCalledWith(statusCode);
+  expect(mockRes.send).toHaveBeenCalledWith(message);
   expect(response).toBeUndefined();
 };
 
