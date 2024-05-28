c27ebedd750458de0a07c894b6d0b1aea1c1a3ee
luisarevalo21 <arevaloluis89@yahoo.com>
2024-05-27 20:35:16 -0700

    test: completed test for inventory controller postInvInProjectWBS

--- src/controllers/inventoryController.spec.js
+++ src/controllers/inventoryController.spec.js
@@ -8,15 +8,20 @@ const inventoryController = require('./inventoryController');
 const inventoryItem = require('../models/inventoryItem');
 const inventoryItemType = require('../models/inventoryItemType');
 const mongoose = require('mongoose');
-
+const moment = require('moment');
 const makeSut = () => {
-  const { getAllInvInProjectWBS, postInvInProjectWBS } = inventoryController(inventoryItem, null);
+  const { getAllInvInProjectWBS, postInvInProjectWBS } = inventoryController(
+    inventoryItem,
+    inventoryItemType,
+    projects,
+  );
 
   return {
     getAllInvInProjectWBS,
     postInvInProjectWBS,
   };
 };
+const flushPromises = () => new Promise(setImmediate);
 
 jest.mock('../utilities/permissions', () => ({
   hasPermission: jest.fn(),
@@ -193,67 +198,229 @@ describe('inventoryController', () => {
         mockRes,
       );
     });
-    // test('Ensure postInvInProjectWBS Returns error 500 if saving an inventoryItem occurs', async () => {
-    // const { postInvInProjectWBS } = makeSut();
-    // const hasPermissionSpy = mockHasPermission(true);
-    // const findObject = {
-    //   populate: () => {},
-    //   inventory: [{ project: 'someName', wbs: null }],
-    // };
-    // mockReq.body.quantity = 1;
-    // mockReq.body.typeId = '6515fcc71dd1dbff0999e156';
-    // mockReq.body.wbsId = '6515fcc71dd1dbff0999e156';
-    // mockReq.body.projectId = '6515fcc71dd1dbff0999e156';
-    // mockReq.body.cost = 400;
-    // mockReq.body.poNum = '1234';
-    // const projectExists = {
-    //   select: () => {},
-    //   lean: () => {},
-    // };
-    // const wbsExists = {
-    //   select: () => {},
-    //   lean: () => {},
-    // };
-    // const inventoryExists = {
-    //   select: () => {},
-    //   lean: () => {},
-    //   save: () => {},
-    // };
-    // const findSpyProjects = jest.spyOn(projects, 'findOne').mockImplementationOnce(() => {
-    //   return projectExists;
-    // });
-    // jest.spyOn(projectExists, 'select').mockImplementationOnce(() => {
-    //   return projectExists;
-    // });
-    // jest.spyOn(projectExists, 'lean').mockImplementationOnce(() => {
-    //   return Promise.resolve(findObject.inventory);
-    // });
-    // // works move onto wbs findone
-    // const findSpyWbs = jest.spyOn(wbs, 'findOne').mockImplementationOnce(() => {
-    //   return wbsExists;
-    // });
-    // jest.spyOn(wbsExists, 'select').mockImplementationOnce(() => {
-    //   return wbsExists;
-    // });
-    // jest.spyOn(wbsExists, 'lean').mockImplementationOnce(() => {
-    //   return Promise.resolve(findObject.inventory);
-    // });
-    // const findSpy = jest.spyOn(inventoryItem, 'findOne').mockImplementationOnce(() => {
-    //   return inventoryExists;
-    // });
-    // jest.spyOn(inventoryExists, 'select').mockImplementationOnce(() => {
-    //   return inventoryExists;
-    // });
-    // jest.spyOn(inventoryExists, 'lean').mockImplementationOnce(() => {
-    //   return null;
-    // });
-    // jest.spyOn(inventoryExists, 'save').mockImplementationOnce(() => {
-    //   return Promise.reject(new Error('an error occured'));
-    // });
-    // const response = await postInvInProjectWBS(mockReq, mockRes);
-    // expect(hasPermissionSpy).toHaveBeenCalledWith(mockReq.body.requestor, 'postInvInProjectWBS');
-    // assertResMock(500, new Error('an error occured'), response, mockRes);
-    // });
+    test('Ensure postInvInProjectWBS Returns error 500 if saving an inventoryItem occurs', async () => {
+      const { postInvInProjectWBS } = makeSut();
+      const hasPermissionSpy = mockHasPermission(true);
+      const findObject = {
+        populate: () => {},
+        inventory: [{ project: 'someName', wbs: null }],
+      };
+      mockReq.body.quantity = 1;
+      mockReq.body.typeId = '6515fcc71dd1dbff0999e156';
+      mockReq.body.wbsId = '6515fcc71dd1dbff0999e156';
+      mockReq.body.projectId = '6515fcc71dd1dbff0999e156';
+      mockReq.body.cost = 400;
+      mockReq.body.poNum = '1234';
+      const projectExists = {
+        select: () => {},
+        lean: () => {},
+      };
+      const wbsExists = {
+        select: () => {},
+        lean: () => {},
+      };
+      const inventoryExists = {
+        select: () => {},
+        lean: () => {},
+        save: () => {},
+      };
+      const findSpyProjects = jest.spyOn(projects, 'findOne').mockImplementationOnce(() => {
+        return projectExists;
+      });
+      jest.spyOn(projectExists, 'select').mockImplementationOnce(() => {
+        return projectExists;
+      });
+      jest.spyOn(projectExists, 'lean').mockImplementationOnce(() => {
+        return Promise.resolve(findObject.inventory);
+      });
+      // works move onto wbs findone
+      const findSpyWbs = jest.spyOn(wbs, 'findOne').mockImplementationOnce(() => {
+        return wbsExists;
+      });
+      jest.spyOn(wbsExists, 'select').mockImplementationOnce(() => {
+        return wbsExists;
+      });
+      jest.spyOn(wbsExists, 'lean').mockImplementationOnce(() => {
+        return Promise.resolve(findObject.inventory);
+      });
+      const findSpy = jest.spyOn(inventoryItem, 'findOne').mockImplementationOnce(() => {
+        return inventoryExists;
+      });
+      jest.spyOn(inventoryExists, 'select').mockImplementationOnce(() => {
+        return inventoryExists;
+      });
+      jest.spyOn(inventoryExists, 'lean').mockImplementationOnce(() => {
+        return null;
+      });
+
+      jest.spyOn(inventoryItem.prototype, 'save').mockImplementationOnce(() => {
+        return Promise.reject(new Error('an error occured'));
+      });
+      const response = await postInvInProjectWBS(mockReq, mockRes);
+      await flushPromises();
+      expect(hasPermissionSpy).toHaveBeenCalledWith(mockReq.body.requestor, 'postInvInProjectWBS');
+      assertResMock(500, new Error('an error occured'), response, mockRes);
+    });
+
+    test("Ensure postInvInProjectWBS Receives a 201 if an inventory item doesn't exist and is sucessfully created", async () => {
+      const { postInvInProjectWBS } = makeSut();
+      const hasPermissionSpy = mockHasPermission(true);
+      const findObject = {
+        populate: () => {},
+        inventory: [{ project: 'someName', wbs: null }],
+      };
+      const resolvedInventoryItem = new inventoryItem({
+        project: mockReq.body.projectId,
+        wbs: mockReq.body.wbsId,
+        type: mockReq.body.typeId,
+        quantity: mockReq.body.quantity,
+        cost: mockReq.body.cost,
+        poNum: mockReq.body.poNum,
+      });
+      mockReq.body.quantity = 1;
+      mockReq.body.typeId = '6515fcc71dd1dbff0999e156';
+      mockReq.body.wbsId = '6515fcc71dd1dbff0999e156';
+      mockReq.body.projectId = '6515fcc71dd1dbff0999e156';
+      mockReq.body.cost = 400;
+      mockReq.body.poNum = '1234';
+
+      const projectExists = {
+        select: () => {},
+        lean: () => {},
+      };
+      const wbsExists = {
+        select: () => {},
+        lean: () => {},
+      };
+      const inventoryExists = {
+        select: () => {},
+        lean: () => {},
+        save: () => {},
+      };
+      const findSpyProjects = jest.spyOn(projects, 'findOne').mockImplementationOnce(() => {
+        return projectExists;
+      });
+      jest.spyOn(projectExists, 'select').mockImplementationOnce(() => {
+        return projectExists;
+      });
+      jest.spyOn(projectExists, 'lean').mockImplementationOnce(() => {
+        return Promise.resolve(findObject.inventory);
+      });
+      // works move onto wbs findone
+      const findSpyWbs = jest.spyOn(wbs, 'findOne').mockImplementationOnce(() => {
+        return wbsExists;
+      });
+      jest.spyOn(wbsExists, 'select').mockImplementationOnce(() => {
+        return wbsExists;
+      });
+      jest.spyOn(wbsExists, 'lean').mockImplementationOnce(() => {
+        return Promise.resolve(findObject.inventory);
+      });
+      const findSpy = jest.spyOn(inventoryItem, 'findOne').mockImplementationOnce(() => {
+        return inventoryExists;
+      });
+      jest.spyOn(inventoryExists, 'select').mockImplementationOnce(() => {
+        return inventoryExists;
+      });
+      jest.spyOn(inventoryExists, 'lean').mockImplementationOnce(() => {
+        return null;
+      });
+
+      jest.spyOn(inventoryItem.prototype, 'save').mockImplementationOnce(() => {
+        return Promise.resolve(resolvedInventoryItem);
+      });
+
+      const response = await postInvInProjectWBS(mockReq, mockRes);
+      await flushPromises();
+      expect(hasPermissionSpy).toHaveBeenCalledWith(mockReq.body.requestor, 'postInvInProjectWBS');
+      assertResMock(201, resolvedInventoryItem, response, mockRes);
+    });
+    test('Ensure postInvInProjectWBS Receives a 201 if an inventory item does exist and is updated with new values', async () => {
+      const { postInvInProjectWBS } = makeSut();
+      const hasPermissionSpy = mockHasPermission(true);
+      const findObject = {
+        populate: () => {},
+        inventory: [{ project: 'someName', wbs: null }],
+      };
+      const resolvedInventoryItem = new inventoryItem({
+        project: mockReq.body.projectId,
+        wbs: mockReq.body.wbsId,
+        type: mockReq.body.typeId,
+        quantity: mockReq.body.quantity,
+        cost: mockReq.body.cost,
+        poNum: mockReq.body.poNum,
+      });
+
+      const updatedResolvedInventoryItem = new inventoryItem({
+        project: mockReq.body.projectId,
+        wbs: mockReq.body.wbsId,
+        type: mockReq.body.typeId,
+        quantity: mockReq.body.quantity,
+        costPer: 200,
+      });
+      mockReq.body.quantity = 1;
+      mockReq.body.typeId = '6515fcc71dd1dbff0999e156';
+      mockReq.body.wbsId = 'Unassigned';
+      mockReq.body.projectId = '';
+      mockReq.body.cost = 400;
+      mockReq.body.poNum = '1234';
+
+      const projectExists = {
+        select: () => {},
+        lean: () => {},
+      };
+      const wbsExists = {
+        select: () => {},
+        lean: () => {},
+      };
+      const inventoryExists = {
+        select: () => {},
+        lean: () => {},
+        save: () => {},
+        inventory: [{ project: 'someName', wbs: null }],
+      };
+      const findSpyProjects = jest.spyOn(projects, 'findOne').mockImplementationOnce(() => {
+        return projectExists;
+      });
+      jest.spyOn(projectExists, 'select').mockImplementationOnce(() => {
+        return projectExists;
+      });
+      jest.spyOn(projectExists, 'lean').mockImplementationOnce(() => {
+        return Promise.resolve(findObject.inventory);
+      });
+      // works move onto wbs findone
+      const findSpyWbs = jest.spyOn(wbs, 'findOne').mockImplementationOnce(() => {
+        return wbsExists;
+      });
+      jest.spyOn(wbsExists, 'select').mockImplementationOnce(() => {
+        return wbsExists;
+      });
+      jest.spyOn(wbsExists, 'lean').mockImplementationOnce(() => {
+        return Promise.resolve(findObject.inventory);
+      });
+      const findSpy = jest.spyOn(inventoryItem, 'findOne').mockImplementationOnce(() => {
+        return inventoryExists;
+      });
+      jest.spyOn(inventoryExists, 'select').mockImplementationOnce(() => {
+        return inventoryExists;
+      });
+      jest.spyOn(inventoryExists, 'lean').mockImplementationOnce(() => {
+        return inventoryExists;
+      });
+
+      jest.spyOn(inventoryItem, 'findOneAndUpdate').mockImplementationOnce(() => {
+        return Promise.resolve(resolvedInventoryItem);
+      });
+
+      jest.spyOn(inventoryItem, 'findByIdAndUpdate').mockImplementationOnce(() => {
+        return Promise.resolve(updatedResolvedInventoryItem);
+      });
+
+      const response = await postInvInProjectWBS(mockReq, mockRes);
+      await flushPromises();
+      expect(hasPermissionSpy).toHaveBeenCalledWith(mockReq.body.requestor, 'postInvInProjectWBS');
+      assertResMock(201, updatedResolvedInventoryItem, response, mockRes);
+    });
   });
 });
 
