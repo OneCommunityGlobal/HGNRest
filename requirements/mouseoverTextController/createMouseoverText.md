5aa435e057cb118dee17d04a2def5cbb8cda1335
20chen-7 <xiaoyuchen007@gmail.com>
2024-05-31 12:43:36 -0600

    test: create-update-get

--- requirements/mouseoverTextController/createMouseoverText.md
+++ requirements/mouseoverTextController/createMouseoverText.md
@@ -4,8 +4,8 @@ Cross Mark: ❌
 # createMouseoverText
 
 > ## Positive case
-1. ❌ Return 201 if create new mouseoverText successfully.
+1. ✅ Return 201 if create new mouseoverText successfully.
 
 > ## Negative case
-1. ❌ Returns error 500 if any error when saving the new mouseoverText
+1. ✅ Returns error 500 if any error when saving the new mouseoverText
 > ## Edge case
--- src/controllers/mouseoverTextController.js
+++ src/controllers/mouseoverTextController.js
@@ -1,43 +1,48 @@
-const mouseoverTextController = (function (MouseoverText) {
-    const createMouseoverText = function (req, res) {
-        const newMouseoverText = new MouseoverText();
-        newMouseoverText.mouseoverText = req.body.newMouseoverText;
-        newMouseoverText.save().then(() => res.status(201).json({
-            _serverMessage: 'MouseoverText succesfuly created!',
-            mouseoverText: newMouseoverText,
-        })).catch(err => res.status(500).send({ err }));
-    };
+const mouseoverTextController = function (MouseoverText) {
+  const createMouseoverText = function (req, res) {
+    const newMouseoverText = new MouseoverText();
+    newMouseoverText.mouseoverText = req.body.newMouseoverText;
+    newMouseoverText
+      .save()
+      .then((result) =>
+        res
+          .status(201)
+          .send({ _serverMessage: 'MouseoverText successfully created!', mouseoverText: result }),
+      )
+      .catch((error) => res.status(500).send(error));
+  };
 
-    const getMouseoverText = function (req, res) {
-        MouseoverText.find()
-            .then(results => res.status(200).send(results))
-            .catch(error => res.status(404).send(error));
-    };
+  const getMouseoverText = function (req, res) {
+    MouseoverText.find()
+      .then((results) => res.status(200).send(results))
+      .catch((error) => res.status(404).send(error));
+  };
 
-    const updateMouseoverText = function (req, res) {
-        // if (req.body.requestor.role !== 'Owner') {
-        //     res.status(403).send('You are not authorized to update mouseoverText!');
-        // }
-        const { id } = req.params;
+  const updateMouseoverText = function (req, res) {
+    // if (req.body.requestor.role !== 'Owner') {
+    //     res.status(403).send('You are not authorized to update mouseoverText!');
+    // }
+    const { id } = req.params;
 
-        return MouseoverText.findById(id, (error, mouseoverText) => {
-            if (error || mouseoverText === null) {
-                res.status(500).send('MouseoverText not found with the given ID');
-                return;
-            }
+    return MouseoverText.findById(id, (error, mouseoverText) => {
+      if (error || mouseoverText === null) {
+        res.status(500).send(new Error('MouseoverText not found with the given ID'));
+        return;
+      }
 
-            mouseoverText.mouseoverText = req.body.newMouseoverText;
-            mouseoverText.save()
-                .then(results => res.status(201).send(results))
-                .catch(errors => res.status(400).send(errors));
-        });
-    };
+      mouseoverText.mouseoverText = req.body.newMouseoverText;
+      mouseoverText
+        .save()
+        .then((results) => res.status(201).send(results))
+        .catch((errors) => res.status(400).send(errors));
+    });
+  };
 
-    return {
-        createMouseoverText,
-        getMouseoverText,
-        updateMouseoverText,
-    };
-});
+  return {
+    createMouseoverText,
+    getMouseoverText,
+    updateMouseoverText,
+  };
+};
 
 module.exports = mouseoverTextController;
