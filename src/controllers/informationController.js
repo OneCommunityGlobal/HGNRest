87fa2a452a0aa59e149dc24539676a12f7931a88
20chen-7 <xiaoyuchen007@gmail.com>
2024-06-03 13:55:47 -0600

    test: not change original controller

--- src/controllers/informationController.js
+++ src/controllers/informationController.js
@@ -8,9 +8,8 @@ const informationController = function (Information) {
   const cache = cacheClosure();
   const getInformations = function (req, res) {
     // return all informations if cache is available
-    if (cache.getCache('informations')) {
-      const results = cache.getCache('informations');
-      res.status(200).send(results);
+    if (cache.hasCache('informations')) {
+      res.status(200).send(cache.getCache('informations'));
       return;
     }
     Information.find({}, 'infoName infoContent visibility')
@@ -19,41 +18,37 @@ const informationController = function (Information) {
         cache.setCache('informations', results);
         res.status(200).send(results);
       })
-      /* eslint-disable no-unused-vars */
-      .catch((error) => res.status(500).send(new Error('Error when finding infoName')));
+      .catch((error) => res.status(500).send(error));
   };
 
   const addInformation = function (req, res) {
-    try {
-      const result = Information.findOne({
-        infoName: { $regex: escapeRegex(req.body.infoName), $options: 'i' },
-      });
-      if (result) {
-        res
-          .status(403)
-          .send(
-            new Error(
-              `Info Name must be unique. Another infoName with name ${result[0].infoName} already exists. Please note that info names are case insensitive`,
-            ),
-          );
-        return;
-      }
-      const _info = new Information();
-      _info.infoName = req.body.infoName;
-      _info.infoContent = req.body.infoContent || 'Unspecified';
-      _info.visibility = req.body.visibility || '0';
-      try {
-        const newInfo = _info.save();
-        if (cache.getCache('informations')) {
-          cache.removeCache('informations');
+    Information.find({ infoName: { $regex: escapeRegex(req.body.infoName), $options: 'i' } })
+      .then((result) => {
+        if (result.length > 0) {
+          res
+            .status(400)
+            .send({
+              error: `Info Name must be unique. Another infoName with name ${result[0].infoName} already exists. Please note that info names are case insensitive`,
+            });
+          return;
         }
-        res.status(201).send(newInfo);
-      } catch (error) {
-        res.status(500).send(new Error('Error when saving information'));
-      }
-    } catch (error) {
-      res.status(500).send(new Error('Error when finding single info'));
-    }
+        const _info = new Information();
+        _info.infoName = req.body.infoName;
+        _info.infoContent = req.body.infoContent || 'Unspecified';
+        _info.visibility = req.body.visibility || '0';
+        _info
+          .save()
+          .then((newInformation) => {
+            // remove cache if cache is available
+            if (cache.hasCache('informations')) {
+              cache.removeCache('informations');
+              return;
+            }
+            res.status(201).send(newInformation);
+          })
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(500).send({ error }));
   };
 
   const deleteInformation = function (req, res) {
@@ -66,9 +61,7 @@ const informationController = function (Information) {
         }
         res.status(200).send(deletedInformation);
       })
-      .catch((error) =>
-        res.status(400).send(new Error('Error when finding and deleting information by Id')),
-      );
+      .catch((error) => res.status(400).send(error));
   };
 
   // Update existing information by id
@@ -82,9 +75,7 @@ const informationController = function (Information) {
         }
         res.status(200).send(updatedInformation);
       })
-      .catch((error) =>
-        res.status(400).send(new Error('Error when finding and updating information by Id')),
-      );
+      .catch((error) => res.status(400).send(error));
   };
 
   return {
