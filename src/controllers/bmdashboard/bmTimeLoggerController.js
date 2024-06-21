fd9ed9d75cece6aa682655a5a2ea6a56ad127645
alla0810 <kyosook.shin@gmail.com>
2024-06-20 21:37:33 -0500

    adding task information to member information

--- src/controllers/bmdashboard/bmTimeLoggerController.js
+++ src/controllers/bmdashboard/bmTimeLoggerController.js
@@ -1,5 +1,7 @@
 // const userprofile = require('../../models/userProfile');
-const BuildingProject = require('../../models/bmdashboard/buildingProject')
+const mongoose = require('mongoose');
+const BuildingProject = require('../../models/bmdashboard/buildingProject');
+const Task = require('../../models/task');
 
 const bmTimeLoggerController = function () {
 
@@ -8,18 +10,45 @@ const bmTimeLoggerController = function () {
     try {
       BuildingProject
         .findById(projectId)
-        .populate([
+        .populate({path: 'buildingManager', select: '_id firstName lastName email'})
+        // .populate({
+        //   path: 'teams', 
+        //   select: '_id teamName'
+        // })        
+        .populate(
           {
-            path: 'buildingManager',
-            select: '_id firstName lastName email',
+            path: 'members', 
+            populate: [
+              {
+                path: 'user',
+                select: '_id firstName lastName email teams',      
+              },
+            ]
           },
-          {
-          path: 'members', 
-          populate: [{
-            path: 'user',
-            select: '_id firstName lastName email',
-          }]}])
+        )
         .exec()
+        .then((project) => {
+          console.log("project", project);
+
+          project.members.forEach((member)=> {
+            const userId = member.user._id;
+
+            console.log("userId", userId);
+
+            try {
+              Task.find(
+                {
+                  'resources.userID': mongoose.Types.ObjectId(userId),
+                }
+              ).then ((results) => {
+                console.log("results", results);
+              })
+            } catch(error)
+            {console.log("error");}
+
+          })
+          return project;
+        })
         .then(project => res.status(200).send(project))
         .catch(error => res.status(500).send(error));
     } catch (err) {
