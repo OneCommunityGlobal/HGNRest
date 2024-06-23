1c1dd82f05e059009409a9cc20418fe0aeda2021
Shengwei Peng <ptrpengdev@gmail.com>
2024-06-23 11:58:39 -0400

    Update Auditing feature - Update helper function to return only updated key from both objects

--- src/utilities/objectUtils.js
+++ src/utilities/objectUtils.js
@@ -6,10 +6,28 @@ function deepCopyMongooseObjectWithLodash(originalDoc) {
   return deepCopy;
 }
 
+
+
 function filterFieldsFromObj(obj, keysToFilter) {
   const filteredObj = {};
-  console.log('keysToFilter', keysToFilter);
+  // keys to exclude: sensitive data and verbose data
+  const keysToExclude = [
+    '_id',
+    '__v',
+    'password',
+    'location',
+    'privacySettings',
+    'infringements',
+    'badgeCollection',
+    'copiedAiPrompt',
+    'hoursByCategory',
+    'savedTangibleHrs',
+  ];
+  // a list of keys to filter from the object
   Object.keys(obj).forEach((key) => {
+    if (keysToExclude.includes(key)) {
+      return;
+    }
     if (keysToFilter.includes(key)) {
       filteredObj[key] = obj[key];
     }
@@ -18,4 +36,25 @@ function filterFieldsFromObj(obj, keysToFilter) {
   return filteredObj;
 }
 
-module.exports = { deepCopyMongooseObjectWithLodash, filterFieldsFromObj };
+/**
+ * Return two objects that have different values for the same key.
+ * @param {Object} originalDoc Must be a object
+ * @param {Object} updatedDoc
+ * @param {Array} keysToFilter
+ * @returns
+ */
+function returnObjectDifference(originalDoc, updatedDoc, keysToFilter) {
+  const originalDocFiltered = filterFieldsFromObj(originalDoc, keysToFilter);
+  const updatedDocFiltered = filterFieldsFromObj(updatedDoc, keysToFilter);
+  // filter out the keys that have the same value in both objects
+  const updatedObj = _.omitBy(updatedDocFiltered, (value, key) =>
+    _.isEqual(value, originalDocFiltered[key]),
+  );
+  const originalObj = _.omitBy(originalDocFiltered, (value, key) =>
+    _.isEqual(value, updatedDocFiltered[key]),
+  );
+  // return an object contains the difference between the original and updated document
+  return { originalObj, updatedObj };
+}
+
+module.exports = { deepCopyMongooseObjectWithLodash, filterFieldsFromObj, returnObjectDifference };
