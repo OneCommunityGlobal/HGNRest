0c4c3b35dab85bcbfdb8629b2d724aea190a80ff
Shereen Punnassery <pshereen@gmail.com>
2024-06-29 17:19:06 -0700

    Created Routing and controller function for fetching equipment types
    Created Routing and controller function for fetching equipment types

--- src/routes/bmdashboard/bmInventoryTypeRouter.js
+++ src/routes/bmdashboard/bmInventoryTypeRouter.js
@@ -24,6 +24,8 @@ const routes = function (baseInvType, matType, consType, reusType, toolType, equ
 
   inventoryTypeRouter.route('/invtypes/equipment').post(controller.addEquipmentType);
 
+  inventoryTypeRouter.route('/invtypes/equipments').get(controller.fetchEquipmentTypes);
+
   inventoryTypeRouter.route('/invtypes/consumables').get(controller.fetchConsumableTypes);
 
   // Route for fetching types by selected type
