4d351aba1c01c1a74df748503e9448a7c3547905
alla0810 <kyosook.shin@gmail.com>
2024-07-22 12:29:04 -0500

    update for pull request

--- src/controllers/bmdashboard/bmTimeLoggerController.js
+++ src/controllers/bmdashboard/bmTimeLoggerController.js
@@ -6,6 +6,9 @@ const Task = require('../../models/task');
 const bmTimeLoggerController = function () {
 
   const fetchProjectMembers = async (req, res) => {
+    // console.log("fetchProjectMembers");
+
+
     const { projectId } = req.params;
     try {
       BuildingProject
@@ -33,15 +36,24 @@ const bmTimeLoggerController = function () {
           project.members.forEach((member)=> {
             const userId = member.user._id;
 
-            console.log("userId", userId);
+            // console.log("userId", userId);
 
             try {
               Task.find(
                 {
                   'resources.userID': mongoose.Types.ObjectId(userId),
                 }
-              ).then ((results) => {
-                console.log("results", results);
+              ).then ((results) => {                
+                // console.log("results", results);       
+                if (results[0].taskName)
+                {
+                  const memberTaskName = results[0].taskName;  
+                  // console.log("memberTaskName: ", memberTaskName);
+                  // Not working because of db access latency, maybe need to implement separate signaling to fetch task name
+                  // also needs to add 'task' field to insert memberTaskName rather than using teams field
+                  member.user.teams[0] = memberTaskName;   
+                  // console.log("member: ", member);
+                }
               })
             } catch(error)
             {console.log("error");}
