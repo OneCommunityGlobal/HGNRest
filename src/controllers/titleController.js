21b2887cf697d6a4d160d66179d7ca86269489d5
Jatin Agrawal <jatinagrawal0801@gmail.com>
2024-08-17 18:31:12 -0700

    new route added to update existing quick action functions

--- src/controllers/titleController.js
+++ src/controllers/titleController.js
@@ -1,7 +1,11 @@
 const Team = require('../models/team');
 const Project = require('../models/project');
 const cacheClosure = require('../utilities/nodeCache');
-const { getAllTeamCodeHelper } = require("./userProfileController");
+const userProfileController = require("./userProfileController");
+const userProfile = require('../models/userProfile');
+const project = require('../models/project');
+const controller = userProfileController(userProfile, project);
+const getAllTeamCodeHelper = controller.getAllTeamCodeHelper;
 
 const titlecontroller = function (Title) {
   const cache = cacheClosure();
@@ -22,7 +26,6 @@ const titlecontroller = function (Title) {
 
     const postTitle = async function (req, res) {
       const title = new Title();
-
       title.titleName = req.body.titleName;
       title.teamCode = req.body.teamCode;
       title.projectAssigned = req.body.projectAssigned;
@@ -81,6 +84,81 @@ const titlecontroller = function (Title) {
         .catch((error) => res.status(404).send(error));
     };
 
+    const putTitle = async function (req, res) {
+      const title = new Title();
+      const filter=req.body.id;
+      title.titleName = req.body.titleName;
+      title.teamCode = req.body.teamCode;
+      title.projectAssigned = req.body.projectAssigned;
+      title.mediaFolder = req.body.mediaFolder;
+      title.teamAssiged = req.body.teamAssiged;
+
+      // valid title name
+      if (!title.titleName.trim()) {
+        res.status(400).send({ message: 'Title cannot be empty.' });
+        return;
+      }
+
+      //  if media is empty
+      if (!title.mediaFolder.trim()) {
+        res.status(400).send({ message: 'Media folder cannot be empty.' });
+        return;
+      }
+
+      const shortnames = title.titleName.trim().split(' ');
+      let shortname;
+      if (shortnames.length > 1) {
+          shortname = (shortnames[0][0] + shortnames[1][0]).toUpperCase();
+      } else if (shortnames.length === 1) {
+          shortname = shortnames[0][0].toUpperCase();
+      }
+      title.shortName = shortname;
+
+      // Validate team code by checking if it exists in the database
+      if (!title.teamCode) {
+        res.status(400).send({ message: 'Please provide a team code.' });
+        return;
+      }
+
+      const teamCodeExists = await checkTeamCodeExists(title.teamCode);
+      if (!teamCodeExists) {
+        res.status(400).send({ message: 'Invalid team code. Please provide a valid team code.' });
+        return;
+      }
+
+      // validate if project exist
+      const projectExist = await checkProjectExists(title.projectAssigned._id);
+      if (!projectExist) {
+        res.status(400).send({ message: 'Project is empty or not exist.' });
+        return;
+      }
+
+      // validate if team exist
+      if (title.teamAssiged && title.teamAssiged._id === 'N/A') {
+        res.status(400).send({ message: 'Team not exists.' });
+        return;
+      }
+    
+      title
+      .updateOne({ _id: filter }, title)
+      .then((result) => {
+          if (result.modifiedCount === 0) {
+              // No documents were modified, handle this case if necessary
+              console.log("No docs")
+              return res.status(404).send({ message: 'No documents were updated' });
+          }
+          // Send a success response
+          console.log("Success")
+          res.status(200).send({ message: 'Update successful', result });
+      })
+      .catch((error) => {
+          // Send a server error response
+          console.log(error)
+          res.status(500).send({ message: 'An error occurred', error });
+    });
+
+    };
+
     const deleteTitleById = async function (req, res) {
       const { titleId } = req.params;
       Title.deleteOne({ _id: titleId })
@@ -126,12 +204,15 @@ const titlecontroller = function (Title) {
       }
     }
 
+   
+
   return {
     getAllTitles,
     getTitleById,
     postTitle,
     deleteTitleById,
     deleteAllTitles,
+    putTitle
   };
 };
 
--- src/routes/titleRouter.js
+++ src/routes/titleRouter.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const router = function (title) {
   const controller = require('../controllers/titleController')(title);
-
   const titleRouter = express.Router();
 
   titleRouter.route('/title')
     .get(controller.getAllTitles)
-    .post(controller.postTitle);
+    .post(controller.postTitle)
+    // .put(controller.putTitle);
+
+  titleRouter.route('/title/update').put(controller.putTitle);
 
   titleRouter.route('/title/:titleId')
     .get(controller.getTitleById)
