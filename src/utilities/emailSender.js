8a5ea9ca44c8d6d849f66bd55a068fcfa15d2b6d
angelalala <angela9004@gmail.com>
2024-08-21 15:44:18 -0700

    feat: address emails in batch

--- src/utilities/emailSender.js
+++ src/utilities/emailSender.js
@@ -2,78 +2,114 @@ const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 const logger = require('../startup/logger');
 
+const config = {
+  email: process.env.REACT_APP_EMAIL,
+  clientId: process.env.REACT_APP_EMAIL_CLIENT_ID,
+  clientSecret: process.env.REACT_APP_EMAIL_CLIENT_SECRET,
+  redirectUri: process.env.REACT_APP_EMAIL_CLIENT_REDIRECT_URI,
+  refreshToken: process.env.REACT_APP_EMAIL_REFRESH_TOKEN,
+  batchSize: 100,
+  batchDelay: 60000,
+  mailQueueInterval: process.env.MAIL_QUEUE_INTERVAL || 60000,
+};
+
 const closure = () => {
   const queue = [];
 
-  const CLIENT_EMAIL = process.env.REACT_APP_EMAIL;
-  const CLIENT_ID = process.env.REACT_APP_EMAIL_CLIENT_ID;
-  const CLIENT_SECRET = process.env.REACT_APP_EMAIL_CLIENT_SECRET;
-  const REDIRECT_URI = process.env.REACT_APP_EMAIL_CLIENT_REDIRECT_URI;
-  const REFRESH_TOKEN = process.env.REACT_APP_EMAIL_REFRESH_TOKEN;
   // Create the email envelope (transport)
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       type: 'OAuth2',
-      user: CLIENT_EMAIL,
-      clientId: CLIENT_ID,
-      clientSecret: CLIENT_SECRET,
+      user: config.email,
+      clientId: config.clientId,
+      clientSecret: config.clientSecret,
     },
   });
 
-  const OAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
-
-  OAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+  const OAuth2Client = new google.auth.OAuth2(
+    config.clientId,
+    config.clientSecret,
+    config.redirectUri,
+  );
+  OAuth2Client.setCredentials({ refresh_token: config.refreshToken });
 
   const processQueue = async () => {
-    const nextItem = queue.shift();
-    if (!nextItem) {
-      setTimeout(processQueue, process.env.MAIL_QUEUE_INTERVAL || 1000);
-      return;
-    }
+    const delay = (ms) =>
+      new Promise((resolve) => {
+        setTimeout(resolve, ms);
+      });
 
-    const { recipient, subject, message, attachments, cc, bcc, replyTo, acknowledgingReceipt } =
-      nextItem;
+    const sendEmail = async (item) => {
+      const { recipient, subject, message, attachments, cc, bcc, replyTo, acknowledgingReceipt } =
+        item;
 
-    try {
-      const res = await OAuth2Client.getAccessToken();
-      const ACCESSTOKEN = res.token;
+      try {
+        const res = await OAuth2Client.getAccessToken();
+        const accessToken = res.token;
 
-      const mailOptions = {
-        from: CLIENT_EMAIL,
-        to: recipient,
-        cc,
-        bcc,
-        subject,
-        html: message,
-        attachments,
-        replyTo,
-        auth: {
-          user: CLIENT_EMAIL,
-          refreshToken: REFRESH_TOKEN,
-          accessToken: ACCESSTOKEN,
-        },
-      };
+        const mailOptions = {
+          from: config.email,
+          to: recipient,
+          cc,
+          bcc,
+          subject,
+          html: message,
+          attachments,
+          replyTo,
+          auth: {
+            user: config.email,
+            refreshToken: config.refreshToken,
+            accessToken,
+          },
+        };
 
-      const result = await transporter.sendMail(mailOptions);
-      if (typeof acknowledgingReceipt === 'function') {
-        acknowledgingReceipt(null, result);
+        const result = await transporter.sendMail(mailOptions);
+        if (typeof acknowledgingReceipt === 'function') {
+          acknowledgingReceipt(null, result);
+        }
+        if (process.env.NODE_ENV === 'local') {
+          logger.logInfo(`Email sent: ${JSON.stringify(result)}`);
+        }
+      } catch (error) {
+        if (typeof acknowledgingReceipt === 'function') {
+          acknowledgingReceipt(error, null);
+        }
+        logger.logException(
+          error,
+          `Error sending email: from ${config.email} to ${recipient} subject ${subject}`,
+          `Extra Data: cc ${cc} bcc ${bcc}`,
+        );
       }
-      if (process.env.NODE_ENV === 'local') {
-        logger.logInfo(`Email sent: ${JSON.stringify(result)}`);
+    };
+
+    const processBatch = async (batch) => {
+      await batch.reduce(async (previousPromise, item) => {
+        console.log('item', item);
+        await previousPromise;
+        await sendEmail(item);
+        await delay(1000);
+      }, Promise.resolve());
+    };
+
+    const processAllBatches = async () => {
+      if (queue.length === 0) {
+        setTimeout(processQueue, config.mailQueueInterval);
+        return;
       }
-    } catch (error) {
-      if (typeof acknowledgingReceipt === 'function') {
-        acknowledgingReceipt(error, null);
+
+      const batch = queue.splice(0, config.batchSize);
+      console.log('processBatch', batch);
+      await processBatch(batch);
+
+      if (queue.length > 0) {
+        setTimeout(processAllBatches, config.batchDelay);
+      } else {
+        setTimeout(processQueue, config.mailQueueInterval);
       }
-      logger.logException(
-        error,
-        `Error sending email: from ${CLIENT_EMAIL} to ${recipient} subject ${subject}`,
-        `Extra Data: cc ${cc} bcc ${bcc}`,
-      );
-    } finally {
-      setTimeout(processQueue, process.env.MAIL_QUEUE_INTERVAL || 1000);
-    }
+    };
+    console.log('processAllBatches in queue');
+    processAllBatches();
   };
 
   processQueue();
