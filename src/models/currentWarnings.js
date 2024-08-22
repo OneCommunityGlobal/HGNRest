ab1e953d43890c0c4d5baca24dd3e13d44bfb02b
luisarevalo21 <arevaloluis89@yahoo.com>
2024-08-21 20:04:59 -0700

    fix: removed logs and added isPermanent Attribute to new warnings

--- src/models/currentWarnings.js
+++ src/models/currentWarnings.js
@@ -5,6 +5,7 @@ const { Schema } = mongoose;
 const currentWarnings = new Schema({
   warningTitle: { type: String, required: true },
   activeWarning: { type: Boolean, required: true },
+  isPermanent: { type: Boolean, required: true },
 });
 
 module.exports = mongoose.model('currentWarning', currentWarnings, 'currentWarnings');
