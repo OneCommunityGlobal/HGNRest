d4a2b4a447ffb96ec4138b40d62861addc202843
Jatin Agrawal <jatinagrawal0801@gmail.com>
2024-09-14 16:19:38 -0700

    function added to send email at 4AM for missed summary

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 /* eslint-disable quotes */
 require('dotenv').load();
-
 const { app, logger } = require('./app');
 const websockets = require('./websockets').default;
-
 require('./startup/db')();
 require('./cronjobs/userProfileJobs')();
+
+// const userHelper=require('./helpers/userHelper')();
+// userHelper.completeHoursAndMissedSummary()
 
 const port = process.env.PORT || 4500;
 
