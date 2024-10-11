a9cffe949c74e6f2d583992a81098db62d7813e8
luisarevalo21 <arevaloluis89@yahoo.com>
2024-10-10 19:55:30 -0700

    added jae's email

--- src/controllers/warningsController.js
+++ src/controllers/warningsController.js
@@ -140,7 +140,7 @@ const warningsController = function (UserProfile) {
 
 //helper to get the team members admin emails
 async function getUserRoleByEmail(user) {
-  const recipients = [];
+  const recipients = ['onecommunityglobal@gmail.com'];
   for (const teamId of user.teams) {
     const managementEmails = await userHelper.getTeamManagementEmail(teamId);
     if (Array.isArray(managementEmails) && managementEmails.length > 0) {
