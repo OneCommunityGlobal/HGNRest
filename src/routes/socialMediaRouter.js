e220af8a97b69af02076d58aea86ccf921c9cd04
angelalala <angela9004@gmail.com>
2024-11-17 22:36:21 -0800

    decouple poster

--- src/routes/socialMediaRouter.js
+++ src/routes/socialMediaRouter.js
@@ -1,17 +1,10 @@
 const express = require('express');
-const {
-  getPinterestAccessToken,
-  getTwitterAccessToken,
-  createPin,
-  createTweet,
-} = require('../controllers/socialMediaController');
+const { getTwitterAccessToken, createTweet } = require('../controllers/socialMediaController');
 
 const routes = function () {
   const socialMediaRouter = express.Router();
 
-  socialMediaRouter.route('/getPinterestAccessToken').post(getPinterestAccessToken);
   socialMediaRouter.route('/getTwitterAccessToken').post(getTwitterAccessToken);
-  socialMediaRouter.route('/createPin').post(createPin);
   socialMediaRouter.route('/createTweet').post(createTweet);
 
   return socialMediaRouter;
