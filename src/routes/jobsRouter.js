28ad93b98d5ea6608ef3e7633992bfa6b28f2180
Anoushka Gupta <anoushka.post@gmail.com>
2024-11-28 20:21:33 -0800

    add /summaries API

--- src/routes/jobsRouter.js
+++ src/routes/jobsRouter.js
@@ -5,9 +5,9 @@ const router = express.Router();
 
 // Define routes
 router.get('/', jobsController.getJobs);
+router.get('/summaries', jobsController.getJobSummaries); // GET request to fetch job summaries
 router.get('/:id', jobsController.getJobById);
 router.post('/', jobsController.createJob);
 router.put('/:id', jobsController.updateJob);
 router.delete('/:id', jobsController.deleteJob);
-
 module.exports = router;
