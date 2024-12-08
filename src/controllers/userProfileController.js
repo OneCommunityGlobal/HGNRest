de039d5bc8ead8c4d076ee875f06931e29e82bb8
suaniii <yili.sun3688@gmail.com>
2024-12-07 16:54:42 -0800

    fix

--- src/controllers/userProfileController.js
+++ src/controllers/userProfileController.js
@@ -488,7 +488,7 @@ const userProfileController = function (UserProfile, Project) {
     const isRequestorAuthorized = !!(
       canEditProtectedAccount &&
       ((await hasPermission(req.body.requestor, 'putUserProfile')) ||
-        req.body.requestor.requestorId === userid)
+        req.body.requestor.requestorId === userid || await hasPermission(req.body.requestor, 'editTeamCode'))
     );
 
     const canManageAdminLinks = await hasPermission(req.body.requestor, 'manageAdminLinks');
