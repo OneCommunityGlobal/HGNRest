91dca93998b293fa715d69be12301a21985a345c
yashwanth170 <pokala.y@northeastern.edu>
2024-12-18 10:46:45 -0500

    fix: Dublicate entries are not logged

--- src/utilities/logUserPermissionChangeByAccount.js
+++ src/utilities/logUserPermissionChangeByAccount.js
@@ -2,37 +2,26 @@ const moment = require('moment-timezone');
 const UserPermissionChangeLog = require('../models/userPermissionChangeLog');
 const UserProfile = require('../models/userProfile');
 
-// Middleware function to log permission changes
-const logUserPermissionChangeByAccount = async (req, res, next) => {
-  const {
-    permissions, firstName, lastName, requestor
-  } = req.body;
-
+const logUserPermissionChangeByAccount = async (req) => {
+  const { permissions, firstName, lastName, requestor } = req.body;
   const dateTime = moment().tz('America/Los_Angeles').format();
 
   try {
     let permissionsAdded = [];
     let permissionsRemoved = [];
     const { userId } = req.params;
-
-    // Ensure permissions is always an array
     const Permissions = permissions.frontPermissions;
-    const requestorEmailId = await UserProfile.findById(req.body.requestor.requestorId)
-        .select('email')
-        .exec();
-    // Find the latest log related to the user's permissions
+    const requestorEmailId = await UserProfile.findById(requestor.requestorId).select('email').exec();
     const document = await findLatestRelatedLog(userId);
 
     if (document) {
-      // Ensure document.permissions is an array before filtering
       const docPermissions = Array.isArray(document.permissions) ? document.permissions : [];
-
-      // Calculate removed permissions
+      if(JSON.stringify(docPermissions) === JSON.stringify(Permissions)) {
+        return;
+      }
       permissionsRemoved = docPermissions.filter((item) => !Permissions.includes(item));
-      // Calculate added permissions
       permissionsAdded = Permissions.filter((item) => !docPermissions.includes(item));
     } else {
-      // If no previous log exists, assume all permissions are new (first time log)
       permissionsAdded = Permissions;
     }
 
@@ -47,24 +36,18 @@ const logUserPermissionChangeByAccount = async (req, res, next) => {
       requestorEmail: requestorEmailId.email,
     });
 
-    // Save the new log entry
     await logEntry.save();
-
-    // Proceed to the next middleware or route
-    next();
+    console.log('Permission change logged successfully');
   } catch (error) {
     console.error('Error logging permission change:', error);
-    res.status(500).json({ error: 'Failed to log permission change' });
   }
 };
 
-// Helper function to find the latest permission log for a user
 const findLatestRelatedLog = (userId) => new Promise((resolve, reject) => {
   UserPermissionChangeLog.findOne({ userId })
     .sort({ logDateTime: -1 })
     .exec((err, document) => {
       if (err) {
-        console.error(err);
         reject(err);
         return;
       }
