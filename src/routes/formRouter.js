48a6824e6f464f29433d9fff60054fa3685b6753
Jatin Agrawal <jatinagrawal0801@gmail.com>
2025-01-30 01:12:13 -0800

    significant changes pushed

--- src/routes/formRouter.js
+++ src/routes/formRouter.js
@@ -3,30 +3,22 @@ const express=require('express')
 const routes=function(Form,formResponse){
     const controller=require('../controllers/formController')(Form,formResponse);
     const formRouter=express.Router();
-    // route to create a new formc
-    formRouter.route('/form/createform').post(controller.createForm);
     
-    // route to get list of all forms
-    formRouter.route('/form/allforms').get(controller.getAllForms);
+    
+    formRouter.route('/form')
+    .post(controller.createForm) // route to create a new form
+    .get(controller.getAllForms) // route to get list of all forms
+    .get(controller.getFormFormat) // route to get format for a format
+    .put(controller.editFormFormat) // route to edit form format
+    .delete(controller.deleteFormFormat); // route to delete form format
     
     // route to add data to a form
-    formRouter.route('/form/submit').post(controller.addDataToForm);
-    
-    // route to edit form format
-    formRouter.route('/form/edit').post(controller.editFormFormat);
-    
-    // route to delete form format
-    formRouter.route('/form/delete').delete(controller.deleteFormFormat);
+    formRouter.route('/form/response')
+    .post(controller.addDataToForm) // route to add data to a form
+    .get(controller.getFormData); // route to get form data for a specific form
     
     // route to check if user has responded to a specific form or not
     formRouter.route('/form/status').get(controller.checkForResponse);
-
-    // route to get format for a form
-    formRouter.route('/form/format').post(controller.getFormFormat);
-    
-    // route to get form data for a specific form
-    formRouter.route('/form/:id').get(controller.getFormData);
-
 
     return formRouter;
 }
