dc928e3d20a5a4d039648fa317060bb6952b866d
zhifanjia <allenjia98@gmail.com>
2025-02-10 13:06:05 -0500

    fix router

--- src/routes/eventRouter.js
+++ src/routes/eventRouter.js
@@ -1,19 +1,10 @@
 const express = require('express');
+const eventsController = require('../controllers/eventController');
 
-const routes = function (event) {
-  const controller = require('../controllers/eventController')(event);
-  const eventRouter = express.Router();
+const eventRouter = express.Router();
 
-  eventRouter.route('/events')
-    .get(controller.getEvents);
+eventRouter.get('/events', eventsController.getEvents);
+eventRouter.get('/events/types', eventsController.getEventTypes);
+eventRouter.get('/events/locations', eventsController.getEventLocations);
 
-  eventRouter.route('/events/types')
-    .get(controller.getEventTypes);
-
-  eventRouter.route('/events/locations')
-    .get(controller.getEventLocations);
-
-  return eventRouter;
-};
-
-module.exports = routes;
+module.exports = eventRouter;
