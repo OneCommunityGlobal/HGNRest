a6445fd577c25dd55b5d269880c662b9f23f66c0
Jatin Agrawal <jatinagrawal0801@gmail.com>
2025-03-15 15:55:07 -0700

    fixed new question retrieval system

--- src/models/hgnform.js
+++ src/models/hgnform.js
@@ -4,6 +4,7 @@ const QuestionSchema = new mongoose.Schema({
   text: { type: String, required: true },
   page: { type: Number, required: true },
   title: { type: String, required: true },
+  qno: { type: Number },
 });
 
 module.exports = mongoose.model("Question", QuestionSchema);
