81ed454732a337e6a4c6843b7a03b81a158fbeff
michaellambo <lambomichael7@gmail.com>
2025-03-17 13:50:11 -0400

    Implement changes to mount new route

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -133,6 +133,8 @@ const blueSquareEmailAssignmentRouter = require('../routes/BlueSquareEmailAssign
 
 const collaborationRouter=require('../routes/collaborationRouter');
 
+const imageRoutes = require('../routes/imageRoutes');
+
 module.exports = function (app) {
   app.use('/api', forgotPwdRouter);
   app.use('/api', loginRouter);
@@ -186,4 +188,6 @@ module.exports = function (app) {
   app.use('/api/bm', bmConsumablesRouter);
   app.use('/api/bm', bmExternalTeam);
   app.use('api', bmIssueRouter);
+
+  app.use('/api', imageRoutes);
 };
