2d0a12c654ffc35aec4858d9a046bd48f3543aa8
Meenashi <jeyanthi_sm@yahoo.com>
2025-03-29 14:50:45 +0000

    fix:Missed adding this router in the last commit.

---
+++ src/routes/lbdashboard/paymentsRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const Payments = require('../../models/lbdashboard/payments');
+const paymentsController = require('../../controllers/lbdashboard/paymentsController')(Payments);
+
+const paymentsRouter = express.Router();
+
+paymentsRouter
+  .route('/payments')
+  // .get(paymentsController.getPayments)
+  .post(paymentsController.postPayments);
+
+module.exports = paymentsRouter;
