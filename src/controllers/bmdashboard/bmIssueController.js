7e14a8ece4b95c051b033ac6863a9484c2efd067
Rishtha <rishitha.mamidala36@gmail.com>
2025-04-09 17:43:54 -0500

    fix backend issue

--- src/controllers/bmdashboard/bmIssueController.js
+++ src/controllers/bmdashboard/bmIssueController.js
@@ -13,46 +13,6 @@ const bmIssueController = function (BuildingIssue) {
         }
     };
 
-    // const bmGetIssueChart = async (req, res) => {
-    //     try {
-    //         const { issueType, year } = req.query;
-    //         let matchQuery = {}; // Initialize an empty match query object
-    //         let groupFields = {}; // Initialize group fields
-    
-    //         // Apply filters if provided
-    //         if (issueType) {
-    //             matchQuery.issueType = issueType;
-    //         }
-    //         if (year) {
-    //             const startDate = new Date(`${year}-01-01T00:00:00Z`);
-    //             const endDate = new Date(`${year}-12-31T23:59:59Z`);
-    //             matchQuery.issueDate = { $gte: startDate, $lte: endDate }; // Filter based on issueDate
-    //         }
-    
-    //         // Define the $group stage for grouping by issueType and year of issueDate
-    //         groupFields = {
-    //             _id: {
-    //                 issueType: "$issueType",  // Group by issueType
-    //                 issueYear: { $year: "$issueDate" }, // Extract year from issueDate and group by year
-    //             },
-    //             count: { $sum: 1 }, // Count the number of issues per group
-    //         };
-    
-    //         const aggregationPipeline = [
-    //             { $match: matchQuery },  // Match the filtered data
-    //             { $group: groupFields },  // Group by issueType and issueYear
-    //             { $sort: { "_id.issueYear": 1, "_id.issueType": 1 } }, // Sort by issueYear and issueType
-    //         ];
-    
-    //         const issues = await mongoose.model('buildingIssue').aggregate(aggregationPipeline); // Execute the aggregation pipeline
-    
-    //         res.status(200).json(issues);
-    //     } catch (error) {
-    //         console.error('Error fetching issues:', error);
-    //         res.status(500).json({ message: 'Server error', error });
-    //     }
-    // };
-
     const bmGetIssueChart = async (req, res) => {
         try {
             const { issueType, year } = req.query;
