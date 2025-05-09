bb5c7e5489d7446a05f3c7e6f772744c27936b36
Sundar Machani <sundarmachani@gmail.com>
2025-05-08 19:02:42 -0500

    sundar_create_expected_actual_expense_ phase_2_dashboard_BE

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -132,6 +132,7 @@ const bmToolRouter = require('../routes/bmdashboard/bmToolRouter')(buildingTool,
 const bmEquipmentRouter = require('../routes/bmdashboard/bmEquipmentRouter')(buildingEquipment);
 const bmIssueRouter = require('../routes/bmdashboard/bmIssueRouter')(metIssue);
 const bmExternalTeam = require('../routes/bmdashboard/bmExternalTeamRouter');
+const bmActualVsPlannedCostRouter = require('../routes/bmdashboard/bmActualVsPlannedCostRouter');
 
 const blueSquareEmailAssignmentRouter = require('../routes/BlueSquareEmailAssignmentRouter')(
   blueSquareEmailAssignment,
@@ -199,6 +200,7 @@ module.exports = function (app) {
   app.use('/api/bm', bmConsumablesRouter);
   app.use('/api/bm', bmExternalTeam);
   app.use('/api/bm', bmIssueRouter);
+  app.use('/api/bm', bmActualVsPlannedCostRouter);
   app.use('/api/villages', require('../routes/lb_dashboard/villages'));
   app.use('/api', registrationRouter);
 };
