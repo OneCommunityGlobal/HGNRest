d0f98f62409cba5627b4a00264c63473ca0c80ed
Luis Arevalo <arevaloluis89@yahoo.com>
2025-05-09 22:55:48 -0700

    Guirong taking over for Luis - Unit test for getAllInvInProjectWBS inside InventoryController (#945)
    * test: completed testing for getAllInvInProjectWBS
    
    * test: removed comments and only
    
    * test: added tests for 403 and 400 error
    
    * test: completed test for inventory controller postInvInProjectWBS
    
    * resolve merge conflicts
    
    * Fix failed tests
    
    * Fix failed tests
    
    ---------
    
    Co-authored-by: 喜喜懒羊羊 <luckyyangyang@kikihitsujismbp.mynetworksettings.com>
    Co-authored-by: Guirong Wu <619917585@qq.com>

--- src/routes/inventoryRouter.js
+++ src/routes/inventoryRouter.js
@@ -1,34 +1,38 @@
 const express = require('express');
 
-const routes = function (item, itemType) {
-  const controller = require('../controllers/inventoryController')(item, itemType);
+const routes = function (item, itemType, projects) {
+  const controller = require('../controllers/inventoryController')(item, itemType, projects);
 
   const inventoryRouter = express.Router();
 
-  inventoryRouter.route('/invtype')
-    .get(controller.getAllInvType)
-    .post(controller.postInvType);
+  inventoryRouter.route('/invtype').get(controller.getAllInvType).post(controller.postInvType);
 
-  inventoryRouter.route('/invtype/:typeId')
+  inventoryRouter
+    .route('/invtype/:typeId')
     .get(controller.getInvTypeById)
     .put(controller.putInvType);
 
-  inventoryRouter.route('/inv/:invId') // Single Inventory By Inv ID
+  inventoryRouter
+    .route('/inv/:invId') // Single Inventory By Inv ID
     .get(controller.getInvIdInfo)
     .put(controller.putInvById);
 
-  inventoryRouter.route('/invtransfer/:invId') // Transfer some or all of the inventory to another project/wbs
+  inventoryRouter
+    .route('/invtransfer/:invId') // Transfer some or all of the inventory to another project/wbs
     .put(controller.transferInvById);
 
-  inventoryRouter.route('/invwaste/:invId') // Waste some or all of the inventory
+  inventoryRouter
+    .route('/invwaste/:invId') // Waste some or all of the inventory
     .put(controller.unWasteInvById)
     .delete(controller.delInvById);
 
-  inventoryRouter.route('/inv/:projectId') // All By Project seperated into WBS (wbs can be nill which is the unassigned category)
+  inventoryRouter
+    .route('/inv/:projectId') // All By Project seperated into WBS (wbs can be nill which is the unassigned category)
     .get(controller.getAllInvInProject)
     .post(controller.postInvInProject); // Can create a new inventory item in a project with unassigned wbs
 
-  inventoryRouter.route('/inv/:projectId/wbs/:wbsId') // All By Project seperated into WBS (wbs can be nill which is the unassigned category)
+  inventoryRouter
+    .route('/inv/:projectId/wbs/:wbsId') // All By Project seperated into WBS (wbs can be nill which is the unassigned category)
     .get(controller.getAllInvInProjectWBS)
     .post(controller.postInvInProjectWBS); // Can create a new inventory item in a project with a specified wbs
 
