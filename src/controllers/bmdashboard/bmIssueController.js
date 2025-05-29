59875df9dff6739aa47b2da5e6399be7338ced16
karnati007 <karng01@pfw.edu>
2025-05-30 04:29:01 +0530

    Modified the issue controller get method

--- src/controllers/bmdashboard/bmIssueController.js
+++ src/controllers/bmdashboard/bmIssueController.js
@@ -10,58 +10,7 @@ const bmIssueController = function (buildingIssue) {
             res.json(err);
         }
     };
-
-    const bmPostIssue = async (req, res) => {
-        try {
-            buildingIssue.create(req.body)
-            .then((result) => {
-                res.status(201).send(result)})
-            .catch((error) => {
-                res.status(500).send(error)});
-        } catch (err) {
-            res.json(err);
-        }
-    };
-    const bmUpdateIssue = async (req, res) => {
-        try {
-
-            const updates = req.body;
-
-            if (!updates || typeof updates !== 'object') {
-                return res.status(400).json({ message: 'Invalid update data.' });
-            }
-
-            buildingIssue.findByIdAndUpdate(
-                req.params.id,
-                { $set: updates },
-                { new: true }
-            )
-                .then(updatedIssue => {
-                    if (!updatedIssue) {
-                        return res.status(404).json({ message: 'Issue not found.' });
-                    }
-                    res.json(updatedIssue);
-                })
-                .catch(err => res.status(500).json({ error: err.message }));
-        } catch (error) {
-            res.json({ error: error.message });
-          }
-    };
-
-    const bmDeleteIssue = (req, res) => {
-        const { id } = req.params;
-      
-        buildingIssue.findByIdAndDelete(id)
-          .then((deletedIssue) => {
-            if (!deletedIssue) {
-              return res.status(404).json({ message: 'Issue not found.' });
-            }
-            res.json({ message: 'Issue deleted successfully.' });
-          })
-          .catch((err) => res.status(500).json({ error: err.message }));
-      };
-
-    return { bmGetIssue, bmPostIssue, bmUpdateIssue, bmDeleteIssue };
+    return { bmGetIssue };
 };
 
 module.exports = bmIssueController;
--- src/routes/bmdashboard/bmIssueRouter.js
+++ src/routes/bmdashboard/bmIssueRouter.js
@@ -4,14 +4,8 @@ const routes = function (buildingIssue) {
     const IssueRouter = express.Router();
     const controller = require('../../controllers/bmdashboard/bmIssueController')(buildingIssue);
 
-    IssueRouter.route('/issues')
+    IssueRouter.route('/issues/most-expensive')
         .get(controller.bmGetIssue);
-    IssueRouter.route('/issue/add')
-        .post(controller.bmPostIssue);
-    IssueRouter.route('/issues/:id')
-        .patch(controller.bmUpdateIssue);
-    IssueRouter.route('/issues/:id')
-        .delete(controller.bmDeleteIssue);
     return IssueRouter;
 };
 module.exports = routes;
