7f8f5c6228bccd66996f96182f9576d05300c3da
ReinaT5678 <reinat2015@gmail.com>
2025-05-29 10:33:57 -0700

    backend changes

--- src/controllers/jobsController.js
+++ src/controllers/jobsController.js
@@ -17,7 +17,7 @@ const getJobs = async (req, res) => {
       
       query.$or = [
         { title: { $regex: new RegExp(searchString, 'i') } },
-        { description: { $regex: new RegExp(searchString, 'i') } }
+        { category: { $regex: new RegExp(searchString, 'i') } }
       ];} // Case-insensitive search
     
     if (category) query.category = category;
@@ -63,7 +63,13 @@ const getJobSummaries = async (req, res) => {
 
     // Construct the query object
     const query = {};
-    if (search) query.title = { $regex: search, $options: 'i' };
+    if (search) {
+      const searchString = String(search);
+      query.$or = [
+        { title: { $regex: new RegExp(searchString, 'i') } },
+        { category: { $regex: new RegExp(searchString, 'i') } }
+      ];
+    }
     if (category) query.category = category;
 
     // Sorting logic
