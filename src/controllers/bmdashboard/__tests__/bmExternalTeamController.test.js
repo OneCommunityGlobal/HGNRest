0d5690ff58caa34aa7c1bec4b7d9c901db72a41b
Amalesh Arivanan <amalesh.unofficial@gmail.com>
2025-06-14 13:38:38 -0700

    No tests failing

--- src/controllers/bmdashboard/__tests__/bmExternalTeamController.test.js
+++ src/controllers/bmdashboard/__tests__/bmExternalTeamController.test.js
@@ -13,7 +13,9 @@ beforeAll(async () => {
 
 afterAll(async () => {
   await mongoose.disconnect();
-  await mongoServer.stop();
+  if (mongoServer && typeof mongoServer.stop === 'function') {
+    await mongoServer.stop();
+  }
 });
 
 beforeEach(async () => {
