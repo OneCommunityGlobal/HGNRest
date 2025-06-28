be2f3b5ccc4185fc41f319a517854324efabd2d9
Varun Reddy Mogili <mvarunreddy13@gmail.com>
2025-06-27 22:55:10 -0400

    Refactor weeklySummaries function for improved readability and maintainability

--- src/helpers/reporthelper.js
+++ src/helpers/reporthelper.js
@@ -23,7 +23,6 @@ const reporthelper = function () {
    * @param {integer} endWeekIndex The end week index, eg. 1 for last week.
    */
   const weeklySummaries = async (startWeekIndex, endWeekIndex) => {
-
     const pstStart = moment()
       .tz('America/Los_Angeles')
       .startOf('week')
@@ -160,32 +159,38 @@ const reporthelper = function () {
     ]);
 
     // Logic too difficult to do using aggregation.
-    results.forEach((result) => {    
+    results.forEach((result) => {
       // create Array(4) to hold totalSeconds for each week
       result.totalSeconds = [0, 0, 0, 0];
 
       result.timeEntries.forEach((entry) => {
-        const index = startWeekIndex === endWeekIndex ? startWeekIndex : absoluteDifferenceInWeeks(entry.dateOfWork, pstEnd);
+        const index =
+          startWeekIndex === endWeekIndex
+            ? startWeekIndex
+            : absoluteDifferenceInWeeks(entry.dateOfWork, pstEnd);
         if (result.totalSeconds[index] === undefined || result.totalSeconds[index] === null) {
           result.totalSeconds[index] = 0;
         }
 
         if (entry.isTangible === true) {
           result.totalSeconds[index] += entry.totalSeconds;
-        if (index >= 0 && index < 4) {
-          if (entry.isTangible === true) {
-            result.totalSeconds[index] += entry.totalSeconds;
+          if (index >= 0 && index < 4) {
+            if (entry.isTangible === true) {
+              result.totalSeconds[index] += entry.totalSeconds;
+            }
           }
         }
       });
 
-      result.totalSeconds = result.totalSeconds.map(seconds => seconds === 0 ? undefined : seconds);
+      result.totalSeconds = result.totalSeconds.map((seconds) =>
+        seconds === 0 ? undefined : seconds,
+      );
 
       delete result.timeEntries;
     });
 
-      return results;
-    };
+    return results;
+  };
 
   const getReportReceipents = () => {
     let mappedResults;
