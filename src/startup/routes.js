1cf5cc1c6022193cc6501ed9d0b012f5fb47735a
Sundar Machani <76655727+sundarmachani@users.noreply.github.com>
2025-07-14 15:25:10 -0500

    Phase 2 Summary Dashboard: Create Expected v Actual Expense Graph in  phase 2 dashboard Backend - Sundar (#1375)
    sundar_create_expected_actual_expense_ phase_2_dashboard_BE

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -171,6 +171,7 @@ const bmEquipmentRouter = require('../routes/bmdashboard/bmEquipmentRouter')(bui
 const buildingIssue = require('../models/bmdashboard/buildingIssue');
 const bmIssueRouter = require('../routes/bmdashboard/bmIssueRouter')(buildingIssue);
 const bmExternalTeam = require('../routes/bmdashboard/bmExternalTeamRouter');
+const bmActualVsPlannedCostRouter = require('../routes/bmdashboard/bmActualVsPlannedCostRouter');
 const bmRentalChart = require('../routes/bmdashboard/bmRentalChartRouter')();
 
 const lbMessageRouter = require('../routes/lbdashboard/messagesRouter')(message);
@@ -284,7 +285,7 @@ module.exports = function (app) {
   app.use('/api/bm', bmExternalTeam);
 
   app.use('/api/bm', bmIssueRouter);
-
+  app.use('/api/bm', bmActualVsPlannedCostRouter);
   app.use('/api/bm', bmTimeLoggerRouter);
 
   app.use('/api/lb', bidPropertyRouter);
@@ -295,7 +296,6 @@ module.exports = function (app) {
 
   // lb dashboard
   app.use('/api/lb', lbListingsRouter);
-  // lb dashboard
   app.use('/api/villages', require('../routes/lbdashboard/villages'));
   app.use('/api/lb', lbMessageRouter);
   app.use('/api/lb', lbUserPrefRouter);
