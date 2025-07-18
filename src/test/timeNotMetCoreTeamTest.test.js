43ab00815fdba2602c5a6fa7bac4f1fdc8196c8a
Nikita Kolla <42067463+ArtemisNyx3@users.noreply.github.com>
2025-07-18 18:47:26 -0400

    initial structure for describe

--- src/test/timeNotMetCoreTeamTest.test.js
+++ src/test/timeNotMetCoreTeamTest.test.js
@@ -5,8 +5,18 @@ const mongoose = require('mongoose');
 const moment = require('moment-timezone');
 
 // Import dependencies and the module to test
-const userHelper = require('../helpers/userHelper');
+const userHelper = require('../helpers/userHelper')();
 const userProfile = require('../models/userProfile');
 const dashboardHelper = require('../helpers/dashboardhelper')();
 const emailSender = require('../utilities/emailSender');
 const logger = require('../startup/logger');
+
+describe('Time Not Met Core Team Test', () => {
+  // All core team users
+  // 1. No missed hours
+  // 2. Some missed hours but less than 5 blue squares
+  // 3. More than 5 blue squares with no missed hours
+  // 4. More than 5 blue squares with some missed hours
+  describe('Less than 5 Blue Squares ', () => {});
+  describe('More than 5 Blue Squares ', () => {});
+});
