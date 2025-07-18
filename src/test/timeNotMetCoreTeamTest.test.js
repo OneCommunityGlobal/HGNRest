c86d861faf44affdecc986e76421ac8115a48828
Nikita Kolla <42067463+ArtemisNyx3@users.noreply.github.com>
2025-07-18 19:27:24 -0400

    desdcribed testcases and divivded them into 2 describes

--- src/test/timeNotMetCoreTeamTest.test.js
+++ src/test/timeNotMetCoreTeamTest.test.js
@@ -12,11 +12,13 @@ const emailSender = require('../utilities/emailSender');
 const logger = require('../startup/logger');
 
 describe('Time Not Met Core Team Test', () => {
-  // All core team users
-  // 1. No missed hours
-  // 2. Some missed hours but less than 5 blue squares
-  // 3. More than 5 blue squares with no missed hours
-  // 4. More than 5 blue squares with some missed hours
-  describe('Less than 5 Blue Squares ', () => {});
-  describe('More than 5 Blue Squares ', () => {});
+  describe('Less than 5 Blue Squares ', () => {
+    it('should not assign blue square if no missed hours', async () => {});
+    it('should only carry forward missed hours without additional hours', async () => {});
+  });
+  describe('More than 5 Blue Squares ', () => {
+    it('should not assign blue square if no missed hours', async () => {});
+    it('should only carry forward missed hours with additional hours', async () => {});
+    it('should not assign assign extra hours(only missed hours are added) if this is the 5th blue square', async () => {});
+  });
 });
