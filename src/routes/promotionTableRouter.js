b3143b98af411664f15e3299f16f27f7c8525abf
kristin-7 <46657300+KH07@users.noreply.github.com>
2025-07-18 20:39:22 -0700

    feat: add more routes in router

--- src/routes/promotionTableRouter.js
+++ src/routes/promotionTableRouter.js
@@ -3,6 +3,8 @@ const express = require('express');
 const promotionTableRouter = express.Router();
 const controller = require('../controllers/promotionTableController')();
 
+promotionTableRouter.get('/review-for-this-week', controller.reviewForThisWeek);
+promotionTableRouter.post('/process-promotions', controller.processPromotions);
 promotionTableRouter.route('/').get(controller.getPromotionEligibility);
 
 module.exports = promotionTableRouter;
