b7e5a93d5221b4394fb35c728150b6b3300a149e
boppanapraveen <61085397+boppanapraveen@users.noreply.github.com>
2025-07-20 15:00:04 -0500

    Small changes

--- src/controllers/dashBoardController.js
+++ src/controllers/dashBoardController.js
@@ -360,7 +360,7 @@ const dashboardcontroller = function () {
       "daterequestedFeedback": "2025-04-20T04:04:40.189Z",
       "foundHelpSomeWhereClosePermanently": false,
       "userId": "5baac381e16814009017678c"
-  }*/
+  } */
     try {
       const savingRequestFeedbackData = await dashboardhelper.requestFeedback(req);
       return res.status(200).json({ savingRequestFeedbackData });
@@ -396,7 +396,7 @@ const dashboardcontroller = function () {
     {
     "foundHelpSomeWhereClosePermanently": true,
     "userId": "5baac381e16814009017678c"
-}*/
+} */
     try {
       const foundHelp = await dashboardhelper.checkQuestionaireFeedback(req);
       return res.status(200).json({ foundHelp });
