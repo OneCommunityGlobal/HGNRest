3d435beb999882462b4e24e3cd9c3768d489806c
marcusyi1 <143385530+marcusyi1@users.noreply.github.com>
2025-07-24 15:09:55 -0700

    fix: resolved restricted syntax errors

--- src/jobs/dailyMessageEmailNotification.js
+++ src/jobs/dailyMessageEmailNotification.js
@@ -6,7 +6,6 @@ const emailSender = require('../utilities/emailSender');
 
 // Set test mode and test email
 const TEST_MODE = true; // Set to false to disable test mode
-const TEST_EMAIL = 'test@example.com';
 
 // Schedule the job to run daily at midnight
 cron.schedule('0 0 * * *', async () => {
