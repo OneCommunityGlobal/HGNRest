416f731579b6ea1697e697acca7b286b1565186b
myeeli <myeeli@uab.edu>
2025-07-24 12:42:44 -0400

    Resolve Merge Conflicts

--- src/controllers/dashBoardController.js
+++ src/controllers/dashBoardController.js
@@ -145,7 +145,7 @@ const dashboardcontroller = function () {
   };
 
   // 6th month and yearly anniversaries
-    postTrophyIcon = function (req, res) {
+  const postTrophyIcon = function (req, res) {
     console.log("API called with params:", req.params);
     const userId = mongoose.Types.ObjectId(req.params.userId);
     const trophyFollowedUp = req.params.trophyFollowedUp === 'true';
@@ -168,23 +168,7 @@ const dashboardcontroller = function () {
   };
 
   // 6th month and yearly anniversaries
-  const postTrophyIcon = function (req, res) {
-    // console.log('API called with params:', req.params);
-    const userId = mongoose.Types.ObjectId(req.params.userId);
-    const trophyFollowedUp = req.params.trophyFollowedUp === 'true';
 
-    User.findByIdAndUpdate(userId, { trophyFollowedUp }, { new: true })
-      .then((updatedRecord) => {
-        if (!updatedRecord) {
-          return res.status(404).send('No valid records found');
-        }
-        res.status(200).send(updatedRecord);
-      })
-      .catch((error) => {
-        console.error('Error updating trophy icon:', error);
-        res.status(500).send(error);
-      });
-  };
 
   const orgData = function (req, res) {
     const fullOrgData = dashboardhelper.getOrgData();
