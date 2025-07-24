4761f5f5f9743064110fc609f02cdf1f3167cc63
Vamsidhar858 <145138793+Vamsidhar858@users.noreply.github.com>
2025-07-23 19:01:22 -0700

    Fix: Removed stray closing bracket causing syntax error

--- src/helpers/reporthelper.js
+++ src/helpers/reporthelper.js
@@ -163,122 +163,119 @@ const reporthelper = function () {
     results.forEach((result) => {
       // create Array(4) to hold totalSeconds for each week
       result.totalSeconds = [0, 0, 0, 0];
-      results.forEach((result) => {
-        // create Array(4) to hold totalSeconds for each week
-        result.totalSeconds = [0, 0, 0, 0];
 
-        result.timeEntries.forEach((entry) => {
-          const index = startWeekIndex === endWeekIndex ? startWeekIndex : absoluteDifferenceInWeeks(entry.dateOfWork, pstEnd);
+      result.timeEntries.forEach((entry) => {
+        const index = startWeekIndex === endWeekIndex ? startWeekIndex : absoluteDifferenceInWeeks(entry.dateOfWork, pstEnd);
 
-          if (index >= 0 && index < 4) {
-            if (entry.isTangible === true) {
-              result.totalSeconds[index] += entry.totalSeconds;
-            }
+        if (index >= 0 && index < 4) {
+          if (entry.isTangible === true) {
+            result.totalSeconds[index] += entry.totalSeconds;
           }
-        });
-
-        result.totalSeconds = result.totalSeconds.map(seconds => seconds === 0 ? undefined : seconds);
-
-        delete result.timeEntries;
-      });
-
-      return results;
-    };
-
-    const getReportReceipents = () => {
-      let mappedResults;
-      userProfile.find({ getWeeklyReport: true }, { email: 1, _id: 0 }).then((results) => {
-        mappedResults = results.map((ele) => ele.email);
-        mappedResults.push('onecommunityglobal@gmail.com', 'onecommunityhospitality@gmail.com');
-        console.log('results:', mappedResults);
-      });
-      return mappedResults;
-    };
-    /**
-     * Checks whether a date belongs to a specific week based on week index.
-     *
-     * @param {string} dueDate The date to check.
-     * @param {integer} weekIndex An integer in the range 0 - 3, where 0 (this week),
-     *                      1 (last week), 2 (the week before last) or 3 (three weeks ago).
-     * @return {boolean} True if match, false otherwise.
-     */
-    const doesDateBelongToWeek = function (dueDate, weekIndex) {
-      const pstStartOfWeek = moment()
-        .tz('America/Los_Angeles')
-        .startOf('week')
-        .subtract(weekIndex, 'week');
-      const pstEndOfWeek = moment()
-        .tz('America/Los_Angeles')
-        .endOf('week')
-        .subtract(weekIndex, 'week');
-      const fromDate = moment(pstStartOfWeek).toDate();
-      const toDate = moment(pstEndOfWeek).toDate();
-      return moment(dueDate).isBetween(fromDate, toDate, undefined, '[]');
-    };
-
-    /**
-     * Get the week index relative to this week, eg. 0 (this week),
-     * 1 (last week), 2 (the week before last) or 3 (three weeks ago).
-     *
-     * @param {string} dueDate The date to check.
-     * @return {integer} The week index, -1 if no match.
-     */
-    const getTheWeek = function (dueDate) {
-      if (doesDateBelongToWeek(dueDate, 0)) return 0;
-      if (doesDateBelongToWeek(dueDate, 1)) return 1;
-      if (doesDateBelongToWeek(dueDate, 2)) return 2;
-      if (doesDateBelongToWeek(dueDate, 3)) return 3;
-      return -1;
-    };
-
-    /**
-     * This function will make sure the weeklySummaries' array
-     * has its entries always in the right position based on the dueDate.
-     * That means that:
-     *  - the 1st entry should always have the dueDate for this week.
-     *  - the 2nd entry should always have the dueDate for last week.
-     *  - the 3rd entry should always have the dueDate for the week before last.
-     *  - the 4th entry should always have the dueDate for three weeks ago.
-     *
-     * @param {Object} results An array of user objects with selected fields.
-     * @return {Object} An array of user objects with properly sorted weeklySummaries by due date.
-     */
-    const formatSummaries = function (results) {
-      return results.map((user) => {
-        const { weeklySummaries: wS } = user;
-        const wSummaries = [];
-
-        if (Array.isArray(wS) && wS.length && wS.length < 3) {
-          // Common cases for the first entry.
-          if (getTheWeek(wS[0].dueDate) === 0) wSummaries[0] = { ...wS[0] };
-          if (getTheWeek(wS[0].dueDate) === 1) {
-            wSummaries[0] = null;
-            wSummaries[1] = { ...wS[0] };
-          }
-          // When single entry.
-          if (wS.length === 1) {
-            // Special case when first entry belongs to week before last.
-            if (getTheWeek(wS[0].dueDate) === 2) {
-              wSummaries[0] = null;
-              wSummaries[1] = null;
-              wSummaries[2] = { ...wS[0] };
-            }
-          } else {
-            // When two entries.
-            if (getTheWeek(wS[1].dueDate) === 1) wSummaries[1] = { ...wS[1] };
-            if (getTheWeek(wS[1].dueDate) === 2) wSummaries[2] = { ...wS[1] };
-          }
-          user = { ...user, weeklySummaries: wSummaries };
         }
-        return user;
       });
-    };
 
-    return {
-      weeklySummaries,
-      formatSummaries,
-      getReportReceipents,
-    };
+      result.totalSeconds = result.totalSeconds.map(seconds => seconds === 0 ? undefined : seconds);
+
+      delete result.timeEntries;
+    });
+
+    return results;
   };
 
-  module.exports = reporthelper;
+  const getReportReceipents = () => {
+    let mappedResults;
+    userProfile.find({ getWeeklyReport: true }, { email: 1, _id: 0 }).then((results) => {
+      mappedResults = results.map((ele) => ele.email);
+      mappedResults.push('onecommunityglobal@gmail.com', 'onecommunityhospitality@gmail.com');
+      console.log('results:', mappedResults);
+    });
+    return mappedResults;
+  };
+  /**
+   * Checks whether a date belongs to a specific week based on week index.
+   *
+   * @param {string} dueDate The date to check.
+   * @param {integer} weekIndex An integer in the range 0 - 3, where 0 (this week),
+   *                      1 (last week), 2 (the week before last) or 3 (three weeks ago).
+   * @return {boolean} True if match, false otherwise.
+   */
+  const doesDateBelongToWeek = function (dueDate, weekIndex) {
+    const pstStartOfWeek = moment()
+      .tz('America/Los_Angeles')
+      .startOf('week')
+      .subtract(weekIndex, 'week');
+    const pstEndOfWeek = moment()
+      .tz('America/Los_Angeles')
+      .endOf('week')
+      .subtract(weekIndex, 'week');
+    const fromDate = moment(pstStartOfWeek).toDate();
+    const toDate = moment(pstEndOfWeek).toDate();
+    return moment(dueDate).isBetween(fromDate, toDate, undefined, '[]');
+  };
+
+  /**
+   * Get the week index relative to this week, eg. 0 (this week),
+   * 1 (last week), 2 (the week before last) or 3 (three weeks ago).
+   *
+   * @param {string} dueDate The date to check.
+   * @return {integer} The week index, -1 if no match.
+   */
+  const getTheWeek = function (dueDate) {
+    if (doesDateBelongToWeek(dueDate, 0)) return 0;
+    if (doesDateBelongToWeek(dueDate, 1)) return 1;
+    if (doesDateBelongToWeek(dueDate, 2)) return 2;
+    if (doesDateBelongToWeek(dueDate, 3)) return 3;
+    return -1;
+  };
+
+  /**
+   * This function will make sure the weeklySummaries' array
+   * has its entries always in the right position based on the dueDate.
+   * That means that:
+   *  - the 1st entry should always have the dueDate for this week.
+   *  - the 2nd entry should always have the dueDate for last week.
+   *  - the 3rd entry should always have the dueDate for the week before last.
+   *  - the 4th entry should always have the dueDate for three weeks ago.
+   *
+   * @param {Object} results An array of user objects with selected fields.
+   * @return {Object} An array of user objects with properly sorted weeklySummaries by due date.
+   */
+  const formatSummaries = function (results) {
+    return results.map((user) => {
+      const { weeklySummaries: wS } = user;
+      const wSummaries = [];
+
+      if (Array.isArray(wS) && wS.length && wS.length < 3) {
+        // Common cases for the first entry.
+        if (getTheWeek(wS[0].dueDate) === 0) wSummaries[0] = { ...wS[0] };
+        if (getTheWeek(wS[0].dueDate) === 1) {
+          wSummaries[0] = null;
+          wSummaries[1] = { ...wS[0] };
+        }
+        // When single entry.
+        if (wS.length === 1) {
+          // Special case when first entry belongs to week before last.
+          if (getTheWeek(wS[0].dueDate) === 2) {
+            wSummaries[0] = null;
+            wSummaries[1] = null;
+            wSummaries[2] = { ...wS[0] };
+          }
+        } else {
+          // When two entries.
+          if (getTheWeek(wS[1].dueDate) === 1) wSummaries[1] = { ...wS[1] };
+          if (getTheWeek(wS[1].dueDate) === 2) wSummaries[2] = { ...wS[1] };
+        }
+        user = { ...user, weeklySummaries: wSummaries };
+      }
+      return user;
+    });
+  };
+
+  return {
+    weeklySummaries,
+    formatSummaries,
+    getReportReceipents,
+  };
+};
+
+module.exports = reporthelper;
