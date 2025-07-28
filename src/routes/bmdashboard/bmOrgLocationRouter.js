1fbb3a45165c22876e9329b026284bc8cdd4cd98
ReinaT5678 <reinat2015@gmail.com>
2025-07-28 13:09:51 -0700

    feat(routes): missing routes

--- src/routes/bmdashboard/bmOrgLocationRouter.js
+++ src/routes/bmdashboard/bmOrgLocationRouter.js
@@ -1,16 +1,16 @@
-const express = require ("express");
+const express = require('express');
 
-const routes = function() {
-    // initialize routes 
-    const NewOrgRouter = express.Router();
-    const orgLocationController = require('../../controllers/bmdashboard/bmOrgsController')();
-    // get all organizations 
-    NewOrgRouter.route('/orgLocation').get(orgLocationController.getAllOrgs);
+const routes = function () {
+  // initialize routes
+  const NewOrgRouter = express.Router();
+  const orgLocationController = require('../../controllers/bmdashboard/bmOrgsController')();
+  // get all organizations
+  NewOrgRouter.route('/orgLocation').get(orgLocationController.getAllOrgs);
 
-    // get one organization by it's ID
-    NewOrgRouter.route('/orgLocation/:id').get(orgLocationController.getOrgById);
+  // get one organization by it's ID
+  NewOrgRouter.route('/orgLocation/:id').get(orgLocationController.getOrgById);
 
-    return NewOrgRouter;
+  return NewOrgRouter;
 };
 
 module.exports = routes;
