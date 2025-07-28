0f20819c4d2e724b18315df2eadb1139835e3f60
Deekshith Kumar <deekshithkumar.singirikonda@gmail.com>
2025-07-28 11:25:14 -0400

    test(api): add unit tests for registration flow

--- jest-integration.config.js
+++ jest-integration.config.js
@@ -1,4 +1,21 @@
-const config = require('./jest.config');
+// const config = require('./jest.config');
 
-config.testMatch = ['**/*.test.js'];
-module.exports = config;
+// config.testMatch = ['**/*.test.js'];
+// module.exports = config;
+
+const path = require('path');
+const baseConfig = require('./jest.config');
+
+module.exports = {
+  ...baseConfig,
+  testMatch: ['**/test/integration/**/*.test.js'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.js$': [
+      'babel-jest',
+      {
+        configFile: path.resolve(__dirname, 'babel.config.js'),
+      },
+    ],
+  },
+};
