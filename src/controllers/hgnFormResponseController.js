019fc6aa36d052d4a7a483dd5dbd37293fb57af2
saramohansatyaram <saramohansatyaram@gmail.com>
2025-08-07 04:10:30 -0500

    Add hasPermission check to getAllFormResponses() in hgnFormResponseController

--- src/controllers/hgnFormResponseController.js
+++ src/controllers/hgnFormResponseController.js
@@ -1,4 +1,5 @@
 const FormResponse= require('../models/hgnFormResponse');
+const { hasPermission } = require('../utilities/permissions');
 
 const hgnFormController = function (){
     const submitFormResponse=async function(req, res){
@@ -21,6 +22,13 @@ const hgnFormController = function (){
 
     const getAllFormResponses=async function(req, res){
         try {
+            // Check if user has permission to access HGN Skills Dashboard
+            if (!await hasPermission(req.body.requestor, 'accessHgnSkillsDashboard')) {
+                return res.status(403).json({ 
+                    error: 'You are not authorized to access the HGN Skills Dashboard.' 
+                });
+            }
+            
             const formResponses = await FormResponse.find();
             res.json(formResponses);
         } catch (err) {
