3267d6ea9695e7f6eef248272e32e326414a43f1
Jaissica <jaissica.hora@gmail.com>
2025-08-14 00:41:13 -0400

    Jaissica -  create donut chart backend for tools available (#1331)
    * feat: implemented backend for donut chart
    
    * updated bmIssueRouter api
    
    * fix: updated controller
    
    * fix: jaissica added exception for project and tool id
    
    ---------
    
    Co-authored-by: Jaissica Hora <jaissicahora@Jaissicas-MacBook-Air.local>
    Co-authored-by: Jaissica Hora <jaissicahora@macbookair.mynetworksettings.com>

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -168,6 +168,8 @@ const bmInventoryTypeRouter = require('../routes/bmdashboard/bmInventoryTypeRout
   toolType,
   equipmentType,
 );
+
+const toolAvailabilityRoutes = require('../routes/bmdashboard/bmToolAvailabilityRoutes');
 const bmDashboardRouter = require('../routes/bmdashboard/bmDashboardPrototypeRouter')(
   dashboardMetrics,
   buildingProject,
@@ -355,6 +357,7 @@ module.exports = function (app) {
   app.use('/api/financials', bmFinancialRouter);
 
   app.use('/api', registrationRouter);
+  app.use('/api', toolAvailabilityRoutes);
   app.use('/api', projectMaterialRouter);
   app.use('/api/bm', bmRentalChart);
   app.use('/api/lb', lbWishlistsRouter);
