01303e90e1ce068bc691fbc5e7a46f598141a887
Meenashi <jeyanthi_sm@yahoo.com>
2025-08-24 12:16:50 +0100

    chore: remove log messages

--- src/controllers/CommunityPortal/eventFeedbackController.js
+++ src/controllers/CommunityPortal/eventFeedbackController.js
@@ -3,18 +3,15 @@ const logger = require('../../startup/logger');
 
 const eventFeedbackController = function () {
   const submitEventFeedbackResponse = async function (req, res) {
-    console.log(req.body);
     logger.logInfo(`Event Feedback Frontend : ${JSON.stringify(req.body)}`);
 
     const { name, email, rating, comments, eventId, attendanceId } = req.body;
     const createdBy = name;
-    console.log(createdBy);
     if (!name || !email || !rating || !eventId) {
       return res.status(400).json({
         error: 'All fields (name, email, rating, eventId) are required',
       });
     }
-    console.log(req.body.requestor.role);
 
     if (req.body.requestor.role !== 'Owner' && req.body.requestor.role !== 'Administrator') {
       res.status(403).send('You are not authorized to add changes in the eventFeedbacks.');
@@ -30,11 +27,9 @@ const eventFeedbackController = function () {
         attendanceId,
         createdBy,
       });
-      console.log(eventFeedbackResponse);
       await eventFeedbackResponse.save();
       res.status(201).json(eventFeedbackResponse);
     } catch (err) {
-      console.log(err);
       res.status(500).json({ error: `Failed to create eventFeedbackResponse: ${err.message}` });
     }
   };
