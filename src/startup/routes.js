4666475627c06d969ef97efff83609dade62a71b
Vamsidhar858 <145138793+Vamsidhar858@users.noreply.github.com>
2025-08-28 15:07:16 -0700

    feat: add cost breakdown API endpoints and models for expenditure tracking

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved, import/no-extraneous-dependencies, import/extensions, import/no-cycle, import/order, import/no-self-import, import/no-useless-path-segments */
 const timeEntry = require('../models/timeentry');
 const userProfile = require('../models/userProfile');
 const project = require('../models/project');
@@ -159,6 +160,9 @@ const injuryCategoryRoutes = require('../routes/bmdashboard/injuryCategoryRouter
 const bmConsumablesRouter = require('../routes/bmdashboard/bmConsumablesRouter')(
   buildingConsumable,
 );
+const bmExpenditureRouter = require('../routes/bmdashboard/bmExpenditureRouter')();
+const costBreakdown = require('../models/bmdashboard/costBreakdown');
+const costBreakdownRouter = require('../routes/bmdashboard/costBreakdownRouter')(costBreakdown);
 const bmInventoryTypeRouter = require('../routes/bmdashboard/bmInventoryTypeRouter')(
   invTypeBase,
   materialType,
@@ -345,8 +349,10 @@ module.exports = function (app) {
   app.use('/api/slack', slackRouter);
   app.use('/api/accessManagement', appAccessRouter);
   app.use('/api/bm', bmExternalTeam);
+  app.use('/api/bm', bmExpenditureRouter);
+  app.use('/api', costBreakdownRouter);
   app.use('/api', bmProjectRiskProfileRouter);
-
+  app.use('/api/bm', bmIssueRouter);
   app.use('/api/bm', bmTimeLoggerRouter);
   app.use('/api/bm/injuries', injuryCategoryRoutes);
   app.use('/api', toolAvailabilityRouter);
