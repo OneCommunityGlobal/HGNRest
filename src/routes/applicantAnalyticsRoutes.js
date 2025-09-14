89bae064f9afec630a457650ec081233cc099e13
One Community <one-community@users.noreply.github.com>
2025-09-14 00:18:39 -0700

    Revert "Pranav - fix(backend): create donut chart for applicant experience br…"
    This reverts commit 94a522f483010b8cffe7bfe4ead31064e1eccb98.

--- src/routes/applicantAnalyticsRoutes.js
+++ src/routes/applicantAnalyticsRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
 const router = express.Router();
 const Applicant = require('../models/jobApplicants');
 const experienceBreakdownController = require('../controllers/applicantAnalyticsController');
 
-const { getExperienceBreakdown, getAllRoles } = experienceBreakdownController(Applicant);
+const { getExperienceBreakdown } = experienceBreakdownController(Applicant);
 
 router.get('/experience-breakdown', getExperienceBreakdown);
-router.get('/experience-roles', getAllRoles);
+
 module.exports = router;
