5789350f20869a93469e12d7de421040f60b00a6
1709abhishek <1709abhishek@gmail.com>
2025-09-26 17:54:52 -0500

    check coverage %age

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,29 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '!<rootDir>/src/**/*.js', // need to collect coverage from all files that are involved in serving an API request
-    '!<rootDir>/src/server.js',
-    '!<rootDir>/src/models/**/*.js',
-    '!<rootDir>/src/startup/**/*.js',
-    '!<rootDir>/src/test/**/*.js',
-    '!<rootDir>/src/utilities/**/*.js', // need to collect coverage from utilities after all unit tests have been created
+    'src/**/*.js',
+    'src/controllers/**/*.js',
+    'src/routes/**/*.js', 
+    'src/utilities/**/*.js',
+    'src/helpers/**/*.js',
+    'src/models/**/*.js',
+    'src/services/**/*.js',
+    'src/middleware/**/*.js',
+    // Exclude test files and mock data
+    '!src/**/*.test.js',
+    '!src/**/*.spec.js',
+    '!src/**/*MockData.js',
+    '!src/**/*MockData.jsx',
+    '!src/test/**',
+    '!src/__tests__/**'
   ],
     // Coverage thresholds - Start light and increase gradually
   coverageThreshold: {
     global: {
-      branches: 60,
-      functions: 60,
-      lines: 60,
-      statements: 60
+      branches: 12,
+      functions: 27,
+      lines: 34,
+      statements: 34
     },
     
   },
