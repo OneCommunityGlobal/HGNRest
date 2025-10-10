81d5a258ecf2ea3372e1a3d0596d9761a58bbebc
ALISHA WALUNJ <walunjalisha@gmail.com>
2025-10-10 18:21:12 -0400

    feat(grouped-bar-graph): model schema and routes configuration

---
+++ src/controllers/bmdashboard/issueAnalyticsController.js
@@ -0,0 +1,11 @@
+const IssueAnalyticsController = function () {
+  // eslint-disable-next-line no-unused-vars
+  const getTrends = (req, res) => {};
+  // eslint-disable-next-line no-unused-vars
+  const getSummary = (req, res) => {};
+  return {
+    getTrends,
+    getSummary,
+  };
+};
+module.exports = IssueAnalyticsController;
--- src/routes/bmdashboard/bmIssueRouter.js
+++ src/routes/bmdashboard/bmIssueRouter.js
@@ -3,11 +3,16 @@ const express = require('express');
 const routes = function (metIssue) {
   const IssueRouter = express.Router();
   const controller = require('../../controllers/bmdashboard/bmIssueController')(metIssue);
+  const issueController = require('../../controllers/bmdashboard/issueAnalyticsController');
 
   IssueRouter.route('/issues').get(controller.bmGetIssue);
   IssueRouter.route('/issue/add').post(controller.bmPostIssue);
   IssueRouter.route('/issue/issue-chart').get(controller.bmGetIssueChart);
   IssueRouter.route('/issues/longest-open').get(controller.getLongestOpenIssues);
+
+  // New routes for issue analytics
+  IssueRouter.route('/trends').get(issueController.getAllIssueAnalytics);
+  IssueRouter.route('/summary').get(issueController.getIssueAnalyticsById);
   return IssueRouter;
 };
 module.exports = routes;
