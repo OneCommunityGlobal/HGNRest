7a86c05ecea01144d12676ac194fef3e6ee2e27b
Julia Ha <juliaha0902@gmail.com>
2025-10-29 00:22:58 -0500

    build: Create route for upload file

--- src/routes/studentTaskRouter.js
+++ src/routes/studentTaskRouter.js
@@ -4,13 +4,13 @@ const routes = function () {
   const controller = require('../controllers/studentTaskController')();
   const studentTaskRouter = express.Router();
 
-  studentTaskRouter
-    .route('/student/tasks')
-    .get(controller.getStudentTasks);
+  studentTaskRouter.route('/student/tasks').get(controller.getStudentTasks);
 
-  studentTaskRouter
-    .route('/student/tasks/:taskId/progress')
-    .put(controller.updateTaskProgress);
+  studentTaskRouter.route('/student/tasks/:taskId/progress').put(controller.updateTaskProgress);
+
+  studentTaskRouter.route('/student/tasks/:taskId/upload').post(controller.uploadFile);
+
+  studentTaskRouter.route('/student/tasks/:taskId/download').get(controller.getFile);
 
   return studentTaskRouter;
 };
