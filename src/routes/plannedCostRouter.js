d68dffca89880896a9e7092b0979b4c6e714085c
saikrishnaraoyadagiri <saikrishnaraoy1997@gmail.com>
2025-11-14 18:49:08 -0500

    fix db issue

--- src/routes/plannedCostRouter.js
+++ src/routes/plannedCostRouter.js
@@ -1,25 +1,25 @@
 const express = require('express');
 
-const routes = function (PlannedCost, Project) {
-  const controller = require('../controllers/plannedCostController')(PlannedCost, Project);
-  const plannedCostRouter = express.Router();
+const routes = function (Cost, Project) {
+  const controller = require('../controllers/plannedCostController')(Cost, Project);
+  const CostRouter = express.Router();
 
   // Include /projects prefix in the router paths
-  plannedCostRouter.route('/projects/:projectId/planned-cost-breakdown').get((req, res, next) => {
+  CostRouter.route('/projects/:projectId/planned-cost-breakdown').get((req, res, next) => {
     console.log('[Router] Route hit for planned-cost-breakdown, projectId:', req.params.projectId);
     next();
   }, controller.getPlannedCostBreakdown);
 
-  plannedCostRouter
+  CostRouter
     .route('/projects/:projectId/planned-costs')
     .get(controller.getAllPlannedCostsForProject)
     .post(controller.createOrUpdatePlannedCost);
 
-  plannedCostRouter
+  CostRouter
     .route('/projects/:projectId/planned-costs/:category')
     .delete(controller.deletePlannedCost);
 
-  return plannedCostRouter;
+  return CostRouter;
 };
 
 module.exports = routes;
