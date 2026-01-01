eec15c9978f6e256dfaca37cf07bdc3095906736
sumedhkumar96 <sumedhkumar96@gmail.com>
2026-01-01 14:02:23 -0800

    added an api to update equipment in bmdashboard

--- src/routes/bmdashboard/bmEquipmentRouter.js
+++ src/routes/bmdashboard/bmEquipmentRouter.js
@@ -8,6 +8,8 @@ const routes = function (BuildingEquipment) {
 
   equipmentRouter.route('/equipment/:equipmentId').get(controller.fetchSingleEquipment);
 
+  equipmentRouter.route('/equipment/:equipmentId/status').put(controller.updateEquipmentStatus);
+
   equipmentRouter.route('/equipment/purchase').post(controller.bmPurchaseEquipments);
 
   equipmentRouter.route('/equipments').get(controller.fetchBMEquipments);
