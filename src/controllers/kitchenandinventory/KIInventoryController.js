296353c84d6b96c1cb906df652c255f48a69a5df
Bhanu Anish <bhanuanishakkineni77@gmail.com>
2026-01-23 18:34:55 -0600

    feat: create get items and update items endpoints

--- src/controllers/kitchenandinventory/KIInventoryController.js
+++ src/controllers/kitchenandinventory/KIInventoryController.js
@@ -17,7 +17,6 @@ const KIInventoryController = () => {
       nextHarvestDate,
       nextHarvestQuantity,
     } = req.body;
-
     const newItem = new KIInventoryItem({
       name,
       storedQuantity,
@@ -34,7 +33,6 @@ const KIInventoryController = () => {
       nextHarvestDate,
       nextHarvestQuantity,
     });
-
     try {
       const savedItem = await newItem.save();
       res.status(201).json({
@@ -45,6 +43,72 @@ const KIInventoryController = () => {
       res.status(400).json({ message: error.message });
     }
   };
-  return { addItem };
+  const getItems = async (req, res) => {
+    try {
+      const items = await KIInventoryItem.find(null, { __v: 0 }).lean().sort({ createdAt: -1 });
+      res.status(200).json({ message: 'All Items fetched successfully.', data: items });
+    } catch (err) {
+      res.status(400).json({ message: 'Something went wrong while fetching items.' });
+    }
+  };
+  const getItemsByCategory = async (req, res) => {
+    const { category } = req.params;
+    try {
+      const items = await KIInventoryItem.find({ category }, { __v: 0 })
+        .lean()
+        .sort({ createdAt: -1 });
+      res.status(200).json({ message: 'Items fetched successfully.', data: items });
+    } catch (err) {
+      res.status(400).json({ message: 'Something went wrong while fetching items.' });
+    }
+  };
+  const updateOnUsage = async (req, res) => {
+    const { itemId, usedQuantity } = req.body; // Need to implement validation for usedQuantity
+    try {
+      const item = await KIInventoryItem.findById(itemId);
+      if (!item) {
+        return res.status(404).json({ message: 'Item not found.' });
+      }
+      if (item.expiryDate < new Date()) {
+        return res.status(400).json({ message: `This item was expired on ${item.expiryDate}` });
+      }
+      let present = item.presentQuantity;
+      present -= usedQuantity;
+      if (present < 0) {
+        present = 0;
+      }
+      item.presentQuantity = present;
+      item.updatedAt = new Date();
+      await item.save();
+      res.status(200).json({ message: 'Item usage updated successfully.', data: item });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  };
+  const updateStoredQuantity = async (req, res) => {
+    const { itemId, addedQuantity, newExpiry } = req.body;
+    try {
+      const item = await KIInventoryItem.findById(itemId);
+      if (!item) {
+        return res.status(404).json({ message: 'Item not found.' });
+      }
+      if (item.presentQuantity === 0 || item.expiryDate < new Date()) {
+        item.storedQuantity = addedQuantity;
+        item.presentQuantity = 0;
+      } else {
+        item.storedQuantity += addedQuantity;
+      }
+      item.presentQuantity += addedQuantity;
+      if (newExpiry) {
+        item.expiryDate = newExpiry;
+      }
+      item.updatedAt = new Date();
+      await item.save();
+      res.status(200).json({ message: 'Stored quantity updated successfully.', data: item });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  };
+  return { addItem, getItems, getItemsByCategory, updateOnUsage, updateStoredQuantity };
 };
 module.exports = KIInventoryController;
--- src/routes/kitchenandinventory/KIInventoryRouter.js
+++ src/routes/kitchenandinventory/KIInventoryRouter.js
@@ -3,8 +3,13 @@ const controller = require('../../controllers/kitchenandinventory/KIInventoryCon
 
 const router = function () {
   const inventoryRouter = express.Router();
-
-  inventoryRouter.route('/items').post(controller.addItem);
+  // Routes for inventory items
+  inventoryRouter.route('/items').post(controller.addItem); // Route to add a new inventory item
+  inventoryRouter.route('/items').get(controller.getItems); // Route to get all inventory items
+  inventoryRouter.route('/items/:category').get(controller.getItemsByCategory); // Route to get items by category
+  // Below update endpoints are non-idempotent and meant to be used for specific actions
+  inventoryRouter.route('/items/updateOnUsage').post(controller.updateOnUsage); // Route to update item on usage
+  inventoryRouter.route('/items/updateStoredQuantity').post(controller.updateStoredQuantity); // Route to update stored quantity
   return inventoryRouter;
 };
 
