d25c5ef55cc3762a868d936b82c42d9cbe3b1742
Diya Wadhwani <wadhwani2399@gmail.com>
2026-01-29 03:42:07 -0500

    chore(fix): mod for failing test

--- src/constants/userProfile.js
+++ src/constants/userProfile.js
@@ -1,18 +1,24 @@
-export const InactiveReason = {
+const InactiveReason = {
   PAUSED: 'Paused',
   SEPARATED: 'Separated',
   SCHEDULED_SEPARATION: 'ScheduledSeparation',
 };
 
-export const UserStatusOperations = {
+const UserStatusOperations = {
   ACTIVATE: 'ACTIVATE',
   DEACTIVATE: 'DEACTIVATE',
   SCHEDULE_DEACTIVATION: 'SCHEDULE_DEACTIVATION',
   PAUSE: 'PAUSE',
 };
 
-export const LifecycleStatus = {
+const LifecycleStatus = {
   PAUSE_TO_ACTIVE: 'PauseToActive',
   SEPARATED_TO_ACTIVE: 'SeparatedToActive',
   SCHEDULED_SEPARATION_TO_ACTIVE: 'ScheduledSeparationToActive',
+};
+
+module.exports = {
+  InactiveReason,
+  UserStatusOperations,
+  LifecycleStatus,
 };
