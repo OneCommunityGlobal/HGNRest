49b374b9b0e225e159714cec15f0637cc8eb99c3
Sundar Machani <sundarmachani@Sundars-MacBook-Pro.local>
2026-03-28 12:30:28 +0530

    fix: puppeteer chrome install via postinstall

--- src/utilities/puppeteerUtil.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-
-function getChromeExecutable() {
-  const executablePath = puppeteer.executablePath();
-  console.log('Using Chrome at:', executablePath);
-  return executablePath;
-}
-
-module.exports = { getChromeExecutable };
